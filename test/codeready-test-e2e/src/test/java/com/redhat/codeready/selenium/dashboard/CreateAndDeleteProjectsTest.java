45ea7393af51d465bbad9d93d5fd0c7536270180
Skorik Sergey <sskoryk@redhat.com>
2018-11-07 12:43:32 +0200

    copy failed selenium tests from Eclipse CHE

---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/dashboard/CreateAndDeleteProjectsTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.redhat.codeready.selenium.dashboard;
+
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.eclipse.che.selenium.pageobject.ProjectExplorer.FolderTypes.PROJECT_FOLDER;
+import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.JAVA;
+import static org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails.WorkspaceDetailsTab.PROJECTS;
+import static org.testng.Assert.fail;
+
+import com.google.inject.Inject;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.user.DefaultTestUser;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.core.workspace.TestWorkspaceProvider;
+import org.eclipse.che.selenium.pageobject.Ide;
+import org.eclipse.che.selenium.pageobject.MavenPluginStatusBar;
+import org.eclipse.che.selenium.pageobject.NotificationsPopupPanel;
+import org.eclipse.che.selenium.pageobject.ProjectExplorer;
+import org.eclipse.che.selenium.pageobject.ToastLoader;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace;
+import org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceProjects;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.openqa.selenium.TimeoutException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/** @author Andrey Chizhikov */
+public class CreateAndDeleteProjectsTest {
+
+  private static final String WORKSPACE = generate("workspace", 4);
+  private static final String KITCHENSINK_EXAMPLE = "kitchensink-example";
+  private static final String SECOND_KINTCHENSINK_PROJECT_NAME = KITCHENSINK_EXAMPLE + "-1";
+
+  @Inject private Dashboard dashboard;
+  @Inject private WorkspaceProjects workspaceProjects;
+  @Inject private WorkspaceDetails workspaceDetails;
+  @Inject private NewWorkspace newWorkspace;
+  @Inject private ProjectSourcePage projectSourcePage;
+  @Inject private ProjectExplorer explorer;
+  @Inject private SeleniumWebDriver seleniumWebDriver;
+  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
+  @Inject private TestWorkspaceServiceClient workspaceServiceClient;
+  @Inject private DefaultTestUser defaultTestUser;
+  @Inject private NotificationsPopupPanel notificationsPopupPanel;
+  @Inject private MavenPluginStatusBar mavenPluginStatusBar;
+  @Inject private Workspaces workspaces;
+  @Inject private Ide ide;
+  @Inject private ToastLoader toastLoader;
+  @Inject private TestWorkspaceProvider testWorkspaceProvider;
+
+  // it is used to read workspace logs on test failure
+  private TestWorkspace testWorkspace;
+
+  @BeforeClass
+  public void setUp() {
+    dashboard.open();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    workspaceServiceClient.delete(WORKSPACE, defaultTestUser.getName());
+  }
+
+  @Test
+  public void createAndDeleteProjectTest() {
+    dashboard.waitDashboardToolbarTitle();
+    dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.clickOnAddWorkspaceBtn();
+    newWorkspace.waitToolbar();
+
+    // we are selecting 'Java' stack from the 'All Stack' tab for compatibility with OSIO
+    newWorkspace.clickOnAllStacksTab();
+    newWorkspace.selectStack(JAVA);
+    newWorkspace.typeWorkspaceName(WORKSPACE);
+
+    // create 'web-java-spring' and 'console-java-simple' projects
+    projectSourcePage.clickOnAddOrImportProjectButton();
+    projectSourcePage.selectSample(KITCHENSINK_EXAMPLE);
+    projectSourcePage.clickOnAddProjectButton();
+
+    // create 'web-java-spring-1' project
+    projectSourcePage.clickOnAddOrImportProjectButton();
+    projectSourcePage.selectSample(KITCHENSINK_EXAMPLE);
+    projectSourcePage.clickOnAddProjectButton();
+
+    newWorkspace.clickOnCreateButtonAndOpenInIDE();
+    // store info about created workspace to make SeleniumTestHandler.captureTestWorkspaceLogs()
+    // possible to read logs in case of test failure
+    testWorkspace = testWorkspaceProvider.getWorkspace(WORKSPACE, defaultTestUser);
+
+    // switch to the IDE and wait for workspace is ready to use
+    String dashboardWindow = seleniumWebDriverHelper.switchToIdeFrameAndWaitAvailability();
+    toastLoader.waitToastLoaderAndClickStartButton();
+    ide.waitOpenedWorkspaceIsReadyToUse();
+
+    // wait for projects initializing
+    explorer.waitItem(KITCHENSINK_EXAMPLE);
+    explorer.waitItem(SECOND_KINTCHENSINK_PROJECT_NAME);
+    notificationsPopupPanel.waitPopupPanelsAreClosed();
+    mavenPluginStatusBar.waitClosingInfoPanel();
+    explorer.waitDefinedTypeOfFolder(KITCHENSINK_EXAMPLE, PROJECT_FOLDER);
+    notificationsPopupPanel.waitPopupPanelsAreClosed();
+
+    // delete projects from workspace details page
+    switchToWindow(dashboardWindow);
+    dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.selectWorkspaceItemName(WORKSPACE);
+    workspaceDetails.selectTabInWorkspaceMenu(PROJECTS);
+    workspaceProjects.waitProjectIsPresent(KITCHENSINK_EXAMPLE);
+
+    openProjectSettings(KITCHENSINK_EXAMPLE);
+    workspaceProjects.clickOnDeleteProject();
+    workspaceProjects.clickOnDeleteItInDialogWindow();
+    workspaceProjects.waitProjectIsNotPresent(KITCHENSINK_EXAMPLE);
+    workspaceProjects.waitProjectIsPresent(SECOND_KINTCHENSINK_PROJECT_NAME);
+  }
+
+  private void switchToWindow(String windowHandle) {
+    seleniumWebDriver.switchTo().window(windowHandle);
+  }
+
+  private void openProjectSettings(String projectName) {
+    try {
+      workspaceProjects.openSettingsForProjectByName(projectName);
+    } catch (TimeoutException ex) {
+      // remove try-catch block after issue has been resolved
+      fail("Known random failure https://github.com/eclipse/che/issues/8931");
+    }
+  }
+}
---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/dashboard/ImportMavenProjectFromGitTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.redhat.codeready.selenium.dashboard;
+
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.JAVA;
+import static org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage.Sources.GIT;
+
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.eclipse.che.selenium.core.client.TestGitHubRepository;
+import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.user.DefaultTestUser;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.core.workspace.TestWorkspaceProvider;
+import org.eclipse.che.selenium.pageobject.Ide;
+import org.eclipse.che.selenium.pageobject.ProjectExplorer;
+import org.eclipse.che.selenium.pageobject.ToastLoader;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace;
+import org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/** @author Andrey Chizhikov */
+public class ImportMavenProjectFromGitTest {
+
+  private final String WORKSPACE = generate("ImtMvnPrjGit", 4);
+  private String testProjectName;
+
+  @Inject private Dashboard dashboard;
+  @Inject private ProjectExplorer explorer;
+  @Inject private NewWorkspace newWorkspace;
+  @Inject private ProjectSourcePage projectSourcePage;
+  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
+  @Inject private TestWorkspaceServiceClient workspaceServiceClient;
+  @Inject private TestWorkspaceProvider testWorkspaceProvider;
+  @Inject private DefaultTestUser defaultTestUser;
+  @Inject private Workspaces workspaces;
+  @Inject private Ide ide;
+  @Inject private ToastLoader toastLoader;
+  @Inject private TestGitHubRepository testRepo;
+
+  // it is used to read workspace logs on test failure
+  private TestWorkspace testWorkspace;
+
+  @BeforeClass
+  public void setUp() throws IOException {
+    Path entryPath = Paths.get(getClass().getResource("/projects/guess-project").getPath());
+    testRepo.addContent(entryPath);
+
+    dashboard.open();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    workspaceServiceClient.delete(WORKSPACE, defaultTestUser.getName());
+  }
+
+  @Test
+  public void checkAbilityImportMavenProjectTest() {
+    testProjectName = testRepo.getName();
+
+    dashboard.waitDashboardToolbarTitle();
+    dashboard.selectWorkspacesItemOnDashboard();
+
+    workspaces.clickOnAddWorkspaceBtn();
+    newWorkspace.waitToolbar();
+
+    // we are selecting 'Java' stack from the 'All Stack' tab for compatibility with OSIO
+    newWorkspace.clickOnAllStacksTab();
+    newWorkspace.selectStack(JAVA);
+    newWorkspace.typeWorkspaceName(WORKSPACE);
+
+    projectSourcePage.clickOnAddOrImportProjectButton();
+
+    projectSourcePage.selectSourceTab(GIT);
+    projectSourcePage.typeGitRepositoryLocation(testRepo.getHtmlUrl());
+    projectSourcePage.clickOnAddProjectButton();
+
+    newWorkspace.clickOnCreateButtonAndOpenInIDE();
+    // store info about created workspace to make SeleniumTestHandler.captureTestWorkspaceLogs()
+    // possible to read logs in case of test failure
+    testWorkspace = testWorkspaceProvider.getWorkspace(WORKSPACE, defaultTestUser);
+
+    seleniumWebDriverHelper.switchToIdeFrameAndWaitAvailability();
+
+    toastLoader.waitToastLoaderAndClickStartButton();
+    ide.waitOpenedWorkspaceIsReadyToUse();
+    explorer.waitItem(testProjectName);
+  }
+}
