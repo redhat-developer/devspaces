40ba12356036dbc45355c38c12bf0e9343582aff
nickboldt <nboldt@redhat.com>
2018-11-15 04:08:30 -0500

    merge changes in che build-ncl.sh to crw so we can use one script for both builds
    Change-Id: I43beff0803d3734d62307a88239c69bddebef34e
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/build-ncl.sh
+++ product/build-ncl.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -xe
 # script to build eclipse-che in #projectncl
 
+# TODO NOS-1485 build this in NCL directly
+# don't build dashboard from source; include from upstream binary in http://oss.sonatype.org/content/repositories/snapshots/
+includeDashboardFromSource=0
+
 ##########################################################################################
 # enable support for CI builds
 ##########################################################################################
@@ -11,6 +15,8 @@ version=6.14.0
 suffix="" # normally we compute this from version of org/eclipse/che/depmgt/maven-depmgt-pom but can override if needed
 upstreamPom=org/eclipse/che/depmgt/maven-depmgt-pom # usually use depmgt/maven-depmgt-pom but can also align to che-parent for codeready-workspaces build
 INDY=""
+doSedReplacements=1
+doDashboardVersionLookup=1
 
 # read commandline args
 while [[ "$#" -gt 0 ]]; do
@@ -22,6 +28,8 @@ while [[ "$#" -gt 0 ]]; do
     '-PROFILES') PROFILES="$2"; shift 1;; # override default profiles
     '-MVNFLAGS') MVNFLAGS="$2"; shift 1;; # add more mvn flags
     '-INDY') INDY="$2"; shift 1;; # override for default INDY URL
+    '-ns') doSedReplacements=0; shift 0;; # don't do sed replacements (testing NCL-4195)
+    '-ndbvl') doDashboardVersionLookup=0; shift 0;; # don't check for a version of the dashboard, just use what's given (testing NCL-4195)s
     *) OTHER="${OTHER} $1"; shift 0;; 
   esac
   shift 1
@@ -62,7 +70,7 @@ fi
 # persistent: 6.12.0.redhat-00001-ec28abe6
 # pmeVersionSHA=$(git describe --tags)
 # pmeSuffix=${pmeVersion#${version}.}; echo $suffix
-if [[ ${suffix} ]]; then
+if [[ ${suffix} ]] && [[ ${doSedReplacements} -gt 0 ]]; then
   for d in $(find . -name pom.xml); do sed -i "s#\(version>\)${version}.*\(</version>\)#\1${version}.${suffix}\2#g" $d; done
   for d in $(find . -name pom.xml); do sed -i "s#\(<che.\+version>\)${version}.*\(</che.\+version>\)#\1${version}.${suffix}\2#g" $d; done
   for d in $(find . -name pom.xml); do sed -i "s#\(<version>${version}\)-SNAPSHOT#\1.${suffix}#g" $d; done # may not be needed 
@@ -92,6 +100,7 @@ export npmDownloadRoot=http://registry.npmjs.org:80/npm/-/
 export npmRegistryURL=http://registry.npmjs.org:80/
 export YARN_REGISTRY=http://registry.yarnpkg.com:80/
 
+npm config set strict-ssl=false
 npm config set https-proxy ${NCL_PROXY}
 npm config set https_proxy ${NCL_PROXY}
 npm config set proxy ${NCL_PROXY}
@@ -102,30 +111,35 @@ npm config set maxsockets 80
 npm config set fetch-retries 10
 npm config set fetch-retry-mintimeout 60000
 npm config set registry ${npmRegistryURL}
-# npm config list
+npm config list
 
-##########################################################################################
-# get dashboard version from Indy - works but can't be resolved during the build?
-##########################################################################################
+if [[ $includeDashboardFromSource -gt 0 ]]; then
+  # workaround for lack of https support and inability to see github.com as a result
+  mkdir -p /tmp/phantomjs/
+  pushd /tmp/phantomjs/
+    # previously mirrored from https://github.com/Medium/phantomjs/releases/download/v2.1.1/phantomjs-2.1.1-linux-x86_64.tar.bz2
+    time wget -q http://download.jboss.org/jbosstools/requirements/codeready-workspaces/node/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  popd
 
-#tmpfile=/tmp/maven-metadata-${version}.html
-#upstreamPom=org/eclipse/che/dashboard/che-dashboard-war/${includeDashboardVersion}
-#UPSTREAM_POM="api/content/maven/group/builds-untested+shared-imports+public/${upstreamPom}/maven-metadata.xml"
+  pushd dashboard
+    time npm install phantomjs-prebuilt
+    export PATH=${PATH}:`pwd`/node_modules/phantomjs-prebuilt/bin
 
-#wget ${INDY}/${UPSTREAM_POM} -O ${tmpfile}
-#timestamp=$(grep "<timestamp>" ${tmpfile} | sed -e "s#.*<timestamp>\(.\+\)</timestamp>.*#\1#"); echo $timestamp
-#buildNumber=$(grep "<buildNumber>" ${tmpfile} | sed -e "s#.*<buildNumber>\(.\+\)</buildNumber>.*#\1#"); echo $buildNumber
-#cheDashboardVersion=${version}-${timestamp}-${buildNumber} && rm -f ${tmpfile}
-#if [[ ! ${cheDashboardVersion} ]]; then cheDashboardVersion=${includeDashboardVersion}; fi # fallback to 6.13.0-SNAPSHOT if not resolved
+    time npm install yarn
+    PATH=${PATH}:`pwd`/node_modules/yarn/bin
+    yarn config set registry ${YARN_REGISTRY} --global
+    yarn config set YARN_REGISTRY ${YARN_REGISTRY} --global
 
-##########################################################################################
-# get dashboard version from Sonatype - works but requires PME flag -DrepoReportingRemoval=false to resolve Sonatype Nexus
-##########################################################################################
+    yarn config set proxy ${NCL_PROXY} --global
+    yarn config set yarn-proxy ${NCL_PROXY} --global
+    yarn config set yarn_proxy ${NCL_PROXY} --global
 
-if [[ ${includeDashboardVersion} == *"-SNAPSHOT" ]]; then snapOrRel="snapshots"; else snapOrRel="releases"; fi # echo $snapOrRel
-wget -q http://oss.sonatype.org/content/repositories/${snapOrRel}/org/eclipse/che/dashboard/che-dashboard-war/${includeDashboardVersion}/maven-metadata.xml -O /tmp/mm.xml
-cheDashboardVersion=$(grep value /tmp/mm.xml | tail -1 | sed -e "s#.*<value>\(.\+\)</value>#\1#" && rm -f /tmp/mm.xml)
-if [[ ! ${cheDashboardVersion} ]]; then cheDashboardVersion=${includeDashboardVersion}; fi # fallback to 6.13.0-SNAPSHOT if not resolved
+    yarn config set https-proxy false --global
+    yarn config set https_proxy false --global
+    yarn config list
+    yarn install --frozen-lockfile --no-lockfile --pure-lockfile --ignore-optional --non-interactive --production=false
+  popd
+fi
 
 ##########################################################################################
 # configure maven build 
@@ -139,7 +153,20 @@ MVNFLAGS="${MVNFLAGS} -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss "
 MVNFLAGS="${MVNFLAGS} -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
 MVNFLAGS="${MVNFLAGS} -DnodeDownloadRoot=${nodeDownloadRoot} -DnpmDownloadRoot=${npmDownloadRoot}"
 MVNFLAGS="${MVNFLAGS} -DnpmRegistryURL=${npmRegistryURL}"
-MVNFLAGS="${MVNFLAGS} -Dche.dashboard.version=${cheDashboardVersion}"
+MVNFLAGS="${MVNFLAGS} -DYARN_REGISTRY=${YARN_REGISTRY}"
+
+##########################################################################################
+# get dashboard version from Sonatype - works but requires PME flag -DrepoReportingRemoval=false to resolve Sonatype Nexus
+##########################################################################################
+
+if [[ $includeDashboardVersion ]]; then
+  if [[ ${includeDashboardVersion} == *"-SNAPSHOT" ]] && [[ ${doDashboardVersionLookup} -gt 0 ]]; then 
+    wget --server-response http://oss.sonatype.org/content/repositories/snapshots/org/eclipse/che/dashboard/che-dashboard-war/${includeDashboardVersion}/maven-metadata.xml -O /tmp/mm.xml
+    cheDashboardVersion=$(grep value /tmp/mm.xml | tail -1 | sed -e "s#.*<value>\(.\+\)</value>#\1#" && rm -f /tmp/mm.xml)
+  fi
+  if [[ ! ${cheDashboardVersion} ]]; then cheDashboardVersion=${includeDashboardVersion}; fi # fallback to 6.13.0-SNAPSHOT if not resolved
+  MVNFLAGS="${MVNFLAGS} -Dche.dashboard.version=${cheDashboardVersion}"
+fi
 
 ##########################################################################################
 # run maven build 
