9a31bab673732945928d2c4a67e410345006643c
Skorik Sergey <sskoryk@redhat.com>
2018-11-19 12:57:36 +0200

    add copyrights to adapted tests

--- test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/dashboard/workspaces/details/WorkspaceDetailsMachinesRamTest.java
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/dashboard/workspaces/details/WorkspaceDetailsMachinesRamTest.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2018 Red Hat, Inc.
+* Copyright (c) 2018 Red Hat, Inc.
 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Red Hat, Inc. - initial API and implementation
- */
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
 package com.redhat.codeready.selenium.dashboard.workspaces.details;
 
 import static org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails.ActionButton.APPLY_BUTTON;
--- test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/miscellaneous/MachinesAsynchronousStartTest.java
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/miscellaneous/MachinesAsynchronousStartTest.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2018 Red Hat, Inc.
+* Copyright (c) 2018 Red Hat, Inc.
 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Red Hat, Inc. - initial API and implementation
- */
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
 package com.redhat.codeready.selenium.miscellaneous;
 
 import static java.lang.String.format;
