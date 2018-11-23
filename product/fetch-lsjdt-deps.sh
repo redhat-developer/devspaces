40ac2ad1090b525f8bb9291d51e523cd9adf70c0
nickboldt <nboldt@redhat.com>
2018-11-23 13:13:33 -0500

    oops, wrong https
    Change-Id: Ia2fa6f339a6598f3319d6f6f96fe8cf3227ce84b
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/fetch-lsjdt-deps.sh
+++ product/fetch-lsjdt-deps.sh
@@ -4,13 +4,13 @@
 lsjdtVersion="0.0.2-SNAPSHOT" # don't use LATEST, it's unpredictable
 if [[ $1 ]]; then lsjdtVersion=$1; fi
 
-export NCL_PROXY="https://${buildContentId}+tracking:${accessToken}@${proxyServer}:${proxyPort}"
+export NCL_PROXY="http://${buildContentId}+tracking:${accessToken}@${proxyServer}:${proxyPort}"
 # wget proxies
 export http_proxy="${NCL_PROXY}"
 export https_proxy="${NCL_PROXY}"
 
 groupId=org.eclipse.che.ls.jdt
-remoteRepositories=http://oss.sonatype.org/content/repositories/snapshots/
+remoteRepositories=https://oss.sonatype.org/content/repositories/snapshots/
 #tmpRepo=/tmp/m2-repo-temp
 tmpRepo=${HOME}/.m2/repository
 pushd /tmp
