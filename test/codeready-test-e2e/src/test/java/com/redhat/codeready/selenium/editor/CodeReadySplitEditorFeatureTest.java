74b8ff420c96586bf17ccaf268b833da8e5ba615
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-12-29 15:58:45 +0200

    Modify the tests in the 'editor' package to get rid duplicating

---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/editor/CodeReadySplitEditorFeatureTest.java
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2018 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.editor;
+
+import org.eclipse.che.selenium.editor.SplitEditorFeatureTest;
+
+/** @author Aleksandr Shmaraiev */
+public class CodeReadySplitEditorFeatureTest extends SplitEditorFeatureTest {
+
+  protected String getJavaFileNameFromTabTitle() {
+    return "MemberRegistration";
+  }
+
+  @Override
+  protected void expandProjectExplorerAndOpenFile() {
+    String javaFileName = getJavaFileNameFromTabTitle() + ".java";
+    String pathTopackage =
+        PROJECT_NAME + "/src/main/java/org.jboss.as.quickstarts.kitchensink/controller";
+
+    projectExplorer.expandPathInProjectExplorerAndOpenFile(pathTopackage, javaFileName);
+    editor.waitTabIsPresent(getJavaFileNameFromTabTitle());
+    editor.waitTabSelection(0, getJavaFileNameFromTabTitle());
+    editor.waitActive();
+  }
+
+  @Override
+  protected void waitAndSelectItem() {
+    String pathToJavaFile =
+        PROJECT_NAME
+            + "/src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberRegistration.java";
+    projectExplorer.waitAndSelectItem(pathToJavaFile);
+  }
+
+  @Override
+  protected void selectSample() {
+    String sampleName = "kitchensink-example";
+    wizard.selectSample(sampleName);
+  }
+}
---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/editor/autocomplete/CodeReadyAutocompleteProposalJavaDocTest.java
@@ -0,0 +1,62 @@
+/*
+* Copyright (c) 2018 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.editor.autocomplete;
+
+import org.eclipse.che.selenium.editor.autocomplete.AutocompleteProposalJavaDocTest;
+
+public class CodeReadyAutocompleteProposalJavaDocTest extends AutocompleteProposalJavaDocTest {
+
+  protected String getExpectedJavadocHtmlText() {
+    return "<p>Returns concatination of two strings into one divided by special symbol.</p>\n"
+        + "<ul>\n"
+        + "<li><p><strong>Parameters:</strong></p>\n"
+        + "<ul>\n"
+        + "<li><p><strong>part1</strong> part 1 to concat.</p>\n"
+        + "</li>\n"
+        + "<li><p><strong>part2</strong> part 2 to concat.</p>\n"
+        + "</li>\n"
+        + "<li><p><strong>divider</strong> divider of part1 and part2.</p>\n"
+        + "</li>\n"
+        + "</ul>\n"
+        + "</li>\n"
+        + "<li><p><strong>Returns:</strong></p>\n"
+        + "<ul>\n"
+        + "<li>concatination of two strings into one.</li>\n"
+        + "</ul>\n"
+        + "</li>\n"
+        + "<li><p><strong>Throws:</strong></p>\n"
+        + "<ul>\n";
+  }
+
+  @Override
+  protected String getProposalDocumentationHTML() {
+    return "<p>Returns <code>true</code> if the argument is equal to instance. otherwise <code>false</code></p>\n"
+        + "<ul>\n"
+        + "<li><p><strong>Parameters:</strong></p>\n"
+        + "<ul>\n"
+        + "<li><strong>o</strong> an object.</li>\n"
+        + "</ul>\n"
+        + "</li>\n"
+        + "<li><p><strong>Returns:</strong></p>\n"
+        + "<ul>\n"
+        + "<li>Returns <code>true</code> if the argument is equal to instance. otherwise <code>false</code></li>\n"
+        + "</ul>\n"
+        + "</li>\n"
+        + "<li><p><strong>Since:</strong></p>\n"
+        + "<ul>\n"
+        + "<li>1.0</li>\n"
+        + "</ul>\n"
+        + "</li>\n"
+        + "<li><p><strong>See Also:</strong></p>\n"
+        + "<ul>\n";
+  }
+}
