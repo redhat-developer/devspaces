3f06bacb9ecacf67de0314aa323f4d4d34a775f3
Dmytro Nochevnov <dnochevn@redhat.com>
2019-01-02 20:52:06 +0100

    Get read of duplication in Keycloak CLI related code
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- test/codeready-test-e2e/e2e-tests.sh
+++ test/codeready-test-e2e/e2e-tests.sh
@@ -127,6 +127,14 @@ done
 
 export CHE_INFRASTRUCTURE=openshift
 
+export KEYCLOAK_OPENSHIFT_APP=keycloak
+export KEYCLOAK_CLI_INTERNAL_PATH="/opt/eap/bin/kcadm.sh"
+
+export CHE_KEYCLOAK_REALM=codeready
+export CHE_OPENSHIFT_POD=che
+
+export CHE_OPENSHIFT_PROJECT=${CHE_OPENSHIFT_PROJECT:-"codeready"}
+
 mvn $CLEAN_GOAL dependency:unpack-dependencies \
     -DincludeArtifactIds=che-selenium-core \
     -DincludeGroupIds=org.eclipse.che.selenium \
