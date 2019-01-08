8f21e92f2139e9553b0787431f46cb6d59f15adf
Aleksandr Shmaraiev <oshmarai@redhat.com>
2019-01-08 11:20:01 +0200

    Test E2E: Modify the tests in the 'languageserver' package to get rid duplicating (#75)
    * Delete selenium tests which are duplicate from Eclipse Che
    * Create specified selenium tests to CRW
    * Modify the test suite 'CheSuite'

---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/languageserver/CodeReadyGolangFileEditingTest.java
@@ -0,0 +1,24 @@
+/*
+* Copyright (c) 2018 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.languageserver;
+
+import org.eclipse.che.selenium.languageserver.GolangFileEditingTest;
+
+/** @author Aleksandr Shmaraev */
+public class CodeReadyGolangFileEditingTest extends GolangFileEditingTest {
+
+  @Override
+  protected void waitExpectedTextIntoConsole() {
+    String lsInitMessage = "Finished running tool: /usr/bin/go build";
+    consoles.waitExpectedTextIntoConsole(lsInitMessage);
+  }
+}
---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/projectexplorer/CodeReadyCheckShowHideHiddenFilesTest.java
@@ -0,0 +1,22 @@
+/*
+* Copyright (c) 2018 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.projectexplorer;
+
+import org.eclipse.che.selenium.projectexplorer.CheckShowHideHiddenFilesTest;
+
+public class CodeReadyCheckShowHideHiddenFilesTest extends CheckShowHideHiddenFilesTest {
+
+  @Override
+  protected void selectSampleProject() {
+    projectWizard.selectSample("kitchensink-example");
+  }
+}
