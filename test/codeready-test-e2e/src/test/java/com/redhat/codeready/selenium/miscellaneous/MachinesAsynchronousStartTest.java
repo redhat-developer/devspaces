8ddd917080ebd4b512f3065e7544d44e3ae3d1dc
Dmytro Nochevnov <dnochevn@redhat.com>
2019-02-10 21:02:39 +0100

    Test CRW using stack images from 'registry.access.stage.redhat.com'
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/miscellaneous/MachinesAsynchronousStartTest.java
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/miscellaneous/MachinesAsynchronousStartTest.java
@@ -46,7 +46,7 @@ public class MachinesAsynchronousStartTest {
   private static final String WORKSPACE_NAME = generate("test-workspace", 4);
   private static final String MACHINE_NAME = "dev-machine";
   private static final String IMAGE_NAME =
-      "registry.access.redhat.com/codeready-workspaces/stacks-java";
+      "registry.access.stage.redhat.com/codeready-workspaces/stacks-java";
   private static final String IMAGE_NAME_SUFFIX = generate("", 4);
   private static final String NOT_EXISTED_IMAGE_NAME = IMAGE_NAME + IMAGE_NAME_SUFFIX;
   private static final String SUCCESS_NOTIFICATION_TEST = "Workspace updated.";
