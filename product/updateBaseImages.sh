05ff956d6f7e58723ae54d22961202bfd80f2d74
nickboldt <nboldt@redhat.com>
2019-02-25 17:38:20 -0500

    add support for branch, maxdepth query, and quieter output from docker-ls; als scrub off registry.access.redhat.com/ prefix when parsing Dockerfiles in GH
    Change-Id: Iee756551538acf47b5f4b6e71e2a8ae695e32ea6
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/updateBaseImages.sh
+++ product/updateBaseImages.sh
@@ -6,23 +6,27 @@
 # thankfully, the https://registry.access.redhat.com is v2 and does not require authentication to query
 
 WORKDIR=`pwd`
+BRANCH=master
+maxdepth=2
 buildCommand="echo \"No build triggered: use -c for a container-build (or -s for scratch).\""
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-w') WORKDIR="$2"; shift 1;;
-    '-c') buildCommand="rhpkg container-build"; shift 0;; 
-    '-s') buildCommand="rhpkg container-build --scratch"; shift 0;; 
+    '-b') BRANCH="$2"; shift 1;;
+    '-maxdepth') maxdepth="$2"; shift 1;;
+    '-c') buildCommand="rhpkg container-build"; shift 0;;
+    '-s') buildCommand="rhpkg container-build --scratch"; shift 0;;
     *) OTHER="${OTHER} $1"; shift 0;; 
   esac
   shift 1
 done
 
 pushedIn=0
-for d in $(find ${WORKDIR} -name Dockerfile); do 
+for d in $(find ${WORKDIR} -maxdepth ${maxdepth} -name Dockerfile); do
 	if [[ -f ${d} ]]; then
 		# pull latest commits
 		if [[ -d ${d%%/Dockerfile} ]]; then pushd ${d%%/Dockerfile} >/dev/null; pushedIn=1; fi
-		git checkout . && git pull -q
+		git checkout ${BRANCH} && git pull -q
 		if [[ ${pushedIn} -eq 1 ]]; then popd >/dev/null; pushedIn=0; fi
 
 		QUERY=""
@@ -32,12 +36,13 @@ for d in $(find ${WORKDIR} -name Dockerfile); do
 		# echo "# Dockerfile = $d "
 		URLs=$(cat $d | grep FROM -B1);
 		for URL in $URLs; do
+			URL=${URL#registry.access.redhat.com/}
 			# echo "URL=$URL"
 			if [[ $URL == "https"* ]]; then 
 				QUERY="$(echo $URL | sed -e "s#.\+registry.access.redhat.com/#docker run docker-ls docker-ls tags --registry https://registry.access.redhat.com #g" | tr '\n' ' ')"
 				echo "# $QUERY|grep \"^-\"|egrep -v \"\\\"|latest\"|sort -V|tail -5"
 				FROMPREFIX=$(echo $URL | sed -e "s#.\+registry.access.redhat.com/##g")
-				LATESTTAG=$(${QUERY}|grep "^-"|egrep -v "\"|latest"|sed -e "s#^-##" -e "s#[\n\r\ ]\+##g"|sort -V|tail -1)
+				LATESTTAG=$(${QUERY} 2>/dev/null|grep "^-"|egrep -v "\"|latest"|sed -e "s#^-##" -e "s#[\n\r\ ]\+##g"|sort -V|tail -1)
 				echo "+ ${FROMPREFIX}:${LATESTTAG}"
 			elif [[ $URL ]] && [[ $URL == "${FROMPREFIX}:"* ]]; then
 				if [[ ${LATESTTAG} ]] && [[ "${URL}" != "${FROMPREFIX}:${LATESTTAG}" ]]; then # fix the Dockerfile
