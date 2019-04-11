e166a439019f481b041780358f1ce1b5fcc31bbc
nickboldt <nboldt@redhat.com>
2019-04-11 13:16:46 -0400

    add crw flag; rename -u to -c; add examples and usage
    Change-Id: I8c3db9a5110e00626ab826ca8d2f9d97b1e7855d
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/checkLatestImages.sh
+++ product/checkLatestImages.sh
@@ -1,11 +1,50 @@
 #!/bin/bash -e
 #
-# script to query latest tags of the FROM repos, and update Dockerfiles using the latest base images
+# script to query latest tags for a given list of imags in RHCC
 # requires docker-ls container to be built locally -- see https://github.com/mayflower/docker-ls
 # 
 # thankfully, the https://registry.access.redhat.com is v2 and does not require authentication to query
 
-if [[ $(docker run docker-ls docker-ls 2>&1) == *"Unable to find image"* ]]; then 
+# default list of CRW containers to query
+CRW_CONTAINERS="\
+codeready-workspaces/server codeready-workspaces/server-operator \
+codeready-workspaces/stacks-java codeready-workspaces/stacks-node \
+codeready-workspaces/stacks-cpp codeready-workspaces/stacks-dotnet codeready-workspaces/stacks-golang \
+codeready-workspaces/stacks-php codeready-workspaces/stacks-python \
+codeready-workspaces-beta/stacks-java-rhel8 \
+"
+
+# regex pattern of container versions/names to exclude, eg., Beta1 (because version sort thinks 1.0.0.Beta1 > 1.0-12)
+EXCLUDES="\^" 
+
+# less output - omit container tag URLs
+QUIET=0
+
+usage () {
+	echo "
+Usage: 
+  $0 -crw                                                           | use default list of CRW images in RHCC
+  $0 -c \"rhoar-nodejs/nodejs-10 jboss-eap-7/eap72-openshift ...\"    | use specific list of RHCC images
+"
+	exit
+}
+if [[ $# -lt 1 ]]; then usage; fi
+
+CONTAINERS=""
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-crw') CONTAINERS="${CRW_CONTAINERS}"; EXCLUDES="Beta1"; shift 0;;
+    '-c') CONTAINERS="$2"; shift 1;;
+    '-x') EXCLUDES="$2"; shift 1;;
+    '-q') QUIET=1; shift 0;;
+    '-h') usage;;
+  esac
+  shift 1
+done
+
+if [[ ${CONTAINERS} == "" ]]; then usage; fi
+
+if [[ $(docker run docker-ls docker-ls 2>&1) == *"Unable to find image"* ]]; then
 	echo "Installing docker-ls ..."
 	rm -fr /tmp/docker-ls
 	pushd /tmp >/dev/null
@@ -14,29 +53,8 @@ if [[ $(docker run docker-ls docker-ls 2>&1) == *"Unable to find image"* ]]; the
 	popd >/dev/null
 fi
 
-URLs="\
-codeready-workspaces/server codeready-workspaces/server-operator \
-codeready-workspaces/stacks-java codeready-workspaces/stacks-node \
-codeready-workspaces/stacks-cpp codeready-workspaces/stacks-dotnet codeready-workspaces/stacks-golang \
-codeready-workspaces/stacks-php codeready-workspaces/stacks-python \
-codeready-workspaces-beta/stacks-java-rhel8 \
-"
-EXCLUDES="Beta1" # regex pattern of container versions/names to exclude, eg., Beta1 (because version sort thinks 1.0.0.Beta1 > 1.0-12)
-WORKDIR=`pwd`
-QUIET=0
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    '-w') WORKDIR="$2"; shift 1;;
-    '-u') URLs="$2"; shift 1;;
-    '-x') EXCLUDES="$2"; shift 1;;
-    '-q') QUIET=1; shift 0;;
-    *) OTHER="${OTHER} $1"; shift 0;; 
-  esac
-  shift 1
-done
-
 echo ""
-for URLfrag in $URLs; do
+for URLfrag in $CONTAINERS; do
 	URL="https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/${URLfrag}"
 	# echo "URL=$URL"
 	QUERY="$(echo $URL | sed -e "s#.\+registry.access.redhat.com/#docker run docker-ls docker-ls tags --registry https://registry.access.redhat.com #g" | tr '\n' ' ')"
