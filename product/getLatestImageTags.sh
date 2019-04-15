16bece42beebf753cbc4a29e8eeb04ecfd1838cd
nickboldt <nboldt@redhat.com>
2019-04-15 17:23:33 -0400

    install docker-ls if not present; add support for pulp queries; add support for showing more than one tag per container; add support for filtering by partial tag (8.0 vs. 8.1); tweak verbose/quiet settings
    Change-Id: Ic59389e9e24e5cc03c2e05b310933828fc45f877
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/getLatestImageTags.sh
+++ product/getLatestImageTags.sh
@@ -5,6 +5,15 @@
 # 
 # thankfully, the https://registry.access.redhat.com is v2 and does not require authentication to query
 
+if [[ $(docker run docker-ls docker-ls 2>&1) == *"Unable to find image"* ]]; then 
+	echo "Installing docker-ls ..."
+	rm -fr /tmp/docker-ls
+	pushd /tmp >/dev/null
+	git clone -q --depth=1 https://github.com/mayflower/docker-ls && cd docker-ls && docker build -t docker-ls .
+	rm -fr /tmp/docker-ls
+	popd >/dev/null
+fi
+
 # default list of CRW containers to query
 CRW_CONTAINERS="\
 codeready-workspaces/server codeready-workspaces/server-operator \
@@ -19,24 +28,32 @@ EXCLUDES="\^"
 
 # less output - omit container tag URLs
 QUIET=0
+VERBOSE=0
+NUMTAGS=1
 
 usage () {
 	echo "
 Usage: 
-  $0 -crw                                                           | use default list of CRW images in RHCC
-  $0 -c \"rhoar-nodejs/nodejs-10 jboss-eap-7/eap72-openshift ...\"    | use specific list of RHCC images
+  $0 -crw                                                       | use default list of CRW images in RHCC
+  $0 -c \"rhoar-nodejs/nodejs-10 jboss-eap-7/eap72-openshift\"    | use specific list of RHCC images
+  $0 -c ubi7 -c ubi8:8.0 --pulp -n 5                            | check pulp registry; show 8.0* tags; show 5 tags per container
 "
 	exit
 }
 if [[ $# -lt 1 ]]; then usage; fi
 
+REGISTRY="https://registry.access.redhat.com" # or "http://brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 CONTAINERS=""
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-crw') CONTAINERS="${CRW_CONTAINERS}"; EXCLUDES="Beta1"; shift 0;;
-    '-c') CONTAINERS="$2"; shift 1;;
+    '-c') CONTAINERS="${CONTAINERS} $2"; shift 1;;
     '-x') EXCLUDES="$2"; shift 1;;
     '-q') QUIET=1; shift 0;;
+    '-v') QUIET=0; VERBOSE=1; shift 0;;
+    '-r') REGISTRY="$2"; shift 1;;
+    '-p'|'--pulp') REGISTRY="http://brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"; EXCLUDES="candidate|guest|containers"; VERBOSE=0; QUIET=1; shift 0;;
+    '-n') NUMTAGS="$2"; shift 1;;
     '-h') usage;;
   esac
   shift 1
@@ -55,15 +72,30 @@ fi
 
 echo ""
 for URLfrag in $CONTAINERS; do
-	URL="https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/${URLfrag}"
-	# echo "URL=$URL"
-	QUERY="$(echo $URL | sed -e "s#.\+registry.access.redhat.com/#docker run docker-ls docker-ls tags --registry https://registry.access.redhat.com #g" | tr '\n' ' ')"
-	# echo ""; echo "# $QUERY|grep \"^-\"|egrep -v \"\\\"|latest\"|egrep -v "${EXCLUDES}"|sort -V|tail -5"
-	LATESTTAG=$(${QUERY} 2>/dev/null|grep "^-"|egrep -v "\"|latest"|egrep -v "${EXCLUDES}"|sed -e "s#^-##" -e "s#[\n\r\ ]\+##g"|sort -V|tail -1)
-	if [[ $QUIET -eq 1 ]]; then
-		echo "${URLfrag}:${LATESTTAG}"
+	URLfragtag=${URLfrag##*:}
+	if [[ ${URLfragtag} == ${URLfrag} ]]; then # tag appended on url
+		URL="https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/${URLfrag}"
+		URLfragtag="^-"
 	else
-		echo "* ${URLfrag}:${LATESTTAG} :: https://access.redhat.com/containers/#/registry.access.redhat.com/${URLfrag}/images/${LATESTTAG}"
+		URL="https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/${URLfrag%%:*}"
+		URLfragtag="^- ${URLfragtag}"
 	fi
+	# echo "URL=$URL"
+	QUERY="$(echo $URL | sed -e "s#.\+registry.access.redhat.com/#docker run docker-ls docker-ls tags --allow-insecure --registry ${REGISTRY} #g" | tr '\n' ' ')"
+	if [[ $VERBOSE -eq 1 ]]; then 
+		echo ""; echo "# $QUERY|grep \"${URLfragtag}\"|egrep -v \"\\\"|latest\"|egrep -v \"${EXCLUDES}\"|sort -V|tail"; 
+	fi
+	LATESTTAGs=$(${QUERY} 2>/dev/null|grep "${URLfragtag}"|egrep -v "\"|latest"|egrep -v "${EXCLUDES}"|sed -e "s#^-##" -e "s#[\n\r\ ]\+##g"|sort -V|tail -${NUMTAGS})
+	for LATESTTAG in ${LATESTTAGs}; do
+		if [[ $REGISTRY = *"registry.access.redhat.com"* ]]; then
+			if [[ $QUIET -eq 1 ]]; then
+				echo "${URLfrag}:${LATESTTAG}"
+			else
+				echo "* ${URLfrag}:${LATESTTAG} :: https://access.redhat.com/containers/#/registry.access.redhat.com/${URLfrag}/images/${LATESTTAG}"
+			fi
+		else
+			echo "${REGISTRY##*://}/${URLfrag}:${LATESTTAG}"
+		fi
+	done
 done
 echo ""
