459cd7e8aeed8f9761e3f41c9fcd8adf9ffb92b0
Dmytro Nochevnov <dnochevn@redhat.com>
2019-05-06 18:32:44 +0300

    Fix Node10UserStoryTest.java
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/userstory/Node10UserStoryTest.java
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/userstory/Node10UserStoryTest.java
@@ -16,6 +16,7 @@ import static com.redhat.codeready.selenium.pageobject.dashboard.CodereadyNewWor
 import com.redhat.codeready.selenium.pageobject.dashboard.CodereadyNewWorkspace;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import org.testng.annotations.Test;
 
 public class Node10UserStoryTest extends Node8UserStoryTest {
   public Node10UserStoryTest() throws IOException, URISyntaxException {
@@ -25,5 +26,25 @@ public class Node10UserStoryTest extends Node8UserStoryTest {
   @Override
   protected CodereadyNewWorkspace.CodereadyStacks getStackName() {
     return NODE10;
+  }
+
+  @Test
+  public void createWorkspaceFromDashboard() throws Exception {
+    super.createWorkspaceFromDashboard();
+  }
+
+  @Test(priority = 1)
+  public void runAndCheckNodeJsApp() throws Exception {
+    super.runAndCheckNodeJsApp();
+  }
+
+  @Test(priority = 2)
+  public void checkMainLsFeatures() {
+    super.checkMainLsFeatures();
+  }
+
+  @Test(priority = 3)
+  public void checkBayesianLsErrorMarker() throws Exception {
+    super.checkBayesianLsErrorMarker();
   }
 }
