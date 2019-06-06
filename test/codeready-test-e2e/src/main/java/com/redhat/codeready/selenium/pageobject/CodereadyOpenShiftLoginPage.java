9eedc36ad681205537c95818a5ad26a7f0275f8d
Sergey Skorik <sskoryk@redhat.com>
2019-06-06 12:12:53 +0300

    Selenium: create selenium tests that check log into Codeready by Openshift oAuth (#160)

---
+++ test/codeready-test-e2e/src/main/java/com/redhat/codeready/selenium/pageobject/CodereadyOpenShiftLoginPage.java
@@ -0,0 +1,132 @@
+/*
+* Copyright (c) 2019 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-2.0
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.pageobject;
+
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.APPROVE_BUTTON_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.CONFIRM_PASSWORD_INPUT_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.EMAIL_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.FIRST_NAME_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.LAST_NAME_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.LOGIN_BUTTON_XPATH;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.LOGIN_TITLE_ID;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.PASSWORD_INPUT_NAME;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.REGISTER_LINK_XPATH;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.SUBMIT_BUTTON_XPATH;
+import static com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage.Locators.USERNAME_INPUT_NAME;
+import static java.util.Arrays.asList;
+import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.WIDGET_TIMEOUT_SEC;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.pageobject.ocp.OpenShiftLoginPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+@Singleton
+public class CodereadyOpenShiftLoginPage extends OpenShiftLoginPage {
+  private final SeleniumWebDriverHelper seleniumWebDriverHelper;
+
+  protected interface Locators {
+    String FIRST_NAME_NAME = "firstName";
+    String LAST_NAME_NAME = "lastName";
+    String EMAIL_NAME = "email";
+    String USERNAME_INPUT_NAME = "username";
+    String PASSWORD_INPUT_NAME = "password";
+    String CONFIRM_PASSWORD_INPUT_NAME = "password-confirm";
+    String LOGIN_TITLE_ID = "brand";
+    String LOGIN_BUTTON_XPATH = "//button[text()='Log In']";
+    String REGISTER_LINK_XPATH = "//a[text()='Register']";
+    String SUBMIT_BUTTON_XPATH = "//input[@value='Submit']";
+    String APPROVE_BUTTON_NAME = "approve";
+  }
+
+  @FindBy(name = FIRST_NAME_NAME)
+  private WebElement firstUsername;
+
+  @FindBy(name = LAST_NAME_NAME)
+  private WebElement lastUsername;
+
+  @FindBy(name = EMAIL_NAME)
+  private WebElement emailName;
+
+  @FindBy(name = USERNAME_INPUT_NAME)
+  private WebElement usernameInput;
+
+  @FindBy(name = PASSWORD_INPUT_NAME)
+  private WebElement passwordInput;
+
+  @FindBy(xpath = LOGIN_BUTTON_XPATH)
+  private WebElement loginButton;
+
+  @FindBy(name = CONFIRM_PASSWORD_INPUT_NAME)
+  private WebElement confirmPasswordInput;
+
+  @FindBy(xpath = REGISTER_LINK_XPATH)
+  private WebElement registerLink;
+
+  @FindBy(name = APPROVE_BUTTON_NAME)
+  private WebElement approveButton;
+
+  @FindBy(id = LOGIN_TITLE_ID)
+  private WebElement loginTitle;
+
+  @FindBy(xpath = SUBMIT_BUTTON_XPATH)
+  private WebElement submitButton;
+
+  @Inject
+  public CodereadyOpenShiftLoginPage(
+      SeleniumWebDriver seleniumWebDriver, SeleniumWebDriverHelper seleniumWebDriverHelper) {
+    super(seleniumWebDriver, seleniumWebDriverHelper);
+    this.seleniumWebDriverHelper = seleniumWebDriverHelper;
+
+    PageFactory.initElements(seleniumWebDriver, this);
+  }
+
+  public void login(String username, String password) {
+    waitOnOpen();
+
+    seleniumWebDriverHelper.setValue(usernameInput, username);
+    seleniumWebDriverHelper.setValue(passwordInput, password);
+    seleniumWebDriverHelper.waitAndClick(loginButton);
+
+    waitOnClose();
+  }
+
+  public void waitOnOpen() {
+    seleniumWebDriverHelper.waitAllVisibility(
+        asList(usernameInput, passwordInput, loginButton), WIDGET_TIMEOUT_SEC);
+  }
+
+  public void waitOnClose() {
+    seleniumWebDriverHelper.waitInvisibility(loginTitle);
+  }
+
+  public void clickOnRegisterUserLink() {
+    seleniumWebDriverHelper.waitAndClick(registerLink);
+  }
+
+  public void submit(String userName, String email) {
+    seleniumWebDriverHelper.setValue(firstUsername, userName);
+    seleniumWebDriverHelper.setValue(lastUsername, userName);
+    seleniumWebDriverHelper.setValue(emailName, email);
+    seleniumWebDriverHelper.setValue(usernameInput, userName);
+
+    seleniumWebDriverHelper.waitAndClick(submitButton);
+  }
+
+  public Boolean isApproveButtonVisible() {
+    return seleniumWebDriverHelper.isVisible(approveButton);
+  }
+}
---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/ocpoauth/LoginNewUserWithOpenShiftOAuthTest.java
@@ -0,0 +1,124 @@
+/*
+* Copyright (c) 2019 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-2.0
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.ocpoauth;
+
+import static com.redhat.codeready.selenium.pageobject.dashboard.CodereadyNewWorkspace.CodereadyStacks.JAVA_DEFAULT;
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage;
+import com.redhat.codeready.selenium.pageobject.dashboard.CodeReadyCreateWorkspaceHelper;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.TestGroup;
+import org.eclipse.che.selenium.core.provider.TestDashboardUrlProvider;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.pageobject.Ide;
+import org.eclipse.che.selenium.pageobject.ToastLoader;
+import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.eclipse.che.selenium.pageobject.ocp.AuthorizeOpenShiftAccessPage;
+import org.eclipse.che.selenium.pageobject.ocp.OpenShiftProjectCatalogPage;
+import org.eclipse.che.selenium.pageobject.site.CheLoginPage;
+import org.eclipse.che.selenium.pageobject.site.FirstBrokerProfilePage;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+@Test(groups = {TestGroup.OPENSHIFT, TestGroup.MULTIUSER})
+public class LoginNewUserWithOpenShiftOAuthTest {
+
+  private static final String WORKSPACE_NAME = generate("workspace", 4);
+  private static final String PROJECT = "kitchensink-example";
+
+  private TestWorkspace testWorkspace;
+
+  @Inject(optional = true)
+  @Named("env.openshift.regular.username")
+  private String openShiftUsername;
+
+  @Inject(optional = true)
+  @Named("env.openshift.regular.password")
+  private String openShiftPassword;
+
+  @Inject(optional = true)
+  @Named("env.openshift.regular.email")
+  private String openShiftEmail;
+
+  @Inject private CheLoginPage cheLoginPage;
+  @Inject private CodereadyOpenShiftLoginPage codereadyOpenShiftLoginPage;
+  @Inject private FirstBrokerProfilePage firstBrokerProfilePage;
+  @Inject private AuthorizeOpenShiftAccessPage authorizeOpenShiftAccessPage;
+  @Inject private Dashboard dashboard;
+  @Inject private Workspaces workspaces;
+  @Inject private ToastLoader toastLoader;
+  @Inject private Ide ide;
+  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
+  @Inject private OpenShiftProjectCatalogPage openShiftProjectCatalogPage;
+  @Inject private SeleniumWebDriver seleniumWebDriver;
+  @Inject private TestDashboardUrlProvider testDashboardUrlProvider;
+  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
+  @Inject private CodeReadyCreateWorkspaceHelper codeReadyCreateWorkspaceHelper;
+
+  @AfterClass
+  private void removeTestWorkspace() throws Exception {
+    testWorkspace.delete();
+  }
+
+  @Test
+  public void checkWorkspaceOSProjectCreationAndRemoval() throws Exception {
+    // go to login page of Codeready
+    seleniumWebDriver.navigate().to(testDashboardUrlProvider.get());
+
+    cheLoginPage.loginWithOpenShiftOAuth();
+    codereadyOpenShiftLoginPage.login(openShiftUsername, openShiftPassword);
+
+    // authorize ocp-client to access OpenShift account
+    if (codereadyOpenShiftLoginPage.isApproveButtonVisible()) {
+      authorizeOpenShiftAccessPage.waitOnOpen();
+      authorizeOpenShiftAccessPage.allowPermissions();
+    }
+
+    // fill profile page
+    codereadyOpenShiftLoginPage.submit(openShiftUsername, openShiftEmail);
+
+    // create and open workspace
+    testWorkspace =
+        codeReadyCreateWorkspaceHelper.createWsFromStackWithTestProject(
+            WORKSPACE_NAME, JAVA_DEFAULT, ImmutableList.of(PROJECT));
+
+    // switch to the Codeready IDE and wait until workspace is ready to use
+    seleniumWebDriverHelper.switchToIdeFrameAndWaitAvailability();
+    toastLoader.waitToastLoaderAndClickStartButton();
+    ide.waitOpenedWorkspaceIsReadyToUse();
+
+    // go to OCP and check if there is a project with name equals to test workspace id
+    openShiftProjectCatalogPage.open();
+    codereadyOpenShiftLoginPage.login(openShiftUsername, openShiftPassword);
+    openShiftProjectCatalogPage.waitProject("workspace");
+
+    // delete the created workspace on Dashboard
+    seleniumWebDriver.navigate().to(testDashboardUrlProvider.get());
+    dashboard.waitDashboardToolbarTitle();
+    dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.selectAllWorkspacesByBulk();
+    workspaces.clickOnDeleteWorkspacesBtn();
+    workspaces.clickOnDeleteButtonInDialogWindow();
+    workspaces.waitWorkspaceIsNotPresent(WORKSPACE_NAME);
+
+    // go to OCP and check that project is not exist
+    openShiftProjectCatalogPage.open();
+    openShiftProjectCatalogPage.waitProjectAbsence("workspace");
+  }
+}
