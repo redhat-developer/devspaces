f74296e1a527793c6868a10475f31f40c5497a99
Dmytro Nochevnov <dnochevn@redhat.com>
2019-07-16 14:14:16 +0300

    Fix locator of OpenShift OAuth link button in e2e tests (#163)
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

---
+++ test/codeready-test-e2e/src/main/java/com/redhat/codeready/selenium/pageobject/site/CodereadyLoginPage.java
@@ -0,0 +1,46 @@
+/*
+* Copyright (c) 2019 Red Hat, Inc.
+
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-2.0
+*
+* Contributors:
+*   Red Hat, Inc. - initial API and implementation
+*/
+package com.redhat.codeready.selenium.pageobject.site;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.pageobject.site.CheLoginPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/** @author Dmytro Nochevnov TODO implement it in upstream Che 7 and remove in CRW 2.0 */
+@Singleton
+public class CodereadyLoginPage extends CheLoginPage {
+
+  protected interface Locators {
+    String OPEN_SHIFT_OAUTH_LINK_XPATH = "//a[@id[contains(.,'zocial-openshift-v')]]";
+  }
+
+  @FindBy(xpath = Locators.OPEN_SHIFT_OAUTH_LINK_XPATH)
+  private WebElement openShiftOAuthLink;
+
+  private final SeleniumWebDriverHelper seleniumWebDriverHelper;
+
+  @Inject
+  public CodereadyLoginPage(
+      SeleniumWebDriver seleniumWebDriver, SeleniumWebDriverHelper seleniumWebDriverHelper) {
+    super(seleniumWebDriver, seleniumWebDriverHelper);
+
+    this.seleniumWebDriverHelper = seleniumWebDriverHelper;
+  }
+
+  public void loginWithOpenShiftOAuth() {
+    seleniumWebDriverHelper.waitAndClick(openShiftOAuthLink);
+  }
+}
