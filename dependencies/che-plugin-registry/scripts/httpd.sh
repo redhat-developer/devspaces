ee000faeaab8f5757ba61f5a656064b4a1a69a19
nickboldt <nboldt@redhat.com>
2019-09-30 12:37:23 -0400

    make httpd.sh smarter (find the correct executable for different 'nixes)
    Change-Id: Iab3b324f24d105c9252cdc2d8f44f41e5e09e10f
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/scripts/httpd.sh
+++ dependencies/che-plugin-registry/scripts/httpd.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-/usr/sbin/httpd -D FOREGROUND
+#!/bin/bash -x
+
+# start httpd
+if [[ -x /usr/sbin/httpd ]]; then /usr/sbin/httpd -D FOREGROUND
+elif [[ -x /usr/bin/run-httpd ]]; then /usr/bin/run-httpd
+fi
