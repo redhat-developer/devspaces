2705f07b3ecdf513e581397cdf8a700f08060950
nickboldt <nboldt@redhat.com>
2019-09-30 14:05:45 -0400

    remove scripts/httpd.sh; fix scripts/uid_entrypoint.sh (shellcheck you suck)
    Change-Id: I4394f11787e47c757f88ef525e74af9b5054ed10
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/scripts/uid_entrypoint.sh
+++ dependencies/che-plugin-registry/scripts/uid_entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     echo "${USER_NAME:-jboss}:x:$(id -u):0:${USER_NAME:-jboss} user:${HOME}:/sbin/nologin" >> /etc/passwd
