455a58cd3cc49b89d297838563c3bdee50a75935
nickboldt <nboldt@redhat.com>
2019-10-05 16:41:57 -0400

    merge in changes from PR 467
    Change-Id: I062c71c9f926b9708145cecb82811bfbe3114788
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-theia/dockerfiles/theia/build.sh
+++ dependencies/che-theia/dockerfiles/theia/build.sh
@@ -7,53 +7,67 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 
-# Define upstream version of theia to use
-THEIA_VERSION=master
-THEIA_GITHUB_REPO=eclipse-theia/theia
-GIT_BRANCH_NAME=v0.11.0 # use tag v0.11.0 or branch master
-if [[ -d theia-source-code ]]; then rm -fr theia-source-code; fi
-echo "Check out https://github.com/${THEIA_GITHUB_REPO} from branch ${GIT_BRANCH_NAME} to ./theia-source-code ..."
-git clone -q --branch ${GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${THEIA_GITHUB_REPO} theia-source-code
-
-CHE_THEIA_GIT_BRANCH_NAME=7.2.0
-CHE_THEIA_GITHUB_REPO=eclipse/che-theia
-if [[ -d theia-source-code/che-theia ]]; then rm -fr theia-source-code/che-theia; fi
-echo "Check out https://github.com/${CHE_THEIA_GITHUB_REPO} from branch ${CHE_THEIA_GIT_BRANCH_NAME} to ./theia-source-code/che-theia ..."
-git clone -q --branch ${CHE_THEIA_GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${CHE_THEIA_GITHUB_REPO} theia-source-code/che-theia
-
 base_dir=$(cd "$(dirname "$0")"; pwd)
 . "${base_dir}"/../build.include
 
-# check_github_limits
-
-# DIR=$(cd "$(dirname "$0")"; pwd)
-# LOCAL_ASSEMBLY_DIR="${DIR}"/che-theia
-
-# if [ -d "${LOCAL_ASSEMBLY_DIR}" ]; then
-#   rm -r "${LOCAL_ASSEMBLY_DIR}"
-# fi
-
-# # In mac os 'cp' cannot create destination dir, so create it first
-# mkdir -p ${LOCAL_ASSEMBLY_DIR}
-
-# echo "Compresing 'che-theia' --> ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz"
-# pushd "${DIR}"/../.. >/dev/null 
-#   git ls-files -z -c -o --exclude-standard | xargs -0 tar rf ${LOCAL_ASSEMBLY_DIR}/che-theia.tar
-# popd >/dev/null
-
 init --name:theia "$@"
 
-if [ "${CDN_PREFIX:-}" != "" ]; then
-  BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
+if [[ ${OFFLINE} == "false" ]]; then check_github_limits; fi
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+LOCAL_ASSEMBLY_DIR="${DIR}"/che-theia
+
+if [ -d "${LOCAL_ASSEMBLY_DIR}" ]; then
+  rm -r "${LOCAL_ASSEMBLY_DIR}"
 fi
 
-if [ "${MONACO_CDN_PREFIX:-}" != "" ]; then
-  BUILD_ARGS+="--build-arg MONACO_CDN_PREFIX=${MONACO_CDN_PREFIX} "
+if [[ ${OFFLINE} == "false" ]]; then 
+	# In mac os 'cp' cannot create destination dir, so create it first
+	mkdir -p ${LOCAL_ASSEMBLY_DIR}
+
+	echo "Compresing 'che-theia' --> ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz"
+	cd "${DIR}"/../.. && git ls-files -z -c -o --exclude-standard | xargs -0 tar rvf ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz
+
+	if [ "${CDN_PREFIX:-}" != "" ]; then
+	  BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
+	fi
+
+	if [ "${MONACO_CDN_PREFIX:-}" != "" ]; then
+	  BUILD_ARGS+="--build-arg MONACO_CDN_PREFIX=${MONACO_CDN_PREFIX} "
+	fi
 fi
 
-echo "Build image theia using FROM ${ORGANIZATION}/${PREFIX}-theia-dev:${TAG} ..."
-build Dockerfile
-if [[ $SKIP_TESTS == "false" ]] && [[ -x "${base_dir}"/e2e/build.sh ]]; then
+# prefetch 3 offline artifacts
+# TODO: also need to collect npm/yarn installed node_modules but can't do that until the container is built
+if [[ ${OFFLINE} == "true" ]]; then
+	THEIA_VERSION=master
+	THEIA_GITHUB_REPO=eclipse-theia/theia
+	GIT_BRANCH_NAME=v0.11.0 # use tag v0.11.0 or branch master
+	if [[ -d theia-source-code ]]; then rm -fr theia-source-code; fi
+	echo "Check out https://github.com/${THEIA_GITHUB_REPO} from branch ${GIT_BRANCH_NAME} to ./theia-source-code ..."
+	git clone -q --branch ${GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${THEIA_GITHUB_REPO} theia-source-code
+
+	CHE_THEIA_GIT_BRANCH_NAME=7.2.0
+	CHE_THEIA_GITHUB_REPO=eclipse/che-theia
+	if [[ -d theia-source-code/che-theia ]]; then rm -fr theia-source-code/che-theia; fi
+	echo "Check out https://github.com/${CHE_THEIA_GITHUB_REPO} from branch ${CHE_THEIA_GIT_BRANCH_NAME} to ./theia-source-code/che-theia ..."
+	git clone -q --branch ${CHE_THEIA_GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${CHE_THEIA_GITHUB_REPO} theia-source-code/che-theia
+
+	# create theia-source-code.tgz
+	cd theia-source-code; tar -pczf ../theia-source-code.tgz . && cd ..
+
+	# get default theia plugins needed for the build
+	curl -sSL https://github.com/eclipse/theia-yeoman-plugin/releases/download/untagged-c11870b25a17d20bb7a7/theia_yeoman_plugin.theia -o theia_yeoman_plugin.theia
+	curl -sSL https://github.com/che-incubator/vscode-git/releases/download/1.30.1/vscode-git-1.3.0.1.vsix -o vscode-git-1.3.0.1.vsix
+fi
+
+if [[ ${OFFLINE} == "true" ]]; then
+  build rhel.Dockerfile
+else
+  build Dockerfile
+fi
+
+if [ ! skip_tests ] && [[ -x "${base_dir}"/e2e/build.sh ]]; then
   bash "${base_dir}"/e2e/build.sh "$PREFIX-$NAME" "$@"
 else
   echo "E2E tests skipped."
