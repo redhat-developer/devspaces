0148be7d4a144633c2fbde96ba42e35c56a393d3
nickboldt <nboldt@redhat.com>
2019-10-05 22:33:44 -0400

    only collect artifacts from /opt/app-root/src/.npm-global; other dirs are empty
    Change-Id: Id556352299522155ab67ca59b45e43c1a3d826da
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-theia/dockerfiles/theia/collect.installed.dependencies.sh
+++ dependencies/che-theia/dockerfiles/theia/collect.installed.dependencies.sh
@@ -3,12 +3,10 @@
 # script to collect intalled node deps into a single folder for easier tarballing
 
 for d in \
-    /home/theia/.yarn-global \
-    /usr/local/share/.cache/yarn \
     /opt/app-root/src/.npm-global; do \
     if [[ -d ${d} ]]; then 
-        mkdir -p /tmp/root-local/${d%/*}/; # one path segment less than $d
-        rsync -aAXz ${d} /tmp/root-local/${d%/*}/;
+        mkdir -p /tmp/root-local${d%/*}/; # one path segment less than $d
+        rsync -aAXz ${d} /tmp/root-local${d%/*}/;
     else
         echo "Error: did not find directory ${d} to archive!"
         exit 1
