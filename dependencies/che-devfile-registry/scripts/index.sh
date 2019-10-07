472cedbf5704cd841fecc8d7c5bf2774ffbb4dbd
nickboldt <nboldt@redhat.com>
2019-10-07 09:56:57 -0400

    add scripts devfiles/ folder, and rootfiles for che-devfile-registry fork from upstream
    Change-Id: I92e2c425867b9eb5bae8ada8712bdca6cf5db199
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ dependencies/che-devfile-registry/scripts/index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2012-2018 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+set -e
+
+readarray -d '' metas < <(find devfiles -name 'meta.yaml' -print0)
+for meta in "${metas[@]}"; do
+    META_DIR=$(dirname ${meta})
+    # Workaround to include self-links, since it's not possible to
+    # get filename in yq easily
+    echo -e "links:\n  self: /${META_DIR}/devfile.yaml" >> "${meta}"
+done
+yq -s 'map(.)' "${metas[@]}"
