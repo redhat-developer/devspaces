a6467a9ebe680cc2b9f9ba6890ba83dcde99e7ab
Mykhailo Kuznietsov <mkuznets@redhat.com>
2019-10-23 23:51:31 +0300

    fixup! fixup! fixup! Merge branch 'master' of github.com:redhat-developer/codeready-workspaces into che-14677

--- dependencies/che-plugin-registry/build/scripts/cache_artifacts.sh
+++ dependencies/che-plugin-registry/build/scripts/cache_artifacts.sh
@@ -23,45 +23,42 @@ fi
 RESOURCES_DIR="${1}/resources/"
 TEMP_DIR="${1}/extensions_temp/"
 
-PRECACHED_ROOT_DIR_NAME="result"
-
+PREBUILT_VSIX_ROOT_DIR_NAME="vsix"
 
 # if archive with prebuilt extensions is located at given path,
 # use them to substitute
-if [ -f "/tmp/vsix.tar.gz" ]; then
-echo 'found non empty extensions tar, unpacking'
-  tar -zxvf /tmp/vsix.tar.gz
-  readarray -d '' precached_plugins < <(find $PRECACHED_ROOT_DIR_NAME -name '*.vsix' -print0)
+echo "extract?"
+
+if [ -f "/build/vsix.tar.gz" ]; then
+echo 'found vsix.tar.gz, unpacking'
+  tar -zxvf /build/vsix.tar.gz
+  readarray -d '' prebuilt_extensions < <(find /build/$PREBUILT_VSIX_ROOT_DIR_NAME -name '*.vsix' -print0)
 fi
 
 mkdir -p "${RESOURCES_DIR}" "${TEMP_DIR}"
 for extension in $(yq -r '.spec.extensions[]?' "${metas[@]}" | sort | uniq); do
   echo -en "Caching extension ${extension}\n    "
-  # Workaround for getting filenames through content-disposition: copy to temp
-  # dir and read filename before moving to /resources.
-
-
-  # Before attempting to download, check if we already have this file in supplied precached archive
-  # if found, skip the download
-  for plugin_file_path in "${precached_plugins[@]}"; do
+  # Before attempting to download, check if we already have this file in supplied prebuilt plugins
+  # archive. If found, skip the download
+  for plugin_file_path in "${prebuilt_extensions[@]}"; do
     echo evaluating $plugin_file_path
 
     # strip root directory from path on filesystem to match it with extension URL
-    plugin_file_path=${plugin_file_path#${PRECACHED_ROOT_DIR_NAME}/}
+    plugin_file_path=${plugin_file_path#${PREBUILT_VSIX_ROOT_DIR_NAME}/}
     plugin_file_path=${plugin_file_path%/*.vsix}
 
-    echo directorey: "$plugin_file_path"
+    extension_location=${extension#*//}
 
   #  if [[ $precached_url == */vspackage ]]; then
   #    precached_url=${precached_url%/vspackage}
   #    echo --- transformed to "$precached_url"
   #  fi
-    if [[ $plugin_file_path == $precached_url ]]; then
-      echo mached extension "$plugin_file_path" and "$precached_url"
+    if [[ ${plugin_file_path} == $extension_location ]]; then
+      echo mached extension "$plugin_file_path" and "$extension_location"
       matched_plugin_path = plugin_file_path
       break
     else
-      echo not mached extension "$plugin_file_path" and "$precached_url"
+      echo not mached extension "$plugin_file_path" and "$extension_location"
     fi
   done
 
@@ -69,6 +66,8 @@ for extension in $(yq -r '.spec.extensions[]?' "${metas[@]}" | sort | uniq); do
     echo "omitting download of plugin ${matched_plugin_path}"
     mv "${matched_plugin_path}"  ${TEMP_DIR}
   else
+    # Workaround for getting filenames through content-disposition: copy to temp
+    # dir and read filename before moving to /resources.
     wget -P "${TEMP_DIR}" -nv --content-disposition "${extension}"
   fi
 
