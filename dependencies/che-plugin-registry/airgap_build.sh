ab4bcdcb7fe806e8600bc86bb02f9a9e1057dfe9
nickboldt <nboldt@redhat.com>
2019-10-24 14:31:02 -0400

    use rhel.Dockerfile and call the output image airgap-crw-plugin-registry
    Change-Id: I2ccd49cbf6c84524ab782226a63b248f98bdde40
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/airgap_build.sh
+++ dependencies/che-plugin-registry/airgap_build.sh
@@ -4,7 +4,6 @@
 # 
 # see also https://github.com/eclipse/che/issues/14693
 # 
-# eg., ./airgap_build.sh mycorpquay.bigco.co.uk --push --squash
 
 if [[ $1 == "nightly" ]]; then
 	registry="myquay.mycorp.com"
@@ -17,21 +16,24 @@ elif [[ $1 ]]; then
 else
 	echo "Must specify URL of internal registry to use, eg., $0 myquay.mycorp.com"
 	echo "To fetch plugins but not do registry substitutions, use $0 nightly"
-	echo "To push to quay, use $0 myquay.mycorp.com --push"
+	echo "To push to quay, use $0 myquay.mycorp.com --push --squash"
 	exit 1
 fi
 
+DOCKERFILE=build/dockerfiles/rhel.Dockerfile
+
 # Extra step for air gap - replace references to docker.io, quay.io, registry.access.redhat.com, registry.redhat.io with internal registry
 # inject this into the Dockerfile: RUN ./replace_container_repos.sh v3 myquay.mycorp.com
-cat Dockerfile | sed -e "s%#.*\(RUN ./check_plugins_location.sh v3\)%./replace_container_repos.sh v3 ${registry} && \1%" > Dockerfile.2; mv Dockerfile.2 Dockerfile
+cat ${DOCKERFILE} | sed -e "s%#.*\(RUN ./check_plugins_location.sh v3\)%./replace_container_repos.sh v3 ${registry} && \1%" > ${DOCKERFILE}.2
+mv ${DOCKERFILE}.2 ${DOCKERFILE}
 
 now=`date +%Y%m%d-%H%M`
-docker build . -f build/dockerfiles/rhel.Dockerfile -t quay.io/nickboldt/airgap-che-plugin-registry:${nightly} --no-cache $3
-docker tag quay.io/nickboldt/airgap-che-plugin-registry:{${nightly},${now}}
+docker build . -f ${DOCKERFILE} -t quay.io/nickboldt/airgap-crw-plugin-registry:${nightly} --no-cache $3
+docker tag quay.io/nickboldt/airgap-crw-plugin-registry:{${nightly},${now}}
 
 if [[ $2 == "--push" ]]; then
 	for d in ${nightly} ${now}; do
-		docker push quay.io/nickboldt/airgap-che-plugin-registry:${d} &
+		docker push quay.io/nickboldt/airgap-crw-plugin-registry:${d} &
 	done
 	wait
 else
@@ -39,7 +41,7 @@ else
 
 "
 	for d in ${nightly} ${now}; do
-		echo "docker push quay.io/nickboldt/airgap-che-plugin-registry:${d} &"
+		echo "docker push quay.io/nickboldt/airgap-crw-plugin-registry:${d} &"
 	done
 fi
 
