7ec75fbc3e4ac6af307e4bea342b823b28987fd7
Sergey Skorik <sskoryk@redhat.com>
2019-12-20 13:05:22 +0200

    Adapt com.redhat.codeready.selenium.ocpoauth.* tests to be run against OCP 4.x (#212)

---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/ocpoauth/LoginExistedUserWithOpenShiftOAuthTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.redhat.codeready.selenium.ocpoauth;
+
+import static java.lang.String.format;
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.redhat.codeready.selenium.pageobject.CodereadyOpenShiftLoginPage;
+import com.redhat.codeready.selenium.pageobject.site.CodereadyLoginPage;
+import java.util.Collections;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.TestGroup;
+import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.provider.TestDashboardUrlProvider;
+import org.eclipse.che.selenium.core.user.TestUser;
+import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Devfile;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.eclipse.che.selenium.pageobject.ocp.AuthorizeOpenShiftAccessPage;
+import org.eclipse.che.selenium.pageobject.site.FirstBrokerProfilePage;
+import org.eclipse.che.selenium.pageobject.theia.TheiaIde;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+@Test(groups = {TestGroup.OPENSHIFT, TestGroup.MULTIUSER})
+public class LoginExistedUserWithOpenShiftOAuthTest {
+
+  private static final String WORKSPACE_NAME = generate("workspace", 4);
+  private static final String LOGIN_TO_CHE_WITH_OPENSHIFT_OAUTH_MESSAGE_TEMPLATE =
+      "Authenticate as %s to link your account with openshift";
+  private static final String USER_ALREADY_EXISTS_ERROR_MESSAGE_TEMPLATE =
+      "User with username %s already exists. How do you want to continue?";
+  private static final String IDENTITY_PROVIDER_NAME = "htpasswd";
+
+  private static final TestUser testUser = getTestUser();
+
+  @Inject(optional = true)
+  @Named("env.openshift.username")
+  private String openShiftUsername;
+
+  @Inject(optional = true)
+  @Named("env.openshift.password")
+  private String openShiftPassword;
+
+  @Inject private CodereadyLoginPage codereadyLoginPage;
+  @Inject private CodereadyOpenShiftLoginPage codereadyOpenShiftLoginPage;
+  @Inject private FirstBrokerProfilePage firstBrokerProfilePage;
+  @Inject private AuthorizeOpenShiftAccessPage authorizeOpenShiftAccessPage;
+  @Inject private Dashboard dashboard;
+  @Inject private Workspaces workspaces;
+  @Inject private SeleniumWebDriver seleniumWebDriver;
+  @Inject private TestDashboardUrlProvider testDashboardUrlProvider;
+  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
+  @Inject private TheiaIde theiaIde;
+  @Inject private TestWorkspaceServiceClient defaultUserWorkspaceServiceClient;
+
+  @AfterClass
+  private void removeTestWorkspace() throws Exception {
+    defaultUserWorkspaceServiceClient.delete(WORKSPACE_NAME, openShiftUsername);
+  }
+
+  @Test
+  public void checkWorkspaceOSProjectCreationAndRemoval() {
+    String expectedError = format(USER_ALREADY_EXISTS_ERROR_MESSAGE_TEMPLATE, testUser.getName());
+
+    // go to login page of Codeready
+    seleniumWebDriver.navigate().to(testDashboardUrlProvider.get());
+
+    if (codereadyOpenShiftLoginPage.isIdentityProviderLinkVisible(IDENTITY_PROVIDER_NAME)) {
+      codereadyOpenShiftLoginPage.clickOnIdentityProviderLink(IDENTITY_PROVIDER_NAME);
+    }
+    codereadyOpenShiftLoginPage.login(openShiftUsername, openShiftPassword);
+
+    // authorize ocp-client to access OpenShift account
+    if (codereadyOpenShiftLoginPage.isApproveButtonVisible()) {
+      authorizeOpenShiftAccessPage.waitOnOpen();
+      authorizeOpenShiftAccessPage.allowPermissions();
+    }
+
+    firstBrokerProfilePage.submit(testUser);
+
+    // apply OCP user information to Codeready user account
+    assertEquals(firstBrokerProfilePage.getErrorAlert(), expectedError);
+    firstBrokerProfilePage.addToExistingAccount();
+
+    // login into Codeready again
+    String expectedInfo =
+        format(LOGIN_TO_CHE_WITH_OPENSHIFT_OAUTH_MESSAGE_TEMPLATE, testUser.getName());
+    assertTrue(codereadyLoginPage.getInfoAlert().contains(expectedInfo));
+    codereadyLoginPage.loginWithPredefinedUsername(testUser.getPassword());
+
+    createWorkspaceHelper.createAndStartWorkspaceFromStack(
+        Devfile.JAVA_MAVEN, WORKSPACE_NAME, Collections.emptyList(), null);
+
+    // switch to the Codeready IDE and wait until workspace is ready to use
+    theiaIde.switchToIdeFrame();
+    theiaIde.waitTheiaIde();
+    theiaIde.waitLoaderInvisibility();
+    theiaIde.waitTheiaIdeTopPanel();
+
+    // delete the created workspace on Dashboard
+    seleniumWebDriver.navigate().to(testDashboardUrlProvider.get());
+    dashboard.waitDashboardToolbarTitle();
+    dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.selectAllWorkspacesByBulk();
+    workspaces.clickOnDeleteWorkspacesBtn();
+    workspaces.clickOnDeleteButtonInDialogWindow();
+    workspaces.waitWorkspaceIsNotPresent(WORKSPACE_NAME);
+  }
+
+  private static TestUser getTestUser() {
+    return new TestUser() {
+      private final String name = "admin";
+      private final String email = name + "@1.com";
+      private final String password = "admin";
+
+      @Override
+      public String getEmail() {
+        return email;
+      }
+
+      @Override
+      public String getPassword() {
+        return password;
+      }
+
+      @Override
+      public String obtainAuthToken() {
+        return null;
+      }
+
+      @Override
+      public String getName() {
+        return name;
+      }
+
+      @Override
+      public String getId() {
+        return null;
+      }
+
+      @Override
+      public void delete() {}
+    };
+  }
+}
