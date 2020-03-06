59beee8cc7a76722591f8bbb34a8d1c633db9e7a
nickboldt <nboldt@redhat.com>
2020-03-06 16:14:00 -0500

    https://github.com/eclipse/che/issues/16281 https://issues.redhat.com/browse/CRW-653 don't rewrite if can't see sha
    Change-Id: Ie928eb3a8ebc212a5e87e3078be4b4cd7cca8787
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/write_image_digests.sh
+++ dependencies/che-devfile-registry/build/scripts/write_image_digests.sh
@@ -8,14 +8,29 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
+LOG_FILE="/tmp/image_digests.log"
+
+function handle_error() {
+  echo "  Could not read image metadata through skopeo inspect; skipping"
+  echo -n "  Reason: "
+  sed 's|^|    |g' $LOG_FILE
+}
 
 readarray -d '' devfiles < <(find "$1" -name 'devfile.yaml' -print0)
 for image in $(yq -r '.components[]?.image' "${devfiles[@]}" | grep -v "null" | sort | uniq); do
   echo "Rewriting image $image"
-  digest=$(skopeo inspect "docker://${image}" | jq -r '.Digest')
+  # Need to look before we leap in case image is not accessible
+  if ! image_data=$(skopeo inspect "docker://${image}" 2>"$LOG_FILE"); then
+    handle_error
+    continue
+  fi
+  # Grab digest from image metadata json
+  digest=$(echo "$image_data" | jq -r '.Digest')
+
   echo "  to use digest $digest"
   digest_image="${image%:*}@${digest}"
 
   # Rewrite images to use sha-256 digests
   sed -i -E 's|"?'"${image}"'"?|"'"${digest_image}"'" # tag: '"${image}"'|g' "${devfiles[@]}"
 done
+rm $LOG_FILE
