75cca765900f789470a3d67238e7ffda7a480ad3
nickboldt <nboldt@redhat.com>
2020-03-16 18:19:42 -0400

    Support sparseCheckoutDir for offline cached projects - by amisevsk@redhat.com
    Change-Id: I4b9f051a48b7a82d87eb9952b177241a4047fea9
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/cache_projects.sh
+++ dependencies/che-devfile-registry/build/scripts/cache_projects.sh
@@ -26,14 +26,22 @@ TEMP_REPO="${TEMP_DIR}/cloned"
 #   $1 - URL of git repo
 #   $2 - branch to archive
 #   $3 - destination path for the archived project zip file
+#   $4 - sparse checkout directory
 function cache_project() {
   local repo="$1"
   local branch="$2"
   local destination="$3"
+  local sparse_checkout_dir="$4"
+
   rm -fr "$TEMP_REPO"
   git clone "$repo" -b "$branch" --depth 1 "$TEMP_REPO" -q
   pushd "$TEMP_REPO" &>/dev/null
-    git archive "$branch" -o "$destination"
+    if [ -n "$sparse_checkout_dir" ]; then
+      echo "    Using sparse checkout dir '$sparse_checkout_dir'"
+      git archive -9 "$branch" "$sparse_checkout_dir" -o "$destination"
+    else 
+      git archive -9 "$branch" -o "$destination"
+    fi
   popd &>/dev/null
   rm -rf "$TEMP_REPO"
 }
@@ -108,12 +116,17 @@ for devfile in "${devfiles[@]}"; do
     if [[ ! "$branch" ]] || [[ "$branch" == "null" ]]; then
       branch="master"
     fi
+    sparse_checkout_dir=$(echo "$project" | jq -r '.source.sparseCheckoutDir')
+    if [[ ! "$sparse_checkout_dir" ]] || [[ "$sparse_checkout_dir" == "null" ]]; then
+      unset sparse_checkout_dir
+    fi
+
     # echo "    Caching project $project_name from branch $branch"
     destination="${RESOURCES_DIR}/${devfile_name}-${project_name}-${branch}.zip"
     absolute_destination=$(realpath "$destination")
     # echo "    Caching project to $absolute_destination"
     echo "    Caching project from $location/blob/${branch} to $destination"
-    cache_project "$location" "$branch" "$absolute_destination"
+    cache_project "$location" "$branch" "$absolute_destination" "$sparse_checkout_dir"
 
     echo "    Updating devfile $devfile to point at cached project zip $destination"
     update_devfile "$devfile" "$project_name" "$destination"
