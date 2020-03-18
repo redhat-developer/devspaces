8740a009da7a315c27fcd39b292518cc943cf747
nickboldt <nboldt@redhat.com>
2020-03-18 14:06:52 -0400

    add add_che_plugins.sh because I'm sick of manually adding Che plugins; renove 7.8.0 because we don't need it
    Change-Id: I1314d33ac41cf02420e34099f6caeb4f5fd7bd90
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ dependencies/che-plugin-registry/build/scripts/add_che_plugins.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Maintenance process automation script. 
+# Used to create new che-theia and machine-exec plugins and commit changes more easily.
+
+NOCOMMIT=0
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-v'|'--version') VERSION="$2"; shift 1;;
+    '-n'|'--no-commit') NOCOMMIT=1; shift 0;;
+  esac
+  shift 1
+done
+
+usage ()
+{
+  echo "Usage:   $0 --version [VERSION TO ADD] [--no-commit]"
+  echo "Example: $0 --version 7.9.2"; echo
+}
+
+if [[ ! ${VERSION} ]]; then
+  usage
+  exit 1
+fi
+
+# generate new meta.yaml files for the plugins, and update the latest.txt files
+createNewPlugins () {
+  newVERSION=$1
+  rsync -aPrz v3/plugins/eclipse/che-machine-exec-plugin/nightly/* "v3/plugins/eclipse/che-machine-exec-plugin/${newVERSION}/"
+  rsync -aPrz v3/plugins/eclipse/che-theia/next/* "v3/plugins/eclipse/che-theia/${newVERSION}/"
+  pwd
+  for m in "v3/plugins/eclipse/che-theia/${newVERSION}/meta.yaml" "v3/plugins/eclipse/che-machine-exec-plugin/${newVERSION}/meta.yaml"; do
+    sed -i "${m}" \
+        -e "s#firstPublicationDate:.\+#firstPublicationDate: \"$(date +%Y-%m-%d)\"#" \
+        -e "s#version: \(nightly\|next\)#version: ${newVERSION}#" \
+        -e "s#image: \"\(.\+\):\(nightly\|next\)\"#image: \"\1:${newVERSION}\"#" \
+        -e "s# development version\.##" \
+        -e "s#, get the latest release each day\.##"
+  done
+  for m in v3/plugins/eclipse/che-theia/latest.txt v3/plugins/eclipse/che-machine-exec-plugin/latest.txt; do
+    echo "${newVERSION}" > $m
+  done
+}
+
+# change VERSION file
+echo "${VERSION}" > VERSION
+# add new plugins + update latest.txt files
+createNewPlugins "${VERSION}"
+
+# commit change into branch
+if [[ ${NOCOMMIT} -eq 0 ]]; then
+  COMMIT_MSG="[release] Bump to ${VERSION} in ${BRANCH}"
+  git add v3/plugins/eclipse/ || true
+  git commit -s -m "${COMMIT_MSG}" VERSION v3/plugins/eclipse/
+  git pull origin "${BRANCH}"
+  git push origin "${BRANCH}"
+fi
