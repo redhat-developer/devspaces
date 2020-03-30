5e3ecd4bf3dd4adaf2303f0b03374837fa3a1516
Sergey Skorik <sskoryk@redhat.com>
2020-03-30 19:58:58 +0300

    update JavaMaven devfile sample name, remove under repair tests from test suits (#266)

---
+++ test/codeready-test-e2e/src/test/java/com/redhat/codeready/selenium/workspaces/ProjectStateAfterRenameWorkspaceTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2019-2020 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.redhat.codeready.selenium.workspaces;
+
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+
+import com.google.inject.Inject;
+import java.util.Collections;
+import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.user.DefaultTestUser;
+import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Devfile;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceOverview;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.eclipse.che.selenium.pageobject.theia.TheiaEditor;
+import org.eclipse.che.selenium.pageobject.theia.TheiaIde;
+import org.eclipse.che.selenium.pageobject.theia.TheiaProjectTree;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/** @author Aleksandr Shmaraev */
+@Test
+public class ProjectStateAfterRenameWorkspaceTest {
+  private static final String WORKSPACE_NAME =
+      generate(ProjectStateAfterRenameWorkspaceTest.class.getSimpleName(), 5);
+  private static final String PROJECT_NAME = "vertx-health-checks-example-redhat";
+  private static final String WORKSPACE_NEW_NAME = generate("rename_ws", 4);
+  private static final String PATH_TO_POM_FILE = PROJECT_NAME + "/" + "pom.xml";
+  private static final String PATH_TO_README_FILE = PROJECT_NAME + "/" + "README.md";
+
+  @Inject private Dashboard dashboard;
+  @Inject private TestWorkspaceServiceClient workspaceServiceClient;
+  @Inject private DefaultTestUser defaultTestUser;
+  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
+  @Inject private TheiaIde theiaIde;
+  @Inject private TheiaProjectTree theiaProjectTree;
+  @Inject private TheiaEditor theiaEditor;
+  @Inject private WorkspaceDetails workspaceDetails;
+  @Inject private Workspaces workspaces;
+  @Inject private WorkspaceOverview workspaceOverview;
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    dashboard.open();
+    createWorkspaceHelper.createAndStartWorkspaceFromStack(
+        Devfile.JAVA_MAVEN, WORKSPACE_NAME, Collections.emptyList(), null);
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    workspaceServiceClient.delete(WORKSPACE_NAME, defaultTestUser.getName());
+    workspaceServiceClient.delete(WORKSPACE_NEW_NAME, defaultTestUser.getName());
+  }
+
+  @Test
+  public void checkProjectAfterRenameWs() {
+    theiaIde.waitOpenedWorkspaceIsReadyToUse();
+
+    theiaProjectTree.waitFilesTab();
+    theiaProjectTree.clickOnFilesTab();
+    theiaProjectTree.waitProjectAreaOpened();
+    theiaProjectTree.waitItem(PROJECT_NAME);
+    theiaIde.waitAllNotificationsClosed();
+
+    openFilesInEditor();
+    checkFilesAreOpened();
+
+    // go to dashboard and rename ws
+    dashboard.open();
+    dashboard.waitDashboardToolbarTitle();
+    dashboard.selectWorkspacesItemOnDashboard();
+    dashboard.waitToolbarTitleName("Workspaces");
+    workspaces.selectWorkspaceItemName(WORKSPACE_NAME);
+    workspaceOverview.enterNameWorkspace(WORKSPACE_NEW_NAME);
+    workspaceDetails.clickOnSaveChangesBtn();
+    dashboard.waitNotificationMessage("Workspace updated");
+    dashboard.waitNotificationIsClosed();
+    workspaceOverview.checkNameWorkspace(WORKSPACE_NEW_NAME);
+    workspaceDetails.clickOpenInIdeWsBtn();
+
+    theiaIde.waitOpenedWorkspaceIsReadyToUse();
+
+    checkFilesAreOpened();
+  }
+
+  private void openFilesInEditor() {
+    theiaProjectTree.expandItem(PROJECT_NAME);
+    theiaProjectTree.waitItem(PATH_TO_POM_FILE);
+    theiaProjectTree.waitItem(PATH_TO_README_FILE);
+
+    theiaProjectTree.openItem(PATH_TO_POM_FILE);
+    theiaProjectTree.openItem(PATH_TO_README_FILE);
+    theiaEditor.waitEditorTab("pom.xml");
+    theiaEditor.waitEditorTab("README.md");
+  }
+
+  private void checkFilesAreOpened() {
+    theiaEditor.waitEditorTab("pom.xml");
+    theiaEditor.waitEditorTab("README.md");
+  }
+}
