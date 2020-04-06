3905fe0c420cd77c0c9de4388ac4baeecd39f4e5
David Festal <dfestal@redhat.com>
2020-04-06 17:39:49 +0200

    Try `registry.access.redhat.com` for rhscl images
    Signed-off-by: David Festal <dfestal@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/write_image_digests_alternate_urls.sh
+++ dependencies/che-devfile-registry/build/scripts/write_image_digests_alternate_urls.sh
@@ -2,7 +2,8 @@
       echo ${image} | sed -r \
           `# for CRW images, use internal Brew versions when not yet released to RHCC` \
           -e "s|registry.redhat.io/codeready-workspaces/|registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-|g" \
-          -e "s|registry.redhat.io/codeready-workspaces/|registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-|g" \
+          `# for RHSCL images, use public one as an alternate to RHCC` \
+          -e "s|registry.redhat.io/rhscl/|registry.access.redhat.com/rhscl/|g" \
           `# for operator, replace internal container name with quay name` \
           -e "s|crw-2-rhel8-operator|operator-rhel8|g" \
           > ${tmpfile}
