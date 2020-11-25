250acc635c47701acd3cf5148e009700047ba9cb
nickboldt <nboldt@redhat.com>
2020-11-25 12:16:45 -0500

    CRW-1386 improve logic & flow for creating tags/branches in pkgs.devel and GH
    Change-Id: Iab3e8707f9607d683898da1ed21ed6ac5de2ee87
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/tagRelease.sh
+++ product/tagRelease.sh
@@ -52,7 +52,8 @@ cd /tmp/tmp-checkouts
 
 set -ex
 
-if [[ -z ${SOURCE_BRANCH} ]]; then 
+# tag pkgs.devel repos only (branches are created by SPMM ticket, eg., https://projects.engineering.redhat.com/browse/SPMM-2517)
+if [[ ${pkgs_devel_branch} ]] && [[ ${CRW_TAG} ]]; then 
 	for d in \
 	codeready-workspaces-configbump \
 	codeready-workspaces-operator \
@@ -87,20 +88,24 @@ if [[ -z ${SOURCE_BRANCH} ]]; then
 	codeready-workspaces-traefik \
 	; do
 		echo; echo "== $d =="
-		if [[ ! -d ${d} ]]; then git clone -b ${pkgs_devel_branch} ssh://${pduser}@pkgs.devel.redhat.com/containers/${d} containers_${d}; fi
-		cd containers_${d}
-		git config user.email crw-build@REDHAT.COM
-		git config user.name "CRW Build"
-		git config --global push.default matching
+		if [[ ! -d ${d} ]]; then
+			git clone -b ${pkgs_devel_branch} ssh://${pduser}@pkgs.devel.redhat.com/containers/${d} containers_${d}
+			pushd containers_${d} >/dev/null || exit 1
+				export KRB5CCNAME=/var/tmp/${pduser}_ccache
+				git config user.email ${pduser}@REDHAT.COM
+				git config user.name "CRW Build"
+				git config --global push.default matching
 
-		git checkout ${pkgs_devel_branch} -q
-		git pull -q
-
-		git tag -a ${CRW_TAG} -m "${CRW_TAG}" || true
-		git push origin ${CRW_TAG} || true
-		cd ..
+				git checkout --track origin/${pkgs_devel_branch} -q || true
+				git pull -q
+			popd >/dev/null || exit 1
+		fi
+		pushd containers_${d} >/dev/null || exit 1
+			# push new tag (no op if already exists)
+			git tag -a ${CRW_TAG} -m "${CRW_TAG}" || true
+			git push origin ${CRW_TAG} || true
+		popd >/dev/null || exit 1
 	done
-
 fi
 
 for d in \
@@ -114,39 +119,43 @@ codeready-workspaces-theia \
 ; do
 	echo; echo "== $d =="
 	if [[ ${SOURCE_BRANCH} ]]; then clone_branch=${SOURCE_BRANCH}; else clone_branch=${crw_repos_branch}; fi
-	if [[ ! -d ${d} ]]; then git clone --depth 1 -b ${clone_branch} git@github.com:redhat-developer/${d}.git projects_${d}; fi
-	cd projects_${d}
-	export GITHUB_TOKEN="${GITHUB_TOKEN}"
-	git config user.email "nickboldt+devstudio-release@gmail.com"
-	git config user.name "Red Hat Devstudio Release Bot"
-	git config --global push.default matching
-	git config --global hub.protocol https
-	git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/redhat-developer/${d}.git
+	if [[ ! -d ${d} ]]; then
+		git clone --depth 1 -b ${clone_branch} git@github.com:redhat-developer/${d}.git projects_${d}
+		pushd projects_${d} >/dev/null || exit 1
+			export GITHUB_TOKEN="${GITHUB_TOKEN}"
+			git config user.email "nickboldt+devstudio-release@gmail.com"
+			git config user.name "Red Hat Devstudio Release Bot"
+			git config --global push.default matching
+			git config --global hub.protocol https
+			git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/redhat-developer/${d}.git
 
-	git checkout --track origin/${clone_branch} -q || true
-	git pull -q
-	if [[ ${SOURCE_BRANCH} ]]; then # push a new branch
+			git checkout --track origin/${clone_branch} -q || true
+			git pull -q
+		popd >/dev/null || exit 1
+	fi
+	pushd projects_${d} >/dev/null || exit 1
+	if [[ $d == "codeready-workspaces-operator" ]]; then
+		# CRW-1386 OLD WAY, end up with internal repo refs in the published CSV
+		# rsync -aPr ../containers_codeready-workspaces-operator-metadata/manifests/* ./manifests/
+
+		# CRW-1386 new way - use containerExtract.sh to get the live, published operator-metadata image and copy the manifests/ folder from there
+		rm -fr /tmp/registry.redhat.io-codeready-workspaces-crw-2-rhel8-operator-metadata-2.5*
+		"${SCRIPTPATH}"/containerExtract.sh registry.redhat.io/codeready-workspaces/crw-2-rhel8-operator-metadata:2.5
+		rsync -aPr /tmp/registry.redhat.io-codeready-workspaces-crw-2-rhel8-operator-metadata-2.5*/manifests/* ./manifests/
+
+		git add ./manifests/
+		git commit -s -m "[release] copy generated manifests/ content back to codeready-workspaces-operator before tagging" ./manifests/ || true
+		git push origin ${clone_branch}
+	fi
+	if [[ ${SOURCE_BRANCH} ]]; then # push a new branch (or no-op if exists)
 		git branch ${crw_repos_branch} || true
 		git push origin ${crw_repos_branch} || true
-	else # for operator, inject latest CSV files w/ latest digests (CRW-833)
-		if [[ $d == "codeready-workspaces-operator" ]]; then
-			# CRW-1386 OLD WAY, end up with internal repo refs in the published CSV
-			# rsync -aPr ../containers_codeready-workspaces-operator-metadata/manifests/* ./manifests/
-
-			# CRW-1386 new way - use containerExtract.sh to get the live, published operator-metadata image and copy the manifests/ folder from there
-			rm -fr /tmp/registry.redhat.io-codeready-workspaces-crw-2-rhel8-operator-metadata-2.5*
-			"${SCRIPTPATH}"/containerExtract.sh registry.redhat.io/codeready-workspaces/crw-2-rhel8-operator-metadata:2.5
-			rsync -aPr /tmp/registry.redhat.io-codeready-workspaces-crw-2-rhel8-operator-metadata-2.5*/manifests/* ./manifests/
-
-			git add ./manifests/
-			git commit -s -m "[release] copy generated manifests/ content back to codeready-workspaces-operator before tagging" ./manifests/ || true
-			git push origin ${crw_repos_branch} || true
-		fi
-		# then push new tag
+	fi
+	if [[ $CRW_TAG ]]; then # push a new tag (or no-op if exists)
 		git tag ${CRW_TAG} || true
 		git push origin ${CRW_TAG} || true
 	fi
-	cd ..
+	popd >/dev/null || exit 1
 done
 
 # cleanup
