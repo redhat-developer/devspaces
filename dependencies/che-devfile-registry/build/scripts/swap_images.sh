1c702d0c9187f0f11203becf7807aa934f158b39
Nick Boldt <nboldt@redhat.com>
2021-01-21 11:31:05 -0500

    CRW-1184 :: split XP1 devfile into XP1 (s390x + ppc64le) + XP2 (amd64) (#410)
    * CRW-1184 add devfile for XP2 (x86_64 only); rename 00_java-microprofile -> 00_java-microprofile-xp1 (ppc64le and s390x only)
    
    Change-Id: Ibfd033d58aa7149ebc45479d7486f338a4d74cd8
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * update to latest registry.redhat.io/jboss-eap-7/eap-xp1-openj9-11-openshift-rhel8:1.0.2-5.1604581793; remove toggle in build/scripts/swap_images.sh
    
    Change-Id: Ic0e114ff06e5373ba88ab89e694f77f8012f0a3c
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/swap_images.sh
+++ dependencies/che-devfile-registry/build/scripts/swap_images.sh
@@ -14,8 +14,6 @@ devfiles=$($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT")
 # Note: optional -f flag will force this transformation even on an incompatible architecture,
 # so we can call this script from crw-operator/build/scripts/insert-related-images-to-csv.sh
 if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then 
-    # ensure to use full tag.   ex. 1.0.2-5 instead of 1.0 or 1.0-10 as tags for x/z/p may differ on various registries when using find_image.sh
-    sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0.2-5|g' $devfiles
     sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $devfiles
     sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $devfiles
 fi
