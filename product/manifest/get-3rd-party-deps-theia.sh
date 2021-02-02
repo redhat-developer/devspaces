66c126267d53277a329f188878125dec70ed6bfe
Nick Boldt <nboldt@redhat.com>
2021-02-02 18:17:46 -0500

    CRW-1378 fix get-3rd-party-deps-theia.sh so... (#420)
    * CRW-1378 fix get-3rd-party-deps-theia.sh so we actually collect a manifest (as we did in 2.2)
    
    Change-Id: I2009519468f806f9904abcbb870831cc936cd5ea
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * cleanup
    
    Change-Id: Ia2ad829b5f4ec39a03fca9f62c46d50dfa6735ff
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/manifest/get-3rd-party-deps-theia.sh
+++ product/manifest/get-3rd-party-deps-theia.sh
@@ -2,10 +2,12 @@
 
 # script to generate a manifest of all the 3rd party deps not built in OSBS, but built in Jenkins or imported from upstream community.
 
+JENKINS=https://main-jenkins-csb-crwqe.apps.ocp4.prod.psi.redhat.com/job/CRW_CI/job
+
 MIDSTM_BRANCH=""
 usage () 
 {
-    echo "Usage: $0 -b crw-2.y-rhel-8 -v 2.y.0"
+    echo "Usage: $0 -b $(git rev-parse --abbrev-ref HEAD) -v 2.y.0"
     exit
 }
 # commandline args
@@ -36,32 +38,37 @@ function log () {
 	echo "$1" | tee -a ${LOG_FILE}
 }
 
-rm -f ${MANIFEST_FILE} ${MANIFEST_FILE}.2 ${MANIFEST_FILE}.3 ${LOG_FILE}
-curl -sSL -o ${MANIFEST_FILE} https://codeready-workspaces-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/view/CRW_CI/view/Pipelines/job/crw-theia-sources_${CRW_VERSION}/lastSuccessfulBuild/consoleText
-for d in $(cat ${MANIFEST_FILE} | egrep "https://|http://"); do 
-	echo $d | egrep "https://|http://" >> ${MANIFEST_FILE}.2
+rm -f ${MANIFEST_FILE} ${MANIFEST_FILE}.2 ${LOG_FILE}
+echo "Parsing ${JENKINS}/crw-theia-sources_${CRW_VERSION}/lastSuccessfulBuild/consoleText ..."
+curl -sSL -o ${MANIFEST_FILE} ${JENKINS}/crw-theia-sources_${CRW_VERSION}/lastSuccessfulBuild/consoleText
+CHE_THEIA_BRANCH=$(grep "build.include" ${MANIFEST_FILE} | sort -u | grep curl | sed -r -e "s#.+che-theia/(.+)/build.include#\1#") # 7.yy.x
+for d in $(cat ${MANIFEST_FILE} | grep -E "https://|http://"); do 
+	echo $d | grep -E "https://|http://" >> ${MANIFEST_FILE}.2
 	echo -n "."
 done
 echo ""
-cat ${MANIFEST_FILE}.2 | uniq | sort | egrep -v "${EXCLUDE_LINES}" | uniq | sort > ${MANIFEST_FILE}
+cat ${MANIFEST_FILE}.2 | uniq | sort | grep -E -v "${EXCLUDE_LINES}" | uniq | sort > ${MANIFEST_FILE}
 rm -f ${MANIFEST_FILE}.2
 
-# TODO get brew builds' logs too
+TMPDIR=$(mktemp -d)
+pushd $TMPDIR >/dev/null || exit
+	git clone git@github.com:eclipse/che-theia.git 
+	cd che-theia || exit
+		git fetch || true
+		git checkout --track origin/${CHE_THEIA_BRANCH}
+		git pull origin ${CHE_THEIA_BRANCH}
+	cd ..
 
-if [[ ! -d che-theia ]]; then 
-	git clone git@github.com:eclipse/che-theia.git
-else
-	cd che-theia || exit; git pull origin ${MIDSTM_BRANCH}; cd ..
-fi
-
-Dockerfiles="$(find che-theia -name Dockerfile | egrep -v "${EXCLUDE_FILES}")"
-for df in $Dockerfiles; do
-	echo "== $df ==" | tee -a ${LOG_FILE}
-	for line in $(cat ${MANIFEST_FILE}) openjdk " python" "Python-" "pip install"; do
-		grep "$line" $df | egrep -v "${EXCLUDE_LINES2}" | tee -a ${LOG_FILE}
+	Dockerfiles="$(find che-theia -name Dockerfile | grep -E -v "${EXCLUDE_FILES}")"
+	for df in $Dockerfiles; do
+		echo "== $df ==" | tee -a ${LOG_FILE}
+		for line in $(cat ${MANIFEST_FILE}) openjdk " python" "Python-" "pip install"; do
+			grep "$line" $df | grep -E -v "${EXCLUDE_LINES2}" | tee -a ${LOG_FILE}
+		done
+		echo "" | tee -a ${LOG_FILE}
 	done
-	echo "" | tee -a ${LOG_FILE}
-done
+popd >/dev/null || exit
+rm -fr $TMPDIR
 
 ##################################
 
