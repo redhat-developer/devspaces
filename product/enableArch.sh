2f81f044e3693f8d6859320f4e7a082b807976f0
nickboldt <nboldt@redhat.com>
2021-02-10 21:34:09 -0500

    bump to 2.7
    Change-Id: Idd6fcf5bee0d32f330742025458827f0a09a5ec2
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/enableArch.sh
+++ product/enableArch.sh
@@ -1,20 +1,39 @@
-#!/bin/bash
-set -e 
+#!/bin/bash -e
+#
+# Copyright (c) 2020-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# Utility script to uncomment a previously commented-out arch in a container.yaml file
+# This script has questionable usefulness, so YMMV. Might delete it some day. ~nboldt
 
-DWNSTRM_BRANCH="crw-2.6-rhel-8"
+DWNSTM_BRANCH="crw-2.7-rhel-8"
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-b') DWNSTM_BRANCH="$2"; shift 1;;
+    *) ARCH="$1"
+  esac
+  shift 1
+done
+
 usage()
 {
     echo "
-Usage: $0 [arch-to-enable]
-Example: $0 ppc64le"
+Usage: $0 [-b crw-branch] [arch-to-enable]
+Example: $0 -b ${DWNSTM_BRANCH} ppc64le"
 }
-if [[ ! $1 ]]; then usage; exit; fi
-
-ARCH=$1
+if [[ ! $ARCH ]]; then usage; exit; fi
 
 for d in . */; do 
     if [[ -f $d/container.yaml ]]; then 
-        echo; if [[ $d == "." ]]; then echo "== $(basename `pwd`) =="; else echo "== $d =="; fi
+        echo; if [[ $d == "." ]]; then echo "== $(basename $(pwd)) =="; else echo "== $d =="; fi
         cd $d
             grep -E " - ${ARCH}" -r || true
             if [[ $(grep -E "^ *# *- ${ARCH}" -r) ]]; then
