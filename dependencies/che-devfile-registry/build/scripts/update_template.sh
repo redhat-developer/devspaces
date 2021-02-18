2bd5e9876c61334c6b81c37fb8d95f62fb9d7e70
Anatolii Bazko <abazko@redhat.com>
2021-02-18 10:21:31 +0200

    Update OpenShift templates
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

---
+++ dependencies/che-devfile-registry/build/scripts/update_template.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# List all images referenced in meta.yaml files
+#
+
+set -e
+
+unset TEMPLATE
+
+MIDSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+DEFAULT_TAG=${MIDSTM_BRANCH#*-}; DEFAULT_TAG=${DEFAULT_TAG%%-*};
+
+usage () {
+    echo "Usage:   $0 -t TEMPLATE"
+    echo "Options:
+    --crw-version ${DEFAULT_TAG}     (compute from MIDSTM_BRANCH if not set)
+      "
+    exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-t') TEMPLATE="$2"; shift 1;;
+    '--help'|'-h') usage;;
+  esac
+  shift 1
+done
+
+if [[ -z "${TEMPLATE+x}" ]]; then usage; fi
+
+sed -i \
+    -e "s|Eclipse Che|CodeReady Workspaces|g" \
+    -e "s|CHE_|CRW_|g" \
+    -e "s|quay.io/eclipse/che-devfile-registry|registry.redhat.io/codeready-workspaces/devfileregistry-rhel8|g" \
+    -e "s|nightly|\'${DEFAULT_TAG}\'|g" \
+    -e "s|che|codeready|g" \
+    ${TEMPLATE}
