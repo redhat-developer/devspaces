1fd2525157d39c961b8e2f88e95797e2eff59445
Nick Boldt <nboldt@redhat.com>
2021-02-18 13:16:13 -0500

    CRW-1475 remove obsolete machine exec and theia... (#431)
    * rename bump_memory to swap_theia_memory, to align its name to ther arch-specific swap_* scripts
    
    Change-Id: I4e501a1724c62df4a6f338f1690bed0cce089001
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * fix up copyrights
    
    Change-Id: Ia4b73a665751124418cda4a3aa52a0d96729cdf4
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * remove obsolete machine exec and theia plugins
    
    Change-Id: I4c47bcda86ea74efa6702bdf6e9835f63f04ce0e
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/swap_images.sh
+++ dependencies/che-devfile-registry/build/scripts/swap_images.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Copyright (c) 2020-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
 #
