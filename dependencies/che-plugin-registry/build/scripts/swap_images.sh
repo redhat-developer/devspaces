2ccbd4ee1e10da916b6e2352496d6d67337bbe77
nickboldt <nboldt@redhat.com>
2021-02-18 13:09:58 -0500

    fix up copyrights
    Change-Id: Ia4b73a665751124418cda4a3aa52a0d96729cdf4
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/swap_images.sh
+++ dependencies/che-plugin-registry/build/scripts/swap_images.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Copyright (c) 2020-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
 #
@@ -9,13 +12,13 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 YAML_ROOT="$1"
 
-devfiles=$($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT")
+metayamls=$($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT")
 
 # Note: optional -f flag will force this transformation even on an incompatible architecture,
 # so we can call this script from crw-operator/build/scripts/insert-related-images-to-csv.sh
 if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then 
-    # ensure to use full tag.   ex. 1.0-12 instead of 1.0--as 1.0 might be different on various registries when using find_image.sh
-    sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0-12|g' $devfiles
-    sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $devfiles
-    sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $devfiles
+    # use full tag, eg. 1.0-12 instead of 1.0, as 1.0 might resolve to different tag/digest when using find_image.sh
+    sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0-12|g' $metayamls
+    sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $metayamls
+    sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $metayamls
 fi
