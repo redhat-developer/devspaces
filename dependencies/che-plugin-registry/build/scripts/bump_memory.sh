ca89fe687a1022fda561beafd7066ab4746bd3d4
ghatwala <ghatwala@us.ibm.com>
2021-02-18 20:57:23 +0530

    copyright header
    copyright header

--- dependencies/che-plugin-registry/build/scripts/bump_memory.sh
+++ dependencies/che-plugin-registry/build/scripts/bump_memory.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#
+# Copyright (c) 2018-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
 #
 # SPDX-License-Identifier: EPL-2.0
-
 # script invoked for pluginregistry to bump memory for theia on power only - https://issues.redhat.com/browse/CRW-1475
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
