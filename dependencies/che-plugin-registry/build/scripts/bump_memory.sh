f60cfd210fa944733b3de602fa2094e8ed43853f
ghatwala <ghatwala@us.ibm.com>
2021-02-18 11:00:51 +0000

    For https://issues.redhat.com/browse/CRW-1475 bumping up theia memory in plugin registry 1. As per above JIRA , we needed more memory in plugin registry image for "che-plugin-registry/v3/plugins/eclipse/che-theia/next/meta.yaml" 2. Invoke new script bump_memory.sh for power in rhel.dockerfile Signed-off-by: Amit Ghatwal <ghatwala@us.ibm.com>

---
+++ dependencies/che-plugin-registry/build/scripts/bump_memory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+# script invoked for pluginregistry to bump memory for theia on power only - https://issues.redhat.com/browse/CRW-1475
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+YAML_ROOT="$1"
+
+metayaml=$($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT"/plugins/eclipse/che-theia/next)
+
+# we bump up memory for che-theia's meta.yaml only for power 
+if [[ "$(uname -m)" == "ppc64le" ]]; then
+   sed -E -i 's|memoryLimit: "512M"|memoryLimit: "3Gi"|g' $metayaml
+fi
+
