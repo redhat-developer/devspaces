895b53196c2cb0cef04d040087a483e9a80c4642
Anatolii Bazko <abazko@redhat.com>
2021-02-22 13:01:50 +0200

    Update templates and sync scripts
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/update_template.sh
+++ dependencies/che-plugin-registry/build/scripts/update_template.sh
@@ -11,35 +11,59 @@
 #
 
 set -e
+set -x
 
-unset TEMPLATE
+unset SOURCE_TEMPLATE
+unset CSV_VERSION
+unset CRW_VERSION
 
 MIDSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-DEFAULT_TAG=${MIDSTM_BRANCH#*-}; DEFAULT_TAG=${DEFAULT_TAG%%-*};
-[[ ${DEFAULT_TAG} == "2" ]] && DEFAULT_TAG="latest"
+CRW_VERSION=${MIDSTM_BRANCH#*-}
+DOCKER_IMAGE="registry.redhat.io/codeready-workspaces/pluginregistry-rhel8"
 
 usage () {
-    echo "Usage:   $0 -t TEMPLATE"
+    echo "Usage:     $0 -s SOURCE_TEMPLATE"
+    echo "Example:   $0 -s crw-plugin-registry.yaml -i ${DOCKER_IMAGE} -t 2.y"
     echo "Options:
-    --crw-version ${DEFAULT_TAG}     (compute from MIDSTM_BRANCH if not set)
+    -t Codeready Workspaces version (compute from MIDSTM_BRANCH if not set)
+    -i Codeready Workspaces plugin registry image (default to ${DOCKER_IMAGE})
+    --help, -h            help
       "
     exit 1
 }
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    '-t') TEMPLATE="$2"; shift 1;;
+    '-s') SOURCE_TEMPLATE="$2"; shift 1;;
+    '-t') CRW_VERSION="$2"; shift 1;; # 2.y
+    '-i') DOCKER_IMAGE="$2"; shift 1;; # 2.y
     '--help'|'-h') usage;;
   esac
   shift 1
 done
 
-if [[ -z "${TEMPLATE+x}" ]]; then usage; fi
+DEFAULT_TAG=${CRW_VERSION%%-*}
+[[ ${DEFAULT_TAG} == "2" ]] && DEFAULT_TAG="nightly"
+
+if [[ -z "${SOURCE_TEMPLATE}" ]]; then usage; fi
 
 sed -i \
     -e "s|Eclipse Che|CodeReady Workspaces|g" \
     -e "s|CHE_|CRW_|g" \
-    -e "s|quay.io/eclipse/che-plugin-registry|registry.redhat.io/codeready-workspaces/pluginregistry-rhel8|g" \
-    -e "s|nightly|\'${DEFAULT_TAG}\'|g" \
     -e "s|che|codeready|g" \
-    ${TEMPLATE}
+    -e "s|Che|Codeready|g" \
+    ${SOURCE_TEMPLATE}
+
+yq -ryiY "(.parameters[] | select(.name == \"IMAGE\") | .value ) = \"${DOCKER_IMAGE}\"" ${SOURCE_TEMPLATE}
+yq -ryiY "(.parameters[] | select(.name == \"IMAGE\") | .description ) = \"CodeReady Workspaces plugin registry Docker image. Defaults to ${DOCKER_IMAGE}\"" ${SOURCE_TEMPLATE}
+yq -ryiY "(.parameters[] | select(.name == \"IMAGE_TAG\") | .value ) = \"${DEFAULT_TAG}\"" ${SOURCE_TEMPLATE}
+
+echo "$(echo '#
+# Copyright (c) 2018-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+---' | cat - ${SOURCE_TEMPLATE})" > ${SOURCE_TEMPLATE}
