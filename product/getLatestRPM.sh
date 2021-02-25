52b2baa5701858511c47771a982d3d482b8dc4fe
nickboldt <nboldt@redhat.com>
2021-02-25 17:07:37 -0500

    add -q flag and simplify passing in base url for the rpm search; add http://rhsm-pulp.corp.redhat.com alternative uRL (seems to work better?); add new groovy methods updateHelmRpms, updateOCRpms and updateRpms
    Change-Id: Ib8482511ef345c96dc4ddafc5587bac3e94d9968
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/getLatestRPM.sh
+++ product/getLatestRPM.sh
@@ -13,13 +13,16 @@
 # TODO read this from container.yaml next to Dockerfile?, eg., ARCHES=$(yq -r '.platforms.only[]' container.yaml)
 ARCHES="x86_64 s390x ppc64le"
 
+QUIET=0 # if 0, echo what's happening; if 1, echo only the new version found and replaced
+
 # collect params
 while [[ "$#" -gt 0 ]]; do
   case $1 in
   '-r') RPM_PATTERN="$2"; shift 1;; # eg., openshift-clients or helm
   '-s') SOURCE_DIR="$2"; shift 1;; # dir to search for Dockerfiles
   '-a') ARCHES="$ARCHES $2"; shift 1;; # use space-separated list of arches, or use multiple -a flags
-  '-u') BASE_URL="$2"; shift 1;; # eg., http://pulp.dist.prod.ext.phx2.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.6/os/Packages/o/
+  '-u') BASE_URL="$2"; shift 1;; # eg., http://pulp.dist.prod.ext.phx2.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.7
+  '-q') QUIET=1; shift 0;;
   '-h') usage;;
   esac
   shift 1
@@ -30,8 +33,10 @@ usage () {
 Usage: 
   $0 -s SOURCE_DIR -r RPM_PATTERN  -u BASE_URL -a 'ARCH1 ... ARCHN' 
 Example: 
-  $0 -s /path/to/dockerfiles/ -r openshift-clients-4 -u http://pulp.dist.prod.ext.phx2.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.6/os/Packages/o/
-
+  $0 -s /path/to/dockerfiles/ -r openshift-clients-4 -u http://pulp.dist.prod.ext.phx2.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.7
+  $0 -s /path/to/dockerfiles/ -r helm-3              -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/ocp-tools/4.7
+Options:
+  -q quieter output; only reports changed rpm version or 0 if failure
 "
 }
 if [[ ! ${RPM_PATTERN} ]] || [[ ! ${SOURCE_DIR} ]] || [[ ! ${BASE_URL} ]]; then usage; fi
@@ -42,9 +47,17 @@ dockerfiles=$(find ${SOURCE_DIR} -type f -name "*ockerfile")
 # compute latest version that spans all ARCHES
 declare -A versions=()
 for arch in $ARCHES; do
-    echo "[INFO] Load $arch versions from ${BASE_URL/basearch/${arch}}"
-    thisarchversions=$(curl --retry 10 -sSLo- ${BASE_URL/basearch/${arch}} | grep -E "${RPM_PATTERN}" | sed -r -e "s#.+href=\"(.+).${arch}.rpm\">.+#\1#")
-    if [[ ! $thisarchversions ]]; then echo "[ERROR] failed to load ${BASE_URL/basearch/${arch}} !"; exit 1; fi
+    checkURL=${BASE_URL/basearch/${arch}}/os/Packages/${RPM_PATTERN:0:1}/
+    if [[ $QUIET -eq 0 ]]; then echo "[INFO] Load $arch versions from ${checkURL}"; fi
+    thisarchversions=$(curl --retry 10 -sSLo- ${checkURL} | grep -E "${RPM_PATTERN}" | sed -r -e "s#.+href=\"(.+).${arch}.rpm\">.+#\1#")
+    if [[ ! $thisarchversions ]]; then 
+        if [[ $QUIET -eq 0 ]]; then 
+            echo "[ERROR] failed to load ${checkURL} !"
+        else
+            echo "0"
+        fi
+        exit 1
+    fi
     for thisver in $thisarchversions; do
     # echo "[DEBUG] Found ${thisver} for ${arch}"
     versions[${thisver}]+="$arch "
@@ -70,10 +83,12 @@ for d in $dockerfiles; do
   # echo "[DEBUG] Checking $d ..."
   if [[ $(grep -E "${RPM_PATTERN}" $d) ]]; then
     # echo "[Debug] Dockerfile contains ${RPM_PATTERN} ..."
-    echo "[INFO] $RPM_PATTERN -> $newVersion in $d"
+    if [[ $QUIET -eq 0 ]]; then echo "[INFO] $RPM_PATTERN -> $newVersion in $d"; fi
     sed -i $d -r -e "s#(/| )(${RPM_PATTERN}[^ ]+.el8)#\1$newVersion#g"
   fi
 done
 
-# commit changes
+# TODO update content_sets.* files too, if ocp version has changed
 
+# just echo the version we found and changed if we're in quiet mode
+if [[ $QUIET -eq 1 ]]; then echo $newVersion; fi
