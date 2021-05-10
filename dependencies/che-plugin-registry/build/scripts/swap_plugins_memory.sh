5a02cc79dde212e0c54d4dd20bfece27d4b6c803
Eric Williams <ericwill@redhat.com>
2021-05-10 16:07:04 -0400

    Fix shellcheck and EOF warnings
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/swap_plugins_memory.sh
+++ dependencies/che-plugin-registry/build/scripts/swap_plugins_memory.sh
@@ -17,30 +17,31 @@ replaceField()
   yamlFile="$1"
   updateName="$2"
   updateVal="$3"
-  yq -Y --arg updateName "${updateName}" --arg updateVal "${updateVal}" ${updateName}' = $updateVal' ${yamlFile} > ${yamlFile}.2
-  mv ${yamlFile}.2 ${yamlFile}
+  # shellcheck disable=SC2086
+  yq -Y --arg updateName "${updateName}" --arg updateVal "${updateVal}" ${updateName}" = $updateVal" ${yamlFile} > ${yamlFile}.2
+  mv "${yamlFile}".2 "${yamlFile}"
 }
 
 # Note: optional -f flag will force this transformation even on an incompatible architecture for testing purposes
 if [[ "$(uname -m)" == "ppc64le" ]] || [[ "$2" == "-f" ]]; then 
 
    # CRW-1475
-   for metayaml in $($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT"/plugins/eclipse/che-theia); do
-      replaceField $metayaml '.spec.containers[].memoryLimit' "2Gi"
+   for metayaml in $("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT"/plugins/eclipse/che-theia); do
+      replaceField "$metayaml" '.spec.containers[].memoryLimit' "2Gi"
    done
 
    # CRW-1633
-   for metayaml in $($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT"/plugins/redhat/vscode-camelk); do
-      replaceField $metayaml '.spec.containers[].memoryLimit' "1.5Gi"
+   for metayaml in $("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT"/plugins/redhat/vscode-camelk); do
+      replaceField "$metayaml" '.spec.containers[].memoryLimit' "1.5Gi"
    done
 
    # CRW-1634
-   for metayaml in $($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT"/plugins/redhat/vscode-openshift-connector); do
-      replaceField $metayaml '.spec.containers[].memoryLimit' "2.5Gi"
+   for metayaml in $("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT"/plugins/redhat/vscode-openshift-connector); do
+      replaceField "$metayaml" '.spec.containers[].memoryLimit' "2.5Gi"
    done
 
    # CRW-1635
-   for metayaml in $($SCRIPT_DIR/list_yaml.sh "$YAML_ROOT"/plugins/ms-python/python); do
-      replaceField $metayaml '.spec.containers[].memoryLimit' "1Gi"
+   for metayaml in $("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT"/plugins/ms-python/python); do
+      replaceField "$metayaml" '.spec.containers[].memoryLimit' "1Gi"
    done
 fi
