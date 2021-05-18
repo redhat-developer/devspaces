bb253f6feeb475db59c796d3571c491db5244975
Eric Williams <ericwill@redhat.com>
2021-05-18 09:28:36 -0400

    Plugin registry script updates (#495)
    * Script updates
    
    * Copy che-*.yaml files into Dockerfile
    * Fix list_referenced_images.sh to use new che-*.yaml files
    * Fix swap_images.sh and list_yamls.sh to use new che-*.yaml files
    
    Part of CRW-1824
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>
    
    * Do not break other scripts, split list_yamls.sh into two scripts
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>

---
+++ dependencies/che-plugin-registry/build/scripts/list_che_yaml.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+find "$1" -maxdepth 1 -name 'che-*.yaml'
--- dependencies/che-plugin-registry/build/scripts/swap_images.sh
+++ dependencies/che-plugin-registry/build/scripts/swap_images.sh
@@ -12,13 +12,15 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 YAML_ROOT="$1"
 
-metayamls=$("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT")
+cheYamls=$("$SCRIPT_DIR"/list_che_yaml.sh "$YAML_ROOT")
 
 # Note: optional -f flag will force this transformation even on an incompatible architecture,
 # so we can call this script from crw-operator/build/scripts/insert-related-images-to-csv.sh
-if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then 
-    # use full tag, eg. 1.0-12 instead of 1.0, as 1.0 might resolve to different tag/digest when using find_image.sh
-    sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0-12|g' "$metayamls"
-    sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' "$metayamls"
-    sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' "$metayamls"
+if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
+    for yaml in $cheYamls; do
+        # use full tag, eg. 1.0-12 instead of 1.0, as 1.0 might resolve to different tag/digest when using find_image.sh
+        sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0-12|g' "$yaml"
+        sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' "$yaml"
+        sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' "$yaml"
+    done
 fi
