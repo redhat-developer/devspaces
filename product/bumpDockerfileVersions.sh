128a35f9321c2bcf9c64dba7813fd121cee4cb27
nickboldt <nboldt@redhat.com>
2021-07-07 14:27:48 -0300

    script to bump versions in dockerfiles to latest so we can trigger builds of 2.x when jenkins is hosed
    Change-Id: I3dea0e9278036340eafb7b8acccd2a7a97fa5155
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ product/bumpDockerfileVersions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# script to bump .x branch to the latest version, in case Jenkins is hosed and can't do the sync jobs
+
+usage() {
+    echo "
+Usage: $0 -b SOURCE_BRANCH -v CSV_VERSION
+Example: $0 -b crw-2-rhel-8 -v 2.yy
+"
+    exit
+}
+
+# commandline args
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-b') SOURCE_BRANCH="$2"; shift 1;; # this flag will create branches instead of using branches to create tags
+    '-v') CSV_VERSION="$2"; shift 1;; # 2.y.0
+  esac
+  shift 1
+done
+
+if [[ ! $CSV_VERSION ]]; then usage; fi
+if [[ ! $SOURCE_BRANCH ]]; then usage; fi
+
+for d in $(find -H . -maxdepth 1 -type d -a -not -name "*alt" -a -not -name "." | sort); do 
+    echo "Chore: update $d/dockerfile to $CSV_VERSION in branch $SOURCE_BRANCH ... "
+    cd "$d" || exit
+    { 
+        git checkout $SOURCE_BRANCH
+        sed -i Dockerfile -r -e 's#version=".+"#version="'${CSV_VERSION}'"#g'
+        git commit -s -m "chore: update dockerfile to ${CSV_VERSION}" Dockerfile
+        git pull origin $SOURCE_BRANCH
+        git push origin $SOURCE_BRANCH & 
+    }
+    cd ..
+done
