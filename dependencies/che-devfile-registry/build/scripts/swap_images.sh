e8b80ada010e44cc0ad0c2d41a356173428adfe4
nickboldt <nboldt@redhat.com>
2021-07-12 11:57:08 -0300

    debug why we're doing j9 swaps on x86 image builds
    Change-Id: I91de17dd8969ec8a5f65193e4e3b6cb20fed8c92
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/swap_images.sh
+++ dependencies/che-devfile-registry/build/scripts/swap_images.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 
-# script common across operator-metadata, devfileregistry, and pluginregistry
+# script common to devfileregistry and pluginregistry; see also crw-operator/build/scripts/swap_images.sh
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 YAML_ROOT="$1"
@@ -20,4 +20,6 @@ if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
     sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $devfiles
     sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $devfiles
     sed -E -i 's|eap-xp2-openjdk11-openshift-rhel8:.*|eap-xp2-openj9-11-openshift-rhel8:2.0|g' $devfiles
+else
+    echo "[INFO] nothing to do on $(uname -m); only swap openjdk for openj9 images on s390x and ppc64le arches"
 fi
