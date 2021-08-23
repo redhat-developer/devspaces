ef78223d60b88a41f160cd1c9ceec5f82af74649
nickboldt <nboldt@redhat.com>
2021-08-23 17:47:55 -0300

    fix project path
    Change-Id: If5071362d8b3c4a0b57e86f4168a943cd23a4d0f
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- product/uploadAssetsToGHRelease.sh
+++ product/uploadAssetsToGHRelease.sh
@@ -44,7 +44,7 @@ while [[ "$#" -gt 0 ]]; do
   shift 1
 done
 
-curl -XPOST -H 'Authorization:token '"${GITHUB_TOKEN}" --data '{"tag_name": "'"${CSV_VERSION}"'", "target_commitish": "'"${MIDSTM_BRANCH}"'", "name": "'"${CSV_VERSION}"'-ci-assets", "body": "Container build asset files for '"${CSV_VERSION}"'", "draft": true, "prerelease": true}' https://api.github.com/repos/redhat-developer/codeready-workspaces-chectl/releases > "/tmp/${CSV_VERSION}"
+curl -XPOST -H 'Authorization:token '"${GITHUB_TOKEN}" --data '{"tag_name": "'"${CSV_VERSION}"'", "target_commitish": "'"${MIDSTM_BRANCH}"'", "name": "'"${CSV_VERSION}"'-ci-assets", "body": "Container build asset files for '"${CSV_VERSION}"'", "draft": true, "prerelease": true}' https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases > "/tmp/${CSV_VERSION}"
 # Extract the id of the release from the creation response
 RELEASE_ID="$(jq -r .id /tmp/${CSV_VERSION})"
 
