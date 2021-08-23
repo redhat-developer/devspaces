f5e2a570eb636b01a9d9b6f71dfd5ae3d30f0a90
Nick Boldt <nboldt@redhat.com>
2021-08-23 17:01:59 -0400

    feat: CRW-2188 CRW-2081 new script to upload... (#575)
    * feat: CRW-2188 CRW-2081 new script to upload files to GH product/uploadAssetsToGHRelease.sh
    
    Change-Id: Icb22da02867539bc027a9106d32efc446840aca2
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * set better default for MIDSTM_BRANCH; allow override too
    
    Change-Id: I44fb4310cd039d549734643e97efaf80bcad74ee
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * fix project path
    
    Change-Id: If5071362d8b3c4a0b57e86f4168a943cd23a4d0f
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ product/uploadAssetsToGHRelease.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# push locally built asset* files to github
+
+set -e
+
+# defaults
+CSV_VERSION=2.y.0 # csv 2.y.0
+PREFIX=""
+fileList=""
+
+MIDSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "crw-2-rhel-8")
+if [[ ${MIDSTM_BRANCH} != "crw-"*"-rhel-"* ]]; then MIDSTM_BRANCH="crw-2-rhel-8"; fi
+
+usage () {
+    echo "
+Usage:   $0 -v [CRW CSV_VERSION] --prefix [unique prefix] file1.tar.gz file2.tar.gz
+Example: $0 -v 2.y.0 --prefix crw-theia
+"
+    exit
+}
+
+if [[ $# -lt 1 ]]; then usage; fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-v') CSV_VERSION="$2"; shift 1;;
+    '-b') MIDSTM_BRANCH="$2"; shift 1;;
+    '-ght') GITHUB_TOKEN="$2"; shift 1;;
+    '--prefix') PREFIX="$2"; shift 1;;
+    '--help'|'-h') usage;;
+    *) fileList="$1";;
+  esac
+  shift 1
+done
+
+curl -XPOST -H 'Authorization:token '"${GITHUB_TOKEN}" --data '{"tag_name": "'"${CSV_VERSION}"'", "target_commitish": "'"${MIDSTM_BRANCH}"'", "name": "'"${CSV_VERSION}"'-ci-assets", "body": "Container build asset files for '"${CSV_VERSION}"'", "draft": true, "prerelease": true}' https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases > "/tmp/${CSV_VERSION}"
+# Extract the id of the release from the creation response
+RELEASE_ID="$(jq -r .id /tmp/${CSV_VERSION})"
+
+# upload artifacts for each platform 
+for fileName in $fileList; do
+    if [[ ${PREFIX} ]]; then 
+        fileToPush="${PREFIX}-${fileName}"
+    else 
+        fileToPush="${fileName}"
+    fi
+    curl -XPOST -H 'Authorization:token '"$GITHUB_TOKEN" -H 'Content-Type:application/octet-stream' --data-binary @"${fileToPush}" "https://uploads.github.com/repos/redhat-developer/codeready-workspaces-images/releases/${RELEASE_ID}/assets?name=${fileToPush}"
+done
