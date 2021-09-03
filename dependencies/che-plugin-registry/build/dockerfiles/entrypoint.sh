9301e2dcfe23f829ae87a44c07cba22d84f1d11a
Sun Seng David TAN <sutan@redhat.com>
2021-09-03 14:24:34 +0200

    fix(plugin-registry): Should replace 2.11 image references in che-machine-exec-plugin devfile.yaml and theia-ide devfile.yaml with RELATED_IMAGE env vars (#581)
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
+++ dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
@@ -38,6 +38,39 @@ METAS_DIR="${METAS_DIR:-${DEFAULT_METAS_DIR}}"
 IMAGE_REGEX="([[:space:]>-]*[\r]?[[:space:]]*[\"']?)([._:a-zA-Z0-9-]*)/([._a-zA-Z0-9-]*)/([._a-zA-Z0-9-]*)(@sha256)?:([._a-zA-Z0-9-]*)([\"']?)"
 
 function run_main() {
+
+    extract_and_use_related_images_env_variables_with_image_digest_info
+
+    update_container_image_references
+
+    # For testing, the images used for the che-theia, theia runtime, and machine-exec
+    # plugins can be overridden via the environment variables
+    #     CHE_PLUGIN_REGISTRY_THEIA_IMAGE
+    #     CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE
+    #     CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE
+    # If set, these env vars override all modifications
+    # (e.g. via CHE_DEVFILE_IMAGES_REGISTRY_URL)
+    #
+    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_IMAGE" ]; then
+    echo "Overriding Che Theia image with $CHE_PLUGIN_REGISTRY_THEIA_IMAGE"
+    sed -i -E "s|image:.*theia-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_IMAGE|" "${metas[@]}"
+    fi
+
+    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE" ]; then
+    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE"
+    sed -i -E "s|image:.*theia-endpoint-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE|" "${metas[@]}"
+    fi
+
+    if [ -n "$CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE" ]; then
+    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE"
+    sed -i -E "s|image:.*machineexec-rhel8.*|image: $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE|" "${metas[@]}"
+    fi
+
+    exec "${@}"
+
+}
+
+function extract_and_use_related_images_env_variables_with_image_digest_info() {
     # Extract and use env variables with image digest information.
     # Env variable name format: 
     # RELATED_IMAGES_(Image_name)_(Image_label)_(Encoded_base32_image_tag)
@@ -75,7 +108,7 @@ function run_main() {
         done
         echo "--------------------------------------------------------------"
 
-        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml')
+        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
         for meta in "${metas[@]}"; do
             readarray -t images < <(grep "image:" "${meta}" | sed -r "s;.*image:[[:space:]]*'?\"?([._:a-zA-Z0-9-]*/?[._a-zA-Z0-9-]*/[._a-zA-Z0-9-]*(@sha256)?:?[._a-zA-Z0-9-]*)'?\"?[[:space:]]*;\1;")
             for image in "${images[@]}"; do
@@ -124,7 +157,7 @@ function run_main() {
             done
             echo "--------------------------------------------------------------"
 
-            readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml')
+            readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
             for meta in "${metas[@]}"; do
                 readarray -t images < <(grep "image:" "${meta}" | sed -r "s;.*image:[[:space:]]*'?\"?([._:a-zA-Z0-9-]*/?[._a-zA-Z0-9-]*/[._a-zA-Z0-9-]*(@sha256)?:?[._a-zA-Z0-9-]*)'?\"?[[:space:]]*;\1;")
                 for image in "${images[@]}"; do
@@ -147,34 +180,6 @@ function run_main() {
             done
         fi
     fi
-
-    update_container_image_references
-
-    # For testing, the images used for the che-theia, theia runtime, and machine-exec
-    # plugins can be overridden via the environment variables
-    #     CHE_PLUGIN_REGISTRY_THEIA_IMAGE
-    #     CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE
-    #     CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE
-    # If set, these env vars override all modifications
-    # (e.g. via CHE_DEVFILE_IMAGES_REGISTRY_URL)
-    #
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_IMAGE" ]; then
-    echo "Overriding Che Theia image with $CHE_PLUGIN_REGISTRY_THEIA_IMAGE"
-    sed -i -E "s|image:.*theia-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_IMAGE|" "${metas[@]}"
-    fi
-
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE"
-    sed -i -E "s|image:.*theia-endpoint-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE|" "${metas[@]}"
-    fi
-
-    if [ -n "$CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE"
-    sed -i -E "s|image:.*machineexec-rhel8.*|image: $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE|" "${metas[@]}"
-    fi
-
-    exec "${@}"
-
 }
 
 function update_container_image_references() {
@@ -182,7 +187,7 @@ function update_container_image_references() {
     # We can't use the `-d` option for readarray because
     # registry.centos.org/centos/httpd-24-centos7 ships with Bash 4.2
     # The below command will fail if any path contains whitespace
-    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml')
+    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
     for meta in "${metas[@]}"; do
     echo "Checking meta $meta"
     # Need to update each field separately in case they are not defined.
