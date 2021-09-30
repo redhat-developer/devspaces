489bbc86f8524e15e98e638fc75676416c083ba8
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-09-30 14:59:53 -0700

    feat: switching uploadAssetsToGHRelease.sh to use (and install if necessary) gh cli

--- product/uploadAssetsToGHRelease.sh
+++ product/uploadAssetsToGHRelease.sh
@@ -44,58 +44,28 @@ while [[ "$#" -gt 0 ]]; do
   shift 1
 done
 
-curlWithTokenPost()
-{
-  curl -sSL -XPOST -H "Authorization:token ${GITHUB_TOKEN}" "$1" "$2" "$3"
-}
-
-curlWithToken()
-{
-  curl -sSL -H "Authorization:token ${GITHUB_TOKEN}" "$1" "$2" "$3"
-}
-
-curlWithTokenBinary()
-{
-  curl -sSL --http1.1 -H "Authorization:token ${GITHUB_TOKEN}" -H "Content-Type:application/octet-stream" --data-binary "$1" "$2" "$3"
-}
-
-getId()
-{
-    ID=""
-    loc_RELEASE_ID=$1
-    loc_fileToPush=$2
-    ID=$(curlWithToken -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases/${loc_RELEASE_ID}/assets | jq -r --arg fileToPush "${loc_fileToPush}" '.[] | select (.name=="'$loc_fileToPush'")|.id')
-}
-
-# check if existing release exists
-releases_URL="https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases"
-# shellcheck disable=2086
-RELEASE_ID=$(curlWithToken -H "Accept: application/vnd.github.v3+json" $releases_URL | jq -r --arg PREFIX "${PREFIX}" --arg CSV_VERSION "${CSV_VERSION}" '.[] | select(.name=="Assets for the '$CSV_VERSION' '$PREFIX' release")|.url' || true); RELEASE_ID=${RELEASE_ID##*/}
-if [[ -z $RELEASE_ID ]]; then 
-    curlWithTokenPost --data '{"tag_name": "'"${CSV_VERSION}-${PREFIX}-assets"'", "target_commitish": "'"${MIDSTM_BRANCH}"'", "name": "Assets for the '"${CSV_VERSION}"' '"${PREFIX}"' release", "body": "Container build asset files for '"${CSV_VERSION}"'", "draft": false, "prerelease": true}' $releases_URL > "/tmp/${CSV_VERSION}"
-    # Extract the id of the release from the creation response
-    RELEASE_ID="$(jq -r .id "/tmp/${CSV_VERSION}")"
+if [[ $(which gh | grep 'no') ]]; then 
+  #no GH CLI installed, install it
+  brew install gh
+  gh auth login --with-token ${GITHUB_TOKEN}
+else
+  #login again for safety
+  gh auth login --with-token ${GITHUB_TOKEN}
 fi
 
-# TODO: to avoid re-downloading and re-uploading files, can we store sha512sums and compare them to what's already in the pkgs.devel sources files?
+# check if existing release exists
+#RELEASE_ID=$(curlWithToken -H "Accept: application/vnd.github.v3+json" $releases_URL | jq -r --arg PREFIX "${PREFIX}" --arg CSV_VERSION "${CSV_VERSION}" '.[] | select(.name=="Assets for the '$CSV_VERSION' '$PREFIX' release")|.url' || true); RELEASE_ID=${RELEASE_ID##*/}
+if [[ $(gh release list | grep ${CSV_VERSION}) ]]; then
+  #no existing release, create it
+  gh release create "${CSV_VERSION}-${PREFIX}-assets" --target "${MIDSTM_BRANCH}" --title "Assets for the ${CSV_VERSION} ${PREFIX} release" --notes "Container build asset files for ${CSV_VERSION}" --prerelease
+fi
 
 # upload artifacts for each platform 
 for fileToPush in $fileList; do
     # attempt to upload a new file
     echo "Uploading new asset $fileToPush"
-    # TODO: what do we do if the upload failed, and instead of a valid asset, we get "Validation Failed" because "already_exists"?
-    # should we delete the release and create fresh assets?
-    if [[ $(curlWithTokenBinary @"${fileToPush}" -XPOST "https://uploads.github.com/repos/redhat-developer/codeready-workspaces-images/releases/${RELEASE_ID}/assets?name=${fileToPush}") != *"Failed"* ]]; then
-        getId $RELEASE_ID $fileToPush
-        echo "Uploaded new asset $ID to https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases/$RELEASE_ID/assets"
-    else
-        getId $RELEASE_ID $fileToPush
-        # TODO this doesn't work -- PATCH doesn't seem to support POSTing a new binary
-        if [[ $(curlWithTokenBinary @"${fileToPush}" -XPATCH "https://uploads.github.com/repos/redhat-developer/codeready-workspaces-images/releases/${RELEASE_ID}/assets/${ID}?name=${fileToPush}") ]]; then
-                getId $RELEASE_ID $fileToPush
-                echo "Updated asset $ID in https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases/$RELEASE_ID/assets/${ID}"
-        else
-            echo "ERROR: could not upload or update file $fileToPush to https://api.github.com/repos/redhat-developer/codeready-workspaces-images/releases/$RELEASE_ID/assets"
-        fi
-    fi
+    gh release upload "${CSV_VERSION}-${PREFIX}-assets" ${fileToPush} --clobber
 done
+
+#logout ¯\_(ツ)_/¯
+gh auth logout github.com 
