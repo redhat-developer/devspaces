8a8f2aef4b22343d1c466fd901f910a5e1e83323
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-10-06 07:26:01 -0700

    fix: flawed logic, would have always created new release

--- product/uploadAssetsToGHRelease.sh
+++ product/uploadAssetsToGHRelease.sh
@@ -48,7 +48,7 @@ export GITHUB_TOKEN=${GITHUB_TOKEN}
 
 # check if existing release exists
 #RELEASE_ID=$(curlWithToken -H "Accept: application/vnd.github.v3+json" $releases_URL | jq -r --arg PREFIX "${PREFIX}" --arg CSV_VERSION "${CSV_VERSION}" '.[] | select(.name=="Assets for the '$CSV_VERSION' '$PREFIX' release")|.url' || true); RELEASE_ID=${RELEASE_ID##*/}
-if [[ !$(hub release | grep ${CSV_VERSION}) ]]; then
+if [[ $(hub release | grep ${CSV_VERSION}) == "" ]]; then
   #no existing release, create it
   hub release create -t "${MIDSTM_BRANCH}" -m "Assets for the ${CSV_VERSION} ${PREFIX} release" -m "Container build asset files for ${CSV_VERSION}" --prerelease "${CSV_VERSION}-${PREFIX}-assets"
 fi
