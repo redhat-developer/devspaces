d4a2b92571b3a7dae164e7246e0a1e4310dbeab5
nickboldt <nboldt@redhat.com>
2021-11-03 15:46:13 -0300

    chore: minor refactoring for consistency and to add optional uname override to che-devfile-registry/build/scripts/swap_yamlfiles.sh
    Change-Id: Ieeb3c847a606847ed64270f3317dad84c6f8904b
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/swap_images.sh
+++ dependencies/che-plugin-registry/build/scripts/swap_images.sh
@@ -18,15 +18,15 @@ else
     cheYamls=$("$SCRIPT_DIR"/list_che_yaml.sh "$YAML_ROOT")
 fi
 
-# Note: optional -f flag will force this transformation even on an incompatible architecture,
-# so we can call this script from crw-operator/build/scripts/insert-related-images-to-csv.sh
-if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
+# for testing, support passing in uname as a 2nd param, eg., s390x or ppc64le
+if [[ $2 ]]; then arch="$2"; else arch="$(uname -m)"; fi
+
+# shellcheck disable=SC2086
+if [[ "$arch" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
     for yaml in $cheYamls; do
-        # use full tag, eg. 1.0-12 instead of 1.0, as 1.0 might resolve to different tag/digest when using find_image.sh
-        sed -E -i 's|eap-xp1-openjdk11-openshift-rhel8:.*|eap-xp1-openj9-11-openshift-rhel8:1.0-12|g' "$yaml"
         sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' "$yaml"
         sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' "$yaml"
     done
 else
-    echo "[INFO] nothing to do on $(uname -m); only swap openjdk for openj9 images on s390x and ppc64le arches"
+    echo "[INFO] nothing to do on $arch; only swap openjdk for openj9 images on s390x and ppc64le arches"
 fi
