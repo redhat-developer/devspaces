bc7e8df6ac2a51a9f5773a78ffc45b9d8f48ead8
nickboldt <nboldt@redhat.com>
2021-11-05 12:15:25 -0300

    CRW-2426 fix commandline flag handling for plugin reg swap_*.sh scripts
    Change-Id: Ic73de39f9bf044b1ab55d46de96db8f3ac1b2a6c
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    CRW-2426 fix commandline flag handling for plugin reg swap_*.sh scripts
    
    Change-Id: I1f61d2f2f9015ce5df93ac3bfb5e41151f79e65f
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/swap_images.sh
+++ dependencies/che-devfile-registry/build/scripts/swap_images.sh
@@ -10,15 +10,26 @@
 # script common to devfileregistry and pluginregistry; see also crw-operator/build/scripts/swap_images.sh
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-YAML_ROOT="$1"
+
+FORCE_SWAP=0
+arch="$(uname -m)"
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  '-f') FORCE_SWAP=1;; # force transformation even on an incompatible architecture for testing purposes
+  '-a') arch="$2"; shift 1;;
+  *) YAML_ROOT="$1";;
+  esac
+  shift 1
+done
 
 devfiles=$("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT")
 
-# for testing, support passing in uname as a 2nd param, eg., s390x or ppc64le
-if [[ $2 ]]; then arch="$2"; else arch="$(uname -m)"; fi
-
 # shellcheck disable=SC2086
-if [[ "$arch" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
+if [[ "$arch" != "x86_64" ]] || [[ $FORCE_SWAP -eq 1 ]]; then
+    echo -n "[INFO] swap openjdk for openj9 images on $arch"
+    if [[ $FORCE_SWAP -eq 1 ]]; then echo -n " (forced)"; fi
+    echo
     sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $devfiles
     sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $devfiles
     sed -E -i 's|eap-xp3-openjdk11-openshift-rhel8:.*|eap-xp3-openj9-11-openshift-rhel8:3.0|g' $devfiles
