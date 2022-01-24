951057627ab9cd5fc3e93c3c4b7ae8bec703f409
Igor Vinokur <ivinokur@redhat.com>
2022-01-24 13:15:27 +0200

    initial

--- dependencies/che-devfile-registry/build/scripts/list_yaml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-find "$1" -name devfile.yaml
