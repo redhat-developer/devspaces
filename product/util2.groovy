74a39b23a64e58c2458db363115aa38d79a82a03
Mykhailo Kuznietsov <mkuznets@redhat.com>
2022-02-07 18:35:16 +0200

    feat: add successful container build checker function (#686)
    * feat: add successful container build checker function
    
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>
    
    * Update util2.groovy
    
    fix typo, rename to hasSuccessfullyBuiltAllArches
    
    * Update util2.groovy
    
    tweak console output and comparison logic... better to watch for 4 successes for 3 arches, than to count successes and subtract one (eg., if 1 failed arch for a 3-arch build, will end up with 2-1 = 1, not 2 successes).
    
    Co-authored-by: Nick Boldt <nboldt@redhat.com>

--- product/util2.groovy
+++ product/util2.groovy
@@ -590,5 +590,19 @@ exit 0
   return statusCode > 1 ? false : true
 }
 
+boolean hasSuccessfullyBuiltAllArches(String containerYamlPath, String jobOutput) {
+  int containerBuildCount = sh(script: '''#!/bin/bash -xe
+    yq -r ".platforms.only | length" ''' + containerYamlPath, returnStdout: true).trim()
+    echo "Expected number of container builds (arches in container.yaml): "+containerBuildCount
+  int containerSuccessCount = jobOutput.count("build has finished successfully \\\\o/")
+    echo "Successful builds detected: "+containerBuildCount
+  // should get 1 per arch + 1 overall success, which is 1 more than list in container.yaml
+  if (containerSuccessCount > containerBuildCount) { 
+    return true
+  } else {
+    return false
+  }
+}
+
 // return this file's contents when loaded
 return this
