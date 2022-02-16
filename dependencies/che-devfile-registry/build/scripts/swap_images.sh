c2e7958f3d29002cd202292aeaad3591d28742c4
nickboldt <nboldt@redhat.com>
2022-02-16 17:06:07 -0400

    CRW-2729 disable openj9 swaps for plugin/devfile registry related to plugin-java* sidecars; leave in eap swaps for now
    Change-Id: Idf216d43d80c6557497a7287ca7bc0340bc7f37c
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/swap_images.sh
+++ dependencies/che-devfile-registry/build/scripts/swap_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2020-2021 Red Hat, Inc.
+# Copyright (c) 2020-2022 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -30,7 +30,7 @@ if [[ "$arch" != "x86_64" ]] || [[ $FORCE_SWAP -eq 1 ]]; then
     echo -n "[INFO] swap openjdk for openj9 images on $arch"
     if [[ $FORCE_SWAP -eq 1 ]]; then echo -n " (forced)"; fi
     echo
-    # TODO when we have eap-xp3-openjdk11-openshift-rhel8 for Z and P, disable this whole script
+    # TODO CRW-2750 CRW-2729 when we have eap-xp3-openjdk11-openshift-rhel8 for Z and P, disable this whole script
     sed -E -i 's|eap-xp3-openjdk11-openshift-rhel8:.*|eap-xp3-openj9-11-openshift-rhel8:3.0|g' $devfiles
 else
     echo "[INFO] nothing to do on $arch; only swap openjdk for openj9 images on s390x and ppc64le arches"
