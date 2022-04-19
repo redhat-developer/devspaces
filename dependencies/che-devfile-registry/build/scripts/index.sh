f04c4a3b719e311422791b37446275049eec93e6
Igor Vinokur <ivinokur@redhat.com>
2022-04-19 14:25:20 +0300

    chore: add links.devWorkspaces field to the index.json
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- dependencies/che-devfile-registry/build/scripts/index.sh
+++ dependencies/che-devfile-registry/build/scripts/index.sh
@@ -19,6 +19,12 @@ for meta in "${metas[@]}"; do
 
     # Ignore double quotes warning for yq expression
     # shellcheck disable=SC2016,SC2094
-    cat <<< "$(yq --arg metadir "${META_DIR}" '.links |= . + {self: "/\($metadir)/devfile.yaml" }' "${meta}")"  > "${meta}"
+    cat <<< "$(yq -y --arg metadir "${META_DIR}" '.links |= . + {self: "/\($metadir)/devfile.yaml" }' "${meta}")"  > "${meta}"
+    if [ "$(yq '.links.v2' "${meta}")" != "null" ]; then
+      # Ignore double quotes warning for yq expression
+      # shellcheck disable=SC2016,SC2094
+      cat <<< "$(yq -y --arg metadir "${META_DIR}" '.links.devWorkspaces |= . +
+      {"eclipse/che-theia/latest": "/\($metadir)/devworkspace-che-theia-latest.yaml"}' "${meta}")" > "${meta}"
+    fi
 done
 yq -s 'map(.)' "${metas[@]}"
