d68f2ce3eb214c9a7f2bb5e1cb30b18025f8dfd3
Nick Boldt <nboldt@redhat.com>
2022-09-19 17:33:34 -0400

    add updateOdoRpms method and update defaults to 4.10/4.11
    Change-Id: I782e1baf33d52df20339c59e02fed244c7c4ecd3
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- product/util2.groovy
+++ product/util2.groovy
@@ -308,13 +308,18 @@ fi
 // URL from which to get internal RPM installations
 @Field String pulpRepoURL = "http://rhsm-pulp.corp.redhat.com"
 
-// ./getLatestRPM.sh -r "openshift-clients-4" -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.9 -s ...
-def updateOCRpms(String rpmRepoVersion="4.9", String dir="${WORKSPACE}/sources", String branch=MIDSTM_BRANCH, String ARCHES="x86_64 s390x ppc64le") {
+// ./getLatestRPM.sh -r "openshift-clients-4" -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/rhocp/4.11 -s ...
+def updateOCRpms(String rpmRepoVersion="4.11", String dir="${WORKSPACE}/sources", String branch=MIDSTM_BRANCH, String ARCHES="x86_64 s390x ppc64le") {
   updatedVersion=updateRpms("openshift-clients-4", pulpRepoURL + "/content/dist/layered/rhel8/basearch/rhocp/" + rpmRepoVersion, dir, branch, ARCHES)
   commitChanges(dir, "[rpms] Update to " + updatedVersion, branch)
 }
-// ./getLatestRPM.sh -r "helm-3"             -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/ocp-tools/4.9 -s ...
-def updateHelmRpms(String rpmRepoVersion="4.9", String dir="${WORKSPACE}/sources", String branch=MIDSTM_BRANCH, String ARCHES="x86_64 s390x ppc64le") {
+// ./getLatestRPM.sh -r "odo-2"             -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/ocp-tools/4.10 -s ...
+def updateOdoRpms(String rpmRepoVersion="4.10", String dir="${WORKSPACE}/sources", String branch=MIDSTM_BRANCH, String ARCHES="x86_64 s390x ppc64le") {
+  updatedVersion=updateRpms("odo-2", pulpRepoURL + "/content/dist/layered/rhel8/basearch/ocp-tools/" + rpmRepoVersion, dir, branch, ARCHES)
+  commitChanges(dir, "[rpms] Update to " + updatedVersion, branch)
+}
+// ./getLatestRPM.sh -r "helm-3"             -u http://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/basearch/ocp-tools/4.10 -s ...
+def updateHelmRpms(String rpmRepoVersion="4.10", String dir="${WORKSPACE}/sources", String branch=MIDSTM_BRANCH, String ARCHES="x86_64 s390x ppc64le") {
   updatedVersion=updateRpms("helm-3", pulpRepoURL + "/content/dist/layered/rhel8/basearch/ocp-tools/" + rpmRepoVersion, dir, branch, ARCHES)
   commitChanges(dir, "[rpms] Update to " + updatedVersion, branch)
 }
