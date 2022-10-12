8f5a48802796e7a655e1374cfa526f81f2d401d4
Nick Boldt <nboldt@redhat.com>
2022-10-12 08:46:41 -0300

    add --quay flag to search for same image on quay.io (for unreleased images)
    Change-Id: I00885d527f1e5f9f27a703d962968c477891c52d
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- product/getTagForSHA.sh
+++ product/getTagForSHA.sh
@@ -9,17 +9,31 @@
 #
 # for a given image+sha, compute the associated tag
 
-imageAndSHA=$1
 if [[ -z $1 ]]; then 
-    echo "Usage: $0 registry/org/image@sha256:digest"
+    echo "Usage: $0 registry/org/image@sha256:digest [--quay]"
+    echo
+    echo "Use --quay flag to search for image on quay.io registry if not found on provided one (eg., for an unreleased registry.redhat.io image)"
     exit 1
 fi
-echo "For $imageAndSHA"
-image=${imageAndSHA%%@*}
-URL=$(skopeo inspect docker://${imageAndSHA} | jq -r '.Labels.url')
-# echo "Got $URL"
-container=${URL}
-container=${image}:${container##*/images/}
-echo "Got $container"
-# skopeo inspect docker://${container} | jq -r .Digest # note, this might be different from the input SHA, but still equivalent 
-
+imageAndSHAs=$1
+if [[ $2 == "--quay" ]]; then 
+    imageAndSHAs="${imageAndSHAs} quay.io/${imageAndSHAs#*/}"
+fi
+for imageAndSHA in $imageAndSHAs; do
+    echo "For $imageAndSHA"
+    image=${imageAndSHA%%@*}
+    URL=$(skopeo inspect docker://${imageAndSHA} | jq -r '.Labels.url')
+    # echo "Got $URL"
+    if [[ $URL ]]; then
+        container=${URL}
+        container=${image}:${container##*/images/}
+        echo "Got $container"
+    else
+        if [[ $2 == "--quay" ]]; then 
+            echo "Not found"; echo
+        else 
+            echo; echo "Not found; try --quay flag to check same image on quay.io registry"
+        fi
+    fi
+    # skopeo inspect docker://${container} | jq -r .Digest # note, this might be different from the input SHA, but still equivalent 
+done
