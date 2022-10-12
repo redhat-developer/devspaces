eb60d7c68cb90051ed03765895aea057b1450a52
Nick Boldt <nboldt@redhat.com>
2022-10-12 08:47:26 -0300

    add --quay flag to search for same image on quay.io (for unreleased images)
    Change-Id: I02b256969e774c7619761ee871b759aed8e2c823
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- product/getTagForSHA.sh
+++ product/getTagForSHA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2021 Red Hat, Inc.
+# Copyright (c) 2021-2022 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -8,6 +8,7 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 # for a given image+sha, compute the associated tag
+# optionally, use --quay flag to also check quay.io registry
 
 if [[ -z $1 ]]; then 
     echo "Usage: $0 registry/org/image@sha256:digest [--quay]"
