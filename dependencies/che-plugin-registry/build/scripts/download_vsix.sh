24130c3809ba0c40955c571b373bc06e4c82cc5d
Valeriy Svydenko <vsvydenk@redhat.com>
2022-11-09 22:50:55 +0200

    chore: improve list of vsix to use version and download link (#838)
    * chore: improve list of vsix to use version and download link
    
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>
    
    * fix: address comments
    
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>
    
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/download_vsix.sh
+++ dependencies/che-plugin-registry/build/scripts/download_vsix.sh
@@ -6,42 +6,52 @@ set -o pipefail
 # pull vsix from OpenVSX
 mkdir -p /tmp/vsix
 openVsxSyncFileContent=$(cat "/openvsx-server/openvsx-sync.json")
-listOfVsixes=$(echo "${openVsxSyncFileContent}" | jq -r ".[]")
+numberOfExtensions=$(echo "${openVsxSyncFileContent}" | jq ". | length")
 IFS=$'\n' 
 
-for vsixFullName in $listOfVsixes; do
+for i in $(seq 0 "$((numberOfExtensions - 1))"); do
+    vsixFullName=$(echo "${openVsxSyncFileContent}" | jq -r ".[$i].id")
+    vsixVersion=$(echo "${openVsxSyncFileContent}" | jq -r ".[$i].version")
+    vsixDownloadLink=$(echo "${openVsxSyncFileContent}" | jq -r ".[$i].download")
+
     # extract from the vsix name the publisher name which is the first part of the vsix name before dot
     vsixPublisher=$(echo "${vsixFullName}" | cut -d '.' -f 1)
 
     # replace the dot by / in the vsix name
     vsixName=$(echo "${vsixFullName}" | sed 's/\./\//g')
 
-    # grab metadata for the vsix file
-    vsixMetadata=$(curl -sL "https://open-vsx.org/api/${vsixName}/latest")
-
-    # check there is no error field in the metadata
-    if [[ $(echo "${vsixMetadata}" | jq -r ".error") != null ]]; then
-        echo "Error while getting metadata for ${vsixFullName}"
-        echo "${vsixMetadata}"
-        exit 1
-    fi
-
-    # grab the version field from metadata
-    vsixVersion=$(echo "${vsixMetadata}" | jq -r '.version')
-
-    # extract the download link from the json metadata
-    vsixDownloadLink=$(echo "${vsixMetadata}" | jq -r '.files.download')
-    # get linux-x64 download link
-    vsixLinux64DownloadLink=$(echo "${vsixMetadata}" | jq -r '.downloads."linux-x64"')
-    if [[ $vsixLinux64DownloadLink != null ]]; then
-        vsixDownloadLink=$vsixLinux64DownloadLink
+    # if download wasn't set, try to fetch from openvsx.org
+    if [[ $vsixDownloadLink == null ]]; then
+        # grab metadata for the vsix file
+        # if version wasn't set, use latest
+        if [[ $vsixVersion == null ]]; then
+            vsixMetadata=$(curl -sLS "https://open-vsx.org/api/${vsixName}/latest")
+            
+            # if version wasn't set in json, grab it from metadata and add it into the file
+            vsixVersion=$(echo "${vsixMetadata}" | jq -r '.version')
+            jq --argjson i "$i" --arg version "$vsixVersion" '.[$i] += { "version": $version }' /openvsx-server/openvsx-sync.json > tmp.json
+            mv tmp.json /openvsx-server/openvsx-sync.json
+        else
+            vsixMetadata=$(curl -sLS "https://open-vsx.org/api/${vsixName}/${vsixVersion}")
+        fi 
+        # check there is no error field in the metadata
+        if [[ $(echo "${vsixMetadata}" | jq -r ".error") != null ]]; then
+            echo "Error while getting metadata for ${vsixFullName}"
+            echo "${vsixMetadata}"
+            exit 1
+        fi
+        
+        # extract the download link from the json metadata
+        vsixDownloadLink=$(echo "${vsixMetadata}" | jq -r '.files.download')
+        # get linux-x64 download link
+        vsixLinux64DownloadLink=$(echo "${vsixMetadata}" | jq -r '.downloads."linux-x64"')
+        if [[ $vsixLinux64DownloadLink != null ]]; then
+            vsixDownloadLink=$vsixLinux64DownloadLink
+        fi
     fi
 
     echo "Downloading ${vsixDownloadLink} into ${vsixPublisher} folder..."
     vsixFilename="/tmp/vsix/${vsixFullName}-${vsixVersion}.vsix"
     # download the latest vsix file in the publisher directory
-    curl -sL "${vsixDownloadLink}" -o "${vsixFilename}"
-
-    # shellcheck disable=SC2016
-    sed -i "s/\"$vsixFullName\"/\"$vsixFullName:$vsixVersion\"/g" /openvsx-server/openvsx-sync.json
+    curl -sLS "${vsixDownloadLink}" -o "${vsixFilename}"
 done;
