107caf3a40f86ba0852cf6324ff49232127ebc88
Angel Misevski <amisevsk@redhat.com>
2022-11-09 14:21:13 -0500

    Add --more option to query-pulp.sh to print more information
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- product/query-pulp.sh
+++ product/query-pulp.sh
@@ -25,6 +25,7 @@ Usage: $0 [OPTIONS] QUERY
 Options:
   -d, --displayname : print only the display name for matching repos
   -f, --full        : print full response for matching repos
+  -m, --more        : print more information about repos
   -h, --help        : display this help text
 
 Example:
@@ -36,6 +37,7 @@ while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-d'|'--displayname') DISPLAY_NAME_ONLY="true";;
     '-f'|'--full') FULL="true";;
+    '-m'|'--more') MORE="true";;
     '-h'|'--help') usage; exit 0;;
     -*|--*) echo -e "[ERROR] Unknown option: $1.\n"; usage; exit 1;;
     *) if [ -z "$QUERY" ]; then QUERY=$1; else echo -e "[ERROR] Only one positional argument allowed.\n"; usage; exit 1; fi;;
@@ -57,6 +59,10 @@ elif [ "$DISPLAY_NAME_ONLY" == "true" ]; then
   curl -s -k --user qa:qa  https://rhsm-pulp.corp.redhat.com/pulp/api/v2/repositories/search/ \
     --data '{"criteria": {"filters": {"display_name": {"$regex": "'"$QUERY"'"}}}}' \
     | jq -r '.[].display_name'
+elif [ "$MORE" == "true" ]; then
+  curl -s -k --user qa:qa  https://rhsm-pulp.corp.redhat.com/pulp/api/v2/repositories/search/ \
+    --data '{"criteria": {"filters": {"display_name": {"$regex": "'"$QUERY"'"}}}}' \
+    | jq '.[] | {display_name, "content_set": .notes.content_set, "repo_url": "http://rhsm-pulp.corp.redhat.com/\(.notes.relative_url)", "include_in_download_service": .notes.include_in_download_service}'
 else
   curl -s -k --user qa:qa  https://rhsm-pulp.corp.redhat.com/pulp/api/v2/repositories/search/ \
     --data '{"criteria": {"filters": {"display_name": {"$regex": "'"$QUERY"'"}}}}' \
