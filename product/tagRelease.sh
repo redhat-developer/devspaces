7490d514e70df47cd64e10012f48c83d9215f230
Nick Boldt <nboldt@redhat.com>
2023-03-03 11:54:33 -0400

    chore: backport from 3.x / support updating PNC configs
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- product/tagRelease.sh
+++ product/tagRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018-2022 Red Hat, Inc.
+# Copyright (c) 2018-2023 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -9,7 +9,9 @@
 #
 
 # script to tag the Che/devspaces repos for a given release
+# as well as create/update branches and related PNC build-configs
 
+SCRIPT=$(readlink -f "$0"); SCRIPTPATH=$(dirname "$SCRIPT")
 # defaults
 # try to compute branches from currently checked out branch; else fall back to hard coded value
 TARGET_BRANCH="$(git rev-parse --abbrev-ref HEAD 2>/dev/null || true)"
@@ -18,13 +20,12 @@ if [[ $TARGET_BRANCH != "devspaces-3."*"-rhel-8" ]]; then
 fi
 pkgs_devel_branch=${TARGET_BRANCH}
 
-pduser=crw-build
+pduser=devspaces-build
 
 samplesRepo=devspaces-samples
 
 SOURCE_BRANCH="" # normally, use this script to create tags, not branches
 
-SCRIPT=$(readlink -f "$0"); SCRIPTPATH=$(dirname "$SCRIPT")
 CLEAN="false" #  if set true, delete existing folders and do fresh checkouts
 
 if [[ $# -lt 4 ]]; then
@@ -34,7 +35,7 @@ To create tags (and push updated CSV content into operator-bundle repo):
 Example: 
   $0 -v 3.y.0 -t 3.y -gh ${TARGET_BRANCH} -ghtoken \$GITHUB_TOKEN -pd ${pkgs_devel_branch} -pduser $pduser
 
-To create or update existing branches:
+To create or update existing branches and update related PNC build-configs:
   $0 -t DS_VERSION --branchfrom SOURCE_GH_BRANCH -gh TARGET_GH_BRANCH -ghtoken GITHUB_TOKEN
 Example: 
   $0 -t DS_VERSION --branchfrom devspaces-3-rhel-8 -gh ${TARGET_BRANCH} -ghtoken \$GITHUB_TOKEN
@@ -74,30 +75,31 @@ pushTagPD ()
 {
 	d="$1"
 	echo; echo "== $d =="
-	if [[ ! -d /tmp/tmp-checkouts/containers_${d} ]]; then
-		git clone -b ${pkgs_devel_branch} ssh://${pduser}@pkgs.devel.redhat.com/containers/${d} containers_${d}
-		pushd /tmp/tmp-checkouts/containers_${d} >/dev/null || exit 1
+	if [[ ! -d "/tmp/tmp-checkouts/containers_${d}" ]]; then
+		git clone -b "${pkgs_devel_branch}" "ssh://${pduser}@pkgs.devel.redhat.com/containers/${d}" "containers_${d}"
+		pushd "/tmp/tmp-checkouts/containers_${d}" >/dev/null || exit 1
 			export KRB5CCNAME=/var/tmp/${pduser}_ccache
-			git config user.email ${pduser}@REDHAT.COM
+			git config user.email "${pduser}@REDHAT.COM"
 			git config user.name "Dev Spaces Build"
 			git config --global push.default matching
 
-			git checkout --track origin/${pkgs_devel_branch} -q || true
+			git checkout --track origin/"${pkgs_devel_branch}" -q || true
 			git pull -q
 		popd >/dev/null || exit 1
 	fi
-	pushd /tmp/tmp-checkouts/containers_${d} >/dev/null || exit 1
+	pushd "/tmp/tmp-checkouts/containers_${d}" >/dev/null || exit 1
 		# push new tag (no op if already exists)
-		git tag -a ${CSV_VERSION} -m "${CSV_VERSION}" || true
-		git push origin ${CSV_VERSION} || true
+		git tag -a "${CSV_VERSION}" -m "${CSV_VERSION}" || true
+		git push origin "${CSV_VERSION}" || true
 	popd >/dev/null || exit 1
 }
 
 toggleQuayRHECReferences() {
 	YAML_ROOT="dependencies/"
 	# replace DS meta.yaml files with links to current version of devfile v2
+	# shellcheck disable=SC2044
 	for yaml in $(find ${YAML_ROOT} -name "*.yaml"); do
-		sed -r -i $yaml -e "s#quay.io/devspaces/#registry.redhat.io/devspaces/#g"
+		sed -r -i "$yaml" -e "s#quay.io/devspaces/#registry.redhat.io/devspaces/#g"
 	done
 	git commit -s -m "chore(yaml) set image refs to registry.redhat.io/devspaces/" $YAML_ROOT || echo ""
 }
@@ -105,6 +107,7 @@ toggleQuayRHECReferences() {
 # for the devspaces main repo, update meta.yaml files to point to the correct branch of $samplesRepo
 updateImageTags() {
 	YAML_ROOT="dependencies/che-plugin-registry/"
+	# shellcheck disable=SC2044
 	for cheyaml in $(find ${YAML_ROOT} -name "che-*.yaml"); do
 	   sed -r -i "${cheyaml}" \
 		   -e "s|(image: .+/devspaces/.+):[0-9.]+|\1:${DS_VERSION}|g"
@@ -117,6 +120,7 @@ updateLinksToDevfiles() {
 	YAML_ROOT="dependencies/che-devfile-registry/devfiles"
 
 	# replace DS meta.yaml files with links to current version of devfile v2
+	# shellcheck disable=SC2044
 	for meta in $(find ${YAML_ROOT} -name "meta.yaml"); do
 	   sed -r -i "${meta}" \
 		   -e "s|/tree/devfilev2|/tree/${TARGET_BRANCH}|g" \
@@ -147,31 +151,31 @@ pushBranchAndOrTagGH () {
 	org="$2"
 	echo; echo "== $d =="
 	# if source_branch defined and target branch doesn't exist yet, check out the source branch
-	if [[ ${SOURCE_BRANCH} ]] && [[ $(git ls-remote --heads https://github.com/${org}/${d} ${TARGET_BRANCH}) == "" ]]; then
+	if [[ ${SOURCE_BRANCH} ]] && [[ $(git ls-remote --heads "https://github.com/${org}/${d}" "${TARGET_BRANCH}") == "" ]]; then
 		clone_branch=${SOURCE_BRANCH}
 	else # if source branch not set (tagging operation) or target branch already exists
 		clone_branch=${TARGET_BRANCH}
 	fi
-	if [[ ! -d /tmp/tmp-checkouts/projects_${d} ]]; then
-		git clone -q --depth 1 -b ${clone_branch} https://github.com/${org}/${d} projects_${d}
-		pushd /tmp/tmp-checkouts/projects_${d} >/dev/null || exit 1
+	if [[ ! -d "/tmp/tmp-checkouts/projects_${d}" ]]; then
+		git clone -q --depth 1 -b "${clone_branch}" "https://github.com/${org}/${d}" "projects_${d}"
+		pushd "/tmp/tmp-checkouts/projects_${d}" >/dev/null || exit 1
 			export GITHUB_TOKEN="${GITHUB_TOKEN}"
 			git config user.email "nickboldt+devstudio-release@gmail.com"
 			git config user.name "Red Hat Devstudio Release Bot"
 			git config --global push.default matching
 			git config --global hub.protocol https
-			git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${org}/${d}
+			git remote set-url origin "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${org}/${d}"
 
-			git checkout --track origin/${clone_branch} -q || true
+			git checkout --track "origin/${clone_branch}" -q || true
 			git pull -q
 		popd >/dev/null || exit 1
 	fi
-	pushd /tmp/tmp-checkouts/projects_${d} >/dev/null || exit 1
+	pushd "/tmp/tmp-checkouts/projects_${d}" >/dev/null || exit 1
 	if [[ ${SOURCE_BRANCH} ]]; then 
 		# create a branch or use existing
-		git branch ${TARGET_BRANCH} || true
-		git checkout ${TARGET_BRANCH} || true
-		git pull origin ${TARGET_BRANCH} || true
+		git branch "${TARGET_BRANCH}" || true
+		git checkout "${TARGET_BRANCH}" || true
+		git pull origin "${TARGET_BRANCH}" || true
 
 		# for the devspaces main repo, update devfiles to point to the correct tag/branch
 		if [[ $d == "devspaces" ]]; then
@@ -185,18 +189,48 @@ pushBranchAndOrTagGH () {
 			updateSampleDevfileReferences
 		fi
 
-		git pull origin ${TARGET_BRANCH} || true
-		git push origin ${TARGET_BRANCH} || true
+		git pull origin "${TARGET_BRANCH}" || true
+		git push origin "${TARGET_BRANCH}" || true
 	fi
 	if [[ $CSV_VERSION ]]; then # push a new tag (or no-op if exists)
-		git tag ${CSV_VERSION} || true
-		git push origin ${CSV_VERSION} || true
+		git tag "${CSV_VERSION}" || true
+		git push origin "${CSV_VERSION}" || true
+		# update latest floating tag for samples
+		if [[ $org == "${samplesRepo}" ]]; then
+			# check all of the 3.*.* tags in the repo, if tag exists with a higher version than the new tag
+			# then latest floating tag should not be updated
+			LATEST_TAG=$(git tag -l -n 3.*.* --sort -version:refname | head -n 1 | grep -o "^3\.[0-9]*\.[0-9]*")
+			if { echo "${CSV_VERSION}"; echo "${LATEST_TAG}"; } | sort -V --check=silent && [[ "${CSV_VERSION}" != "${LATEST_TAG}" ]];then
+				echo "[DEBUG] Sample version ${d} is less than latest ${LATEST_TAG} version, not updating the latest tag";
+			else
+				echo "[DEBUG] Updating latest tag of sample ${d} with ${CSV_VERSION}"
+				git tag -d "latest" || true
+				git tag "latest"
+				git push origin "latest" -f
+			fi
+		fi
 	fi
 	popd >/dev/null || exit 1
 }
 
+updatePNCBuildConfigs() {
+  if [[ ! -x ${SCRIPTPATH}/updatePNCBuildConfigs.sh ]]; then
+    curl -sSLO https://raw.githubusercontent.com/redhat-developer/devspaces/${MIDSTM_BRANCH}/product/updatePNCBuildConfigs.sh --output-dir /tmp
+    chmod +x /tmp/updatePNCBuildConfigs.sh
+    PNC_SCRIPT_LOCATION="/tmp/updatePNCBuildConfigs.sh"
+  else
+    PNC_SCRIPT_LOCATION="${SCRIPTPATH}/updatePNCBuildConfigs.sh"
+  fi
+  # if source and target branch are the same, we're updating the 3.x / next branch; else we're updating the 3.yy / latest branch
+  if [ "${TARGET_BRANCH}" == "${SOURCE_BRANCH}" ];then
+    ${PNC_SCRIPT_LOCATION} -v ${DS_VERSION} --next
+  else
+    ${PNC_SCRIPT_LOCATION} -v ${DS_VERSION} --latest
+  fi
+}
+
 # tag pkgs.devel repos only (branches are created by SPMM ticket, eg., https://projects.engineering.redhat.com/browse/SPMM-2517)
-if [[ ${pkgs_devel_branch} ]] && [[ ${CSV_VERSION} ]]; then
+if [[ "${pkgs_devel_branch}" ]] && [[ "${CSV_VERSION}" ]]; then
 	for repo in \
 	devspaces-code \
 	devspaces-configbump \
@@ -250,8 +284,13 @@ web-nodejs-sample \
 
 # create branches for devspaces samples, located under https://github.com/${samplesRepo}/
 for s in $sampleprojects; do
-	pushBranchAndOrTagGH $s ${samplesRepo}
+	pushBranchAndOrTagGH "$s" ${samplesRepo}
 done
 
+# update PNC build-configs, only if performing branching operation (not when tagging)
+if [[ ${SOURCE_BRANCH} ]]; then
+  updatePNCBuildConfigs
+fi
+
 # cleanup
-rm -fr /tmp/tmp-checkouts
+rm -fr /tmp/tmp-checkouts /tmp/updatePNCBuildConfigs.sh
