d1987953d0d234e119225f4417c493a77b39945a
Nick Boldt <nboldt@redhat.com>
2023-03-08 15:28:29 -0400

    remove more refs to theia / endpoint; keep refs to machine-exec
    Change-Id: I9736fd2474fe7a50cd942a404af96bb3acc7f29d
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
+++ dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
@@ -30,11 +30,11 @@ METAS_DIR="${METAS_DIR:-${DEFAULT_METAS_DIR}}"
 
 # Regex used to break an image reference into groups:
 #   \1 - Whitespace and (optional) quotation preceding image reference
-#   \2 - Registry portion of image, e.g. (quay.io)/eclipse/che-theia:tag
-#   \3 - Organization portion of image, e.g. quay.io/(eclipse)/che-theia:tag
-#   \4 - Image name portion of image, e.g. quay.io/eclipse/(che-theia):tag
-#   \5 - Optional image digest identifier (empty for tags), e.g. quay.io/eclipse/che-theia(@sha256):digest
-#   \6 - Tag of image or digest, e.g. quay.io/eclipse/che-theia:(tag)
+#   \2 - Registry portion of image, e.g. (quay.io)/eclipse/che-code:tag
+#   \3 - Organization portion of image, e.g. quay.io/(eclipse)/che-code:tag
+#   \4 - Image name portion of image, e.g. quay.io/eclipse/(che-code):tag
+#   \5 - Optional image digest identifier (empty for tags), e.g. quay.io/eclipse/che-code(@sha256):digest
+#   \6 - Tag of image or digest, e.g. quay.io/eclipse/che-code:(tag)
 #   \7 - Optional quotation following image reference
 IMAGE_REGEX="([[:space:]>-]*[\r]?[[:space:]]*[\"']?)([._:a-zA-Z0-9-]*)/([._a-zA-Z0-9-]*)/([._a-zA-Z0-9-]*)(@sha256)?:([._a-zA-Z0-9-]*)([\"']?)"
 
@@ -46,26 +46,8 @@ function run_main() {
 
     update_extension_vsx_references
 
-    # For testing, the images used for the che-theia, theia runtime, and machine-exec
-    # plugins can be overridden via the environment variables
-    #     CHE_PLUGIN_REGISTRY_THEIA_IMAGE
-    #     CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE
-    #     CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE
-    # If set, these env vars override all modifications
-    # (e.g. via CHE_DEVFILE_IMAGES_REGISTRY_URL)
-    #
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_IMAGE" ]; then
-    echo "Overriding Che Theia image with $CHE_PLUGIN_REGISTRY_THEIA_IMAGE"
-    sed -i -E "s|image:.*theia-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_IMAGE|" "${metas[@]}"
-    fi
-
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE"
-    sed -i -E "s|image:.*theia-endpoint-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE|" "${metas[@]}"
-    fi
-
     if [ -n "$CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE"
+    echo "Overriding machine-exec image with $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE"
     sed -i -E "s|image:.*machineexec-rhel8.*|image: $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE|" "${metas[@]}"
     fi
 
@@ -149,7 +131,7 @@ function extract_and_use_related_images_env_variables_with_image_digest_info() {
             done
         fi
 
-        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
         for meta in "${metas[@]}"; do
             readarray -t images < <(grep "image:" "${meta}" | sed -r "s;.*image:[[:space:]]*'?\"?([._:a-zA-Z0-9-]*/?[._a-zA-Z0-9-]*/[._a-zA-Z0-9-]*(@sha256)?:?[._a-zA-Z0-9-]*)'?\"?[[:space:]]*;\1;")
             for image in "${images[@]}"; do
@@ -228,7 +210,7 @@ function update_container_image_references() {
     # We can't use the `-d` option for readarray because
     # registry.centos.org/centos/httpd-24-centos7 ships with Bash 4.2
     # The below command will fail if any path contains whitespace
-    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
     for meta in "${metas[@]}"; do
     echo "Checking meta $meta"
     # Need to update each field separately in case they are not defined.
@@ -250,7 +232,7 @@ function update_container_image_references() {
 }
 
 function update_extension_vsx_references() {
-    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
     if [ -n "$INTERNAL_URL" ]; then
         INTERNAL_URL=${INTERNAL_URL%/}
         echo "Updating relative:extension in files to ${INTERNAL_URL}"
