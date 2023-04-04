00af3f5ad23e22887897285c74ac5497e87b10fe
Valeriy Svydenko <vsvydenk@redhat.com>
2023-04-04 19:13:29 +0300

    chore: remove che-theia+references from plugin reg (#930)
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
+++ dependencies/che-plugin-registry/build/dockerfiles/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018-2021 Red Hat, Inc.
+# Copyright (c) 2018-2023 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -45,29 +45,6 @@ function run_main() {
     update_container_image_references
 
     update_extension_vsx_references
-
-    # For testing, the images used for the che-theia, theia runtime, and machine-exec
-    # plugins can be overridden via the environment variables
-    #     CHE_PLUGIN_REGISTRY_THEIA_IMAGE
-    #     CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE
-    #     CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE
-    # If set, these env vars override all modifications
-    # (e.g. via CHE_DEVFILE_IMAGES_REGISTRY_URL)
-    #
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_IMAGE" ]; then
-    echo "Overriding Che Theia image with $CHE_PLUGIN_REGISTRY_THEIA_IMAGE"
-    sed -i -E "s|image:.*theia-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_IMAGE|" "${metas[@]}"
-    fi
-
-    if [ -n "$CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE"
-    sed -i -E "s|image:.*theia-endpoint-rhel8.*|image: $CHE_PLUGIN_REGISTRY_THEIA_ENDPOINT_RUNTIME_BINARY_IMAGE|" "${metas[@]}"
-    fi
-
-    if [ -n "$CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE" ]; then
-    echo "Overriding Theia remote runtime image with $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE"
-    sed -i -E "s|image:.*machineexec-rhel8.*|image: $CHE_PLUGIN_REGISTRY_MACHINE_EXEC_IMAGE|" "${metas[@]}"
-    fi
 
     # Add current (arbitrary) user to /etc/passwd and /etc/group
     if ! whoami &> /dev/null; then
@@ -149,7 +126,7 @@ function extract_and_use_related_images_env_variables_with_image_digest_info() {
             done
         fi
 
-        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+        readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
         for meta in "${metas[@]}"; do
             readarray -t images < <(grep "image:" "${meta}" | sed -r "s;.*image:[[:space:]]*'?\"?([._:a-zA-Z0-9-]*/?[._a-zA-Z0-9-]*/[._a-zA-Z0-9-]*(@sha256)?:?[._a-zA-Z0-9-]*)'?\"?[[:space:]]*;\1;")
             for image in "${images[@]}"; do
@@ -228,7 +205,7 @@ function update_container_image_references() {
     # We can't use the `-d` option for readarray because
     # registry.centos.org/centos/httpd-24-centos7 ships with Bash 4.2
     # The below command will fail if any path contains whitespace
-    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
     for meta in "${metas[@]}"; do
     echo "Checking meta $meta"
     # Need to update each field separately in case they are not defined.
@@ -250,7 +227,7 @@ function update_container_image_references() {
 }
 
 function update_extension_vsx_references() {
-    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml' -o -name 'che-theia-plugin.yaml')
+    readarray -t metas < <(find "${METAS_DIR}" -name 'meta.yaml' -o -name 'devfile.yaml')
     if [ -n "$INTERNAL_URL" ]; then
         INTERNAL_URL=${INTERNAL_URL%/}
         echo "Updating relative:extension in files to ${INTERNAL_URL}"
