54bbdd10788a91871e8c3e767069870d691a4879
Valeriy Svydenko <vsvydenk@redhat.com>
2023-07-11 19:31:35 +0300

    fix: set correct path to the python3.11 (#981)
    * fix: set correct path to the python3.11
    
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>
    
    * additional fixes to make both copies of rhel.install.sh (in devfile AND plugin registry) more consistent
    
    Change-Id: Ib24d55bcd15d6c8d68e108b5ed1b328fbbd26c50
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    * code cleanup
    
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>
    
    ---------
    
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    Co-authored-by: Nick Boldt <nboldt@redhat.com>

--- dependencies/che-devfile-registry/build/dockerfiles/rhel.install.sh
+++ dependencies/che-devfile-registry/build/dockerfiles/rhel.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018-2022 Red Hat, Inc.
+# Copyright (c) 2018-2023 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -9,10 +9,11 @@
 #
 set -x
 
-# set same version in Dockefiles, eg., base image ubi8/python-38
-PYTHON_VERSION="3.8"
+# set same version in Dockerfiles, eg., base image ubi8/python-311
+PYTHON_VERSION="3.11"
 
 DNF="dnf -q"
+# shellcheck disable=SC2086
 if [[ ! -x $(command -v $DNF || true) ]]; then   DNF="yum -q"
   if [[ ! -x $(command -v $DNF || true) ]]; then DNF=microdnf; fi
 fi
@@ -30,17 +31,13 @@ minrate=1
 retries=20
 timeout=60
 " > /etc/yum.conf
-if [[ $DNF != "microdnf" ]]; then $DNF -y module reset python${PYTHON_VERSION/./}; $DNF -y module enable python${PYTHON_VERSION/./}:${PYTHON_VERSION}; fi
 ${DNF} -y install npm findutils bash wget yum git gzip tar jq skopeo \
-    python${PYTHON_VERSION/./} python${PYTHON_VERSION/./}-devel python${PYTHON_VERSION/./}-setuptools python${PYTHON_VERSION/./}-pip --exclude=unbound-libs || exit 1
+    python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-setuptools python${PYTHON_VERSION}-pip --exclude=unbound-libs || exit 1
 
 # shellcheck disable=SC2010
 PYTHON_BIN=$(ls -1 /usr/bin | grep -E "^python3.[0-9]$" | sort -V | tail -1 || true) # 3.6, 3.7, 3.8, etc.
 if [[ ! ${PYTHON_BIN} ]]; then
-    PYTHON_BIN=$(/usr/bin/python3 -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
-    if [[ ! ${PYTHON_BIN} ]]; then
-        PYTHON_BIN=$(/usr/bin/python -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
-    fi
+    PYTHON_BIN=python$(/usr/bin/python3 -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
 fi
 if [[ ! -L /usr/bin/python ]]; then
     ln -s /usr/bin/"${PYTHON_BIN}" /usr/bin/python
@@ -48,20 +45,20 @@ fi
 
 # install yq (depends on jq and pyyaml - if jq and pyyaml not already installed, this will try to compile it)
 if [[ -f /tmp/root-local.tgz ]] || [[ ${BOOTSTRAP} == "true" ]]; then
+    # NOTE: for plugin registry, use ~/.local instead of /opt/app-root/src/.local
     mkdir -p /opt/app-root/src/.local
     if [[ -f /tmp/root-local.tgz ]]; then
         tar xf /tmp/root-local.tgz -C /opt/app-root/src/.local
         rm -fr /tmp/root-local.tgz
     fi
-    /usr/bin/python -m pip install --user yq argcomplete
-    # NOTE: used to be in /root/.local but now can be found in /opt/app-root/src/.local
+    /usr/bin/"${PYTHON_BIN}" -m pip install --user yq argcomplete pip --upgrade
     # shellcheck disable=SC2043
     for d in /opt/app-root/src/.local; do
         if [[ -d ${d} ]]; then
-            cp ${d}/bin/yq /usr/local/bin/
-            mkdir -p ${d}/lib/"${PYTHON_BIN}"/site-packages/
+            cp "${d}"/bin/yq /usr/local/bin/
+            mkdir -p "${d}"/lib/"${PYTHON_BIN}"/site-packages/
             # shellcheck disable=SC2164
-            pushd ${d}/lib/"${PYTHON_BIN}"/site-packages/ >/dev/null
+            pushd "${d}"/lib/"${PYTHON_BIN}"/site-packages/ >/dev/null
             cp -r PyYAML* xmltodict* yaml* yq* /usr/lib/"${PYTHON_BIN}"/site-packages/
             # shellcheck disable=SC2164
             popd >/dev/null
--- dependencies/che-plugin-registry/build/dockerfiles/rhel.install.sh
+++ dependencies/che-plugin-registry/build/dockerfiles/rhel.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018-2022 Red Hat, Inc.
+# Copyright (c) 2018-2023 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -9,9 +9,9 @@
 #
 set -x
 
-# set same version in Dockerfiles, eg., base image ubi8/python-38
-PYTHON_VERSION="3.8"
-NODEJS_VERSION="16"
+# set same version in Dockerfiles, eg., base image ubi8/python-311
+PYTHON_VERSION="3.11"
+NODEJS_VERSION="18"
 
 DNF="dnf -q"
 # shellcheck disable=SC2086
@@ -32,16 +32,13 @@ minrate=1
 retries=20
 timeout=60
 " > /etc/yum.conf
-if [[ $DNF != "microdnf" ]]; then $DNF -y module reset python${PYTHON_VERSION/./}; $DNF -y module enable python${PYTHON_VERSION/./}:${PYTHON_VERSION}; fi
-${DNF} -y install python${PYTHON_VERSION/./} python${PYTHON_VERSION/./}-devel python${PYTHON_VERSION/./}-setuptools python${PYTHON_VERSION/./}-pip --exclude=unbound-libs || exit 1
+${DNF} -y install \
+    python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-setuptools python${PYTHON_VERSION}-pip --exclude=unbound-libs || exit 1
 
 # shellcheck disable=SC2010
 PYTHON_BIN=$(ls -1 /usr/bin | grep -E "^python3.[0-9]$" | sort -V | tail -1 || true) # 3.6, 3.7, 3.8, etc.
 if [[ ! ${PYTHON_BIN} ]]; then
-    PYTHON_BIN=$(/usr/bin/python3 -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
-    if [[ ! ${PYTHON_BIN} ]]; then
-        PYTHON_BIN=$(/usr/bin/python -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
-    fi
+    PYTHON_BIN=python$(/usr/bin/python3 -V | sed -r -e "s#Python ##" -e "s#([0-9])\.([0-9]+)\.([0-9]+)#\1.\2#")
 fi
 if [[ ! -L /usr/bin/python ]]; then
     ln -s /usr/bin/"${PYTHON_BIN}" /usr/bin/python
@@ -61,12 +58,13 @@ ${DNF} -y module reset nodejs && \
 
 # install yq (depends on jq and pyyaml - if jq and pyyaml not already installed, this will try to compile it)
 if [[ -f /tmp/root-local.tgz ]] || [[ ${BOOTSTRAP} == "true" ]]; then
+    # NOTE: for devfile registry, use /opt/app-root/src/.local instead of ~/.local
     mkdir -p "${HOME}"/.local
     if [[ -f /tmp/root-local.tgz ]]; then
         tar xf /tmp/root-local.tgz -C "${HOME}"/.local
         rm -fr /tmp/root-local.tgz
     fi
-    /usr/bin/"${PYTHON_BIN}" -m pip install --user yq argcomplete
+    /usr/bin/"${PYTHON_BIN}" -m pip install --user yq argcomplete pip --upgrade
     # NOTE: using bootstrap.Dockerfile + ubi8/python-38      image, copy from /opt/app-root/src/.local == ~/.local
     # NOTE: using           Dockerfile + rhel8/postgresql-13 image, copy from /var/lib/pgsql/.local == ~/.local
     # shellcheck disable=SC2043
