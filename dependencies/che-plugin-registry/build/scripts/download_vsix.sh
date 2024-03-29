6a3e2e9b1a980874927707acae2de1770bde48bd
Valeriy Svydenko <vsvydenk@redhat.com>
2024-03-29 17:53:04 +0200

    feat: set fixed versions for vsix extensions; add script to update versions
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/download_vsix.sh
+++ dependencies/che-plugin-registry/build/scripts/download_vsix.sh
@@ -8,8 +8,7 @@ set -o pipefail
 downloadVsix=1
 openvsxJson="/openvsx-server/openvsx-sync.json"
 
-usage()
-{
+usage() {
     echo "Usage: $0 -b devspaces-3.y-rhel-8 -j /path/to/openvsx-sync.json --no-download
 
 All arguments are optional.
@@ -22,13 +21,19 @@ All arguments are optional.
 
 # commandline args
 while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    '-b'|'--branch') scriptBranch="$2"; shift 1;;
-    '-j'|'--json') openvsxJson="$2"; shift 1;;
-    '--no-download') downloadVsix=0;;
-    '-h'|'--help') usage;;
-  esac
-  shift 1
+    case $1 in
+    '-b' | '--branch')
+        scriptBranch="$2"
+        shift 1
+        ;;
+    '-j' | '--json')
+        openvsxJson="$2"
+        shift 1
+        ;;
+    '--no-download') downloadVsix=0 ;;
+    '-h' | '--help') usage ;;
+    esac
+    shift 1
 done
 
 RED="\e[31m"
@@ -43,17 +48,16 @@ DEFAULT_EMOJI_FAIL="✘" # could be overriden with EMOJI_FAIL="[FAIL]"
 EMOJI_FAIL=${EMOJI_FAIL:-$DEFAULT_EMOJI_FAIL}
 
 function initTest() {
-  echo -n -e "${BOLD}\n${EMOJI_HEADER} ${1}${RESETSTYLE} ... "
+    echo -n -e "${BOLD}\n${EMOJI_HEADER} ${1}${RESETSTYLE} ... "
 }
 
 vsixMetadata="" #now global so it can be set/checked via function
-getMetadata(){
+getMetadata() {
     vsixName=$1
     key=$2
 
     # check there is no error field in the metadata and retry if there is
-    for j in 1 2 3 4 5
-    do
+    for j in 1 2 3 4 5; do
         vsixMetadata=$(curl -sLS "https://open-vsx.org/api/${vsixName}/${key}")
         if [[ $(echo "${vsixMetadata}" | jq -r ".error") != null ]]; then
             echo "Attempt $j/5: Error while getting metadata for ${vsixName} version ${key}"
@@ -70,11 +74,10 @@ getMetadata(){
 }
 
 versionsPage=""
-getVersions(){
+getVersions() {
     vsixName=$1
     # check the versions page is empty and retry if it is
-    for j in 1 2 3 4 5
-    do
+    for j in 1 2 3 4 5; do
         versionsPage=$(curl -sLS "https://open-vsx.org/api/${vsixName}/versions?size=200")
         totalSize=$(echo "${versionsPage}" | jq -r ".totalSize")
         if [[ "$totalSize" != "null" && "$totalSize" -eq 0 ]]; then
@@ -106,7 +109,7 @@ fi
 mkdir -p /tmp/vsix
 openVsxSyncFileContent=$(cat "$openvsxJson")
 numberOfExtensions=$(echo "${openVsxSyncFileContent}" | jq ". | length")
-IFS=$'\n' 
+IFS=$'\n'
 
 for i in $(seq 0 "$((numberOfExtensions - 1))"); do
     vsixFullName=$(echo "${openVsxSyncFileContent}" | jq -r ".[$i].id")
@@ -124,63 +127,35 @@ for i in $(seq 0 "$((numberOfExtensions - 1))"); do
         # grab metadata for the vsix file
         # if version wasn't set, use latest
         if [[ $vsixVersion == null ]]; then
-            getVersions "${vsixName}"
+            echo "Version is not set for ${vsixName}"
+            exit 1
+        fi
 
-            # if version wasn't set in json, grab it from metadata and add it into the file
-            # get all versions of the extension
-            allVersions=$(echo "${versionsPage}" | jq -r '.versions')
-            if [[ "$allVersions" == "{}" ]]; then
-                echo "No versions found for ${vsixName}"
-                exit 1
-            fi
-            key_value_pairs=$(echo "$allVersions" | jq -r 'to_entries[] | [ .key, .value ] | @tsv')
-            
-            # go through all versions of the extension to find the latest stable version that is compatible with the VS Code version
-            resultedVersion=null
-            while IFS=$'\t' read -r key value; do
-                # get metadata for the version
-                getMetadata "${vsixName}" "${key}"
-      
-                # check if the version is pre-release
-                preRelease=$(echo "${vsixMetadata}" | jq -r '.preRelease')
-                if [[ $preRelease == true ]]; then
-                    echo "Skipping pre-release version ${value}"
-                    continue
-                fi
+        # get metadata for the version
+        getMetadata "${vsixName}" "${vsixVersion}"
 
-                # extract the engine version from the json metadata
-                vscodeEngineVersion=$(echo "${vsixMetadata}" | jq -r '.engines.vscode')
-                # remove ^ from the engine version
-                vscodeEngineVersion="${vscodeEngineVersion//^/}"
-                # remove >= from the engine version
-                vscodeEngineVersion="${vscodeEngineVersion//>=/}"
-                # replace x by 0 in the engine version
-                vscodeEngineVersion="${vscodeEngineVersion//x/0}"
-                # check if the extension's engine version is compatible with the code version
-                # if the extension's engine version is ahead of the code version, check a next version of the extension
-                if [[  "$vscodeEngineVersion" = "$(echo -e "$vscodeEngineVersion\n$codeVersion" | sort -V | head -n1)" ]]; then
-                    #VS Code version >= Engine version, can proceed."
-                    resultedVersion=$(echo "${vsixMetadata}" | jq -r ".version")
-                    break
-                else 
-                    echo "Skipping ${value}, it is not compatible with VS Code editor $codeVersion"
-                    continue
-                fi
-            done <<< "$key_value_pairs"
+        # check if the version is pre-release
+        preRelease=$(echo "${vsixMetadata}" | jq -r '.preRelease')
+        if [[ $preRelease == true ]]; then
+            echo "Version ${vsixVersion} is marked as pre-release  for ${vsixName}, it should be stable one"
+            exit 1
+        fi
 
-            if [[ $resultedVersion == null ]]; then
-                echo "[ERROR] No stable version of $vsixFullName is compatible with VS Code editor verision $codeVersion; must exit!"
-                exit 1
-            else
-                vsixVersion=$resultedVersion
-            fi
+        # extract the engine version from the json metadata
+        vscodeEngineVersion=$(echo "${vsixMetadata}" | jq -r '.engines.vscode')
+        # remove ^ from the engine version
+        vscodeEngineVersion="${vscodeEngineVersion//^/}"
+        # remove >= from the engine version
+        vscodeEngineVersion="${vscodeEngineVersion//>=/}"
+        # replace x by 0 in the engine version
+        vscodeEngineVersion="${vscodeEngineVersion//x/0}"
+        # check if the extension's engine version is compatible with the code version
+        # if the extension's engine version is ahead of the code version, print an error
+        if [[ "$vscodeEngineVersion" != "$(echo -e "$vscodeEngineVersion\n$codeVersion" | sort -V | head -n1)" ]]; then
+            echo "Version ${vsixVersion} of ${vsixName} is not compatible with VS Code editor $codeVersion"
+            exit 1
+        fi
 
-            jq --argjson i "$i" --arg version "$vsixVersion" '.[$i] += { "version": $version }' "$openvsxJson" > tmp.json
-            mv tmp.json "$openvsxJson"
-        else
-            getMetadata "${vsixName}" "${vsixVersion}"
-        fi 
-        
         # extract the download link from the json metadata
         vsixDownloadLink=$(echo "${vsixMetadata}" | jq -r '.files.download')
         # get universal download link
@@ -203,7 +178,7 @@ for i in $(seq 0 "$((numberOfExtensions - 1))"); do
         curl -sLS "${vsixDownloadLink}" -o "${vsixFilename}"
 
         initTest "Checking $vsixFilename"
-  
+
         # Extract the supported version of VS Code engine from the package.json
         vscodeEngineVersion=$(unzip -p "$vsixFilename" "extension/package.json" | jq -r '.engines.vscode')
 
@@ -215,13 +190,13 @@ for i in $(seq 0 "$((numberOfExtensions - 1))"); do
         vscodeEngineVersion="${vscodeEngineVersion//x/0}"
         # check if the extension's engine version is compatible with the code version
         # if the extension's engine version is ahead of the code version, check a next version of the extension
-        if [[  "$vscodeEngineVersion" = "$(echo -e "$vscodeEngineVersion\n$codeVersion" | sort -V | head -n1)" ]]; then
+        if [[ "$vscodeEngineVersion" = "$(echo -e "$vscodeEngineVersion\n$codeVersion" | sort -V | head -n1)" ]]; then
             #VS Code version >= Engine version, can proceed."
             echo -e "${GREEN}${EMOJI_PASS}${RESETSTYLE} compatible."
-        else 
+        else
             echo -e "Extension requires a newer engine version than Che Code version ($codeVersion)."
             echo -e "${RED}${EMOJI_FAIL}${RESETSTYLE} Test failed!"
             exit 1
         fi
     fi
-done;
+done
