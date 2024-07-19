dcf91418651b9c143639282c275b3d307f60273c
sdawley <sdawley@redhat.com>
2024-07-19 12:32:54 -0700

    Remove UDI specific stuff now that UDi uses cachito. Remove cachito'd containers from manifests list

--- product/manifest/get-3rd-party-deps-manifests.sh
+++ product/manifest/get-3rd-party-deps-manifests.sh
@@ -63,8 +63,8 @@ done
 cd /tmp || exit
 
 if [[ ! ${MIDSTM_BRANCH} ]]; then usage; fi
-# phases 2 and 3 have been removed 
-if [[ ! ${phases} ]]; then phases=" 1 4 5 6 7 8 9 "; fi
+# phases 2, 3, 4, 5 and 9 have been removed 
+if [[ ! ${phases} ]]; then phases=" 1 6 7 8 "; fi
 
 if [[ ! ${CSV_VERSION} ]]; then 
   CSV_VERSION=$(curl -sSLo - https://raw.githubusercontent.com/redhat-developer/devspaces-operator/${MIDSTM_BRANCH}/manifests/devspaces.csv.yaml | yq -r .spec.version)
@@ -92,19 +92,6 @@ function bth () {
 	echo "$1" | tee -a ${LOG_FILE}
 }
 
-function getBashVars () {
-	dir="$1" # script dir
-	buildsh="${2:-build.sh}" # collect vars from a specific build_*.sh script
-	# parse the specific file and export the correct variables
-	pushd /tmp/devspaces-images >/dev/null || exit 1
-		for p in ${dir}/build/${buildsh}; do 
-			grep -E "export " $p | grep -E -v "SCRIPT_DIR|PATH=" | sed -r -e "s@#.+@@g" > "${p}.tmp"
-			# shellcheck disable=SC1090
-			. "${p}.tmp" && rm -f ${p}.tmp
-		done
-	popd >/dev/null || exit 1
-}
-
 function npmList() {
 	prefix="$1"
 	for dep in $(npm list | sed -e "s#[├└─┬│ ]\+##g" -e "s#deduped##g" | sort | uniq | sed -e "s#@#:#g" || true); do
@@ -117,20 +104,6 @@ function dotnetList() {
 	cd /tmp
 	wget https://raw.githubusercontent.com/OmniSharp/omnisharp-roslyn/v${DOTNET_LS_VERSION}/tools/packages.config -q -O - | grep "<package " | \
 sed -e "s#.\+<package id=\"\(.\+\)\" version=\"\(.\+\)\".\+#${prefix}\1:\2#g" | tee -a ${MANIFEST_FILE}
-}
-
-function phpList() {
-	prefix="$1"
-	for dep in $(php composer.phar show -f json | jq -r '.installed[] | [.name,.version] | @csv' | tr -d "\""); do
-		echo "$prefix${dep/,/:}" >> ${MANIFEST_FILE}
-	done
-}
-
-function pythonList() {
-	prefix="$1"
-	for dep in $(/usr/bin/python3 -m pip list --format freeze | sed -e "s#==#:#g"); do
-		echo "$prefix$dep" >> ${MANIFEST_FILE}
-	done
 }
 
 # wget dockerfiles
@@ -158,7 +131,7 @@ function logDockerDetails ()
 
 rm -f ${LOG_FILE} ${MANIFEST_FILE}
 
-if [[ ${phases} == *"1"* ]] || [[ ${phases} == *"4"* ]] || [[ ${phases} == *"5"* ]] || [[ ${phases} == *"6"* ]]; then
+if [[ ${phases} == *"1"* ]] || [[ ${phases} == *"6"* ]]; then
 	log "1a. Check out 3rd party language server dependencies builder repo (will collect variables later)" 
 	cd /tmp
 	if [[ ! -d devspaces-images ]]; then
@@ -178,21 +151,13 @@ if [[ ${phases} == *"1"* ]]; then
 	rm -fr ${MANIFEST_FILE/.txt/-containers-binaries-extras.txt} ${MANIFEST_FILE/.txt/-containers-base-images-only.txt}
 	log "1b. Define list of upstream containers & RPMs pulled into them from https://pkgs.devel.redhat.com/cgit/?q=devspaces "
 	for d in \
-	devspaces-code \
-	devspaces-configbump \
-	devspaces-dashboard \
 	devspaces-devfileregistry \
 	devspaces-idea \
-	\
-	devspaces-imagepuller \
 	devspaces-machineexec \
-	devspaces-operator \
 	devspaces-operator-bundle \
 	devspaces-pluginregistry \
 	\
 	devspaces-server \
-	devspaces-traefik \
-	devspaces-udi \
 	; do
 		if [[ $d == "devspaces" ]]; then
 			containerName=${d##containers/}-server-rhel8-container
@@ -218,90 +183,9 @@ if [[ ${phases} == *"1"* ]]; then
 	log "https://github.com/redhat-developer/devtools-productization/tree/main/codeready-workspaces"
 fi
 
-##################################
-
-if [[ ${phases} == *"4"* ]]; then
-	cd /tmp
-	log ""
-	log " == php =="
-	log""
-	log "4. Install php deps: "
-	if [[ ! $(which php) ]]; then sudo yum -y -q install php-devel php-json || true; fi
-	if [[ ! $(which php) ]]; then echo "Error: install php to run this script: sudo yum -y install php-devel php-json"; exit 1; fi
-	getBashVars devspaces-udi build_php.sh
-	for d in \
-		"PHP_LS_VERSION" \
-		"PHP_LS_IMAGE" \
-		"PHP_XDEBUG_IMAGE" \
-		; do
-		log " * $d = ${!d}"
-	done
-	log ""
-	log "$ php composer.phar require jetbrains/phpstorm-stubs:dev-master"
-	log "$ php composer.phar require felixfbecker/language-server:${PHP_LS_VERSION}"
-	cd /tmp
-	rm -fr /tmp/php-deps-tmp
-	mkdir -p php-deps-tmp && cd php-deps-tmp
-
-	php --version # need something newer than 5.5.9 - https://github.com/composer/composer/issues/4792
-	curl -sSL https://getcomposer.org/installer > /tmp/installer && php /tmp/installer || exit 1
-	php composer.phar require -d /tmp/php-deps-tmp jetbrains/phpstorm-stubs:dev-master | tee -a ${LOG_FILE}
-	log ""
-	php composer.phar require -d /tmp/php-deps-tmp felixfbecker/language-server:${PHP_LS_VERSION} | tee -a ${LOG_FILE}
-	# php composer.phar run-script --working-dir=vendor/felixfbecker/language-server parse-stubs # does not install new deps, so don't need to run this
-	php composer.phar show >> ${LOG_FILE}
-	mnf "devspaces-udi-container:${CSV_VERSION}/jetbrains/phpstorm-stubs:dev-master"
-	mnf "devspaces-udi-container:${CSV_VERSION}/felixfbecker/language-server:v${PHP_LS_VERSION}"
-	phpList "  devspaces-udi-container:${CSV_VERSION}/"
-	mnf ""
-	cd /tmp
-	rm -fr /tmp/php-deps-tmp
-fi
-
-##################################
-
-if [[ ${phases} == *"5"* ]]; then
-	cd /tmp
-	log ""
-	log " == python =="
-	log ""
-	log "5. Install python deps: pip install python-language-server[all]==${PYTHON_LS_VERSION}"
-	pyrpms="python3-six python3-pip"
-	if [[ ! $(which python3) ]] || [[ ! $(pydoc3 modules | grep venv) ]]; then sudo yum install -y -q $pyrpms || true; fi
-	if [[ ! $(which python3) ]] || [[ ! $(pydoc3 modules | grep venv) ]]; then echo "Error: install $pyrpms to run this script: sudo yum -y install $pyrpms"; exit 1; fi
-	getBashVars devspaces-udi build_python.sh
-	for d in \
-		"PYTHON_IMAGE" \
-		"PYTHON_LS_VERSION" \
-		; do
-		log " * $d = ${!d}"
-	done
-	log ""
-	cd /tmp
-	rm -fr /tmp/python-deps-tmp
-	mkdir -p python-deps-tmp && cd python-deps-tmp
-
-	/usr/bin/python3 -m venv env
-	source env/bin/activate
-	/usr/bin/python3 -m pip install --upgrade pip
-	{ /usr/bin/python3 -m pip install python-language-server[all]==${PYTHON_LS_VERSION} | tee -a ${LOG_FILE}; } || true
-	log ""
-	{ /usr/bin/python3 -m pip list >> ${LOG_FILE}; } || true
-	mnf "devspaces-udi-container:${CSV_VERSION}/python-language-server[all]:${PYTHON_LS_VERSION}"
-	pythonList "  devspaces-udi-container:${CSV_VERSION}/"
-	deactivate
-	rm -fr /tmp/python-deps-tmp
-fi
-
-# now we can delete the devspaces-images checkout folder as we don't need its contents anymore
-rm -fr /tmp/devspaces-images
-
 
 ##################################
 # Now call the other get-3rd-party-deps*.sh scripts, and merge results into one overall manifest
-##################################
-
-
 ##################################
 
 if [[ ${phases} == *"6"* ]]; then
